0ef6fd46caca54dab09c26c52f8793bf9a4ed2f4
kumavis <aaron@kumavis.me>
2018-12-29 02:23:17 -0500

    prelude - disallow loading modules via previous requires

--- src/preludeTemplate.js
+++ src/preludeTemplate.js
@@ -24,8 +24,6 @@ __defaultEndowments__
   }
 
   function outer(modules, globalCache, entryPoints) {
-    // Save the require from previous bundle to this closure if any
-    var previousRequire = typeof require == "function" && require
 
     function newRequire(name, jumped, providedEndowments, scopedEndowmentsConfig, depPath){
       // check our modules
@@ -106,20 +104,9 @@ __defaultEndowments__
 
         return module.exports
       }
-      // we dont have it, look somewhere else
 
-      // if we cannot find the module within our internal map or
-      // cache, jump to the current global require ie. the last bundle
-      // that was added to the page.
-      var currentRequire = typeof require == "function" && require
-      if (!jumped && currentRequire) return currentRequire(name, true)
-
-      // If there are other bundles on this page the require from the
-      // previous one is saved to 'previousRequire'. Repeat this as
-      // many times as there are bundles until the module is found or
-      // we exhaust the require chain.
-      if (previousRequire) return previousRequire(name, true)
-      var err = new Error('Cannot find module \'' + name + '\'')
+      // we dont have it, throw an error
+      const err = new Error('Cannot find module \'' + name + '\'')
       err.code = 'MODULE_NOT_FOUND'
       throw err
     }
