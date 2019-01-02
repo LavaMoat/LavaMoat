f07376b0820a3990b6f33f09e45503f152a99381
kumavis <aaron@kumavis.me>
2019-01-01 23:30:34 -0500

    plugin - rewrite to survive rebundles

--- packages/browserify/src/index.js
+++ packages/browserify/src/index.js
@@ -4,36 +4,42 @@ const createCustomPack = require('./browser-pack')
 
 /*  export a Browserify plugin  */
 module.exports = function (browserify, pluginOpts) {
+  // setup the plugin in a re-bundle friendly way
+  browserify.on('reset', setupPlugin)
+  setupPlugin()
+
   // override browserify/browser-pack prelude
-  const defaults = {
-    raw: true,
-    prelude: generatePrelude(pluginOpts),
+  function setupPlugin() {
+    const defaults = {
+      raw: true,
+      prelude: generatePrelude(pluginOpts),
+    }
+
+    defaults.generateModuleInitializer = function (row) {
+      const moduleInitSrc = [
+        `//# sourceURL=${row.sourceFile}`,
+        '(function(require,module,exports){',
+        // combineSourceMap.removeComments(row.source),
+        row.source,
+        '})',
+      ].join('\n')
+      return escapeCodeAsString(moduleInitSrc)
+    }
+
+    const opts = Object.assign({}, browserify._options, defaults, pluginOpts)
+    const customPack = createCustomPack(opts)
+    // replace the standard browser-pack with our custom packer
+    browserify.pipeline.splice('pack', 1, customPack)
   }
 
-  defaults.generateModuleInitializer = function (row) {
-    const moduleInitSrc = [
-      `//# sourceURL=${row.sourceFile}`,
-      '(function(require,module,exports){',
-      // combineSourceMap.removeComments(row.source),
-      row.source,
-      '})',
-    ].join('\n')
-    return escapeCodeAsString(moduleInitSrc)
-  }
-
-  function escapeCodeAsString (source) {
-    const escapedSource = source
-      .split('\\').join('\\\\')
-      .split('$').join('\\$')
-      .split('`').join('\\`')
-    return `\`${escapedSource}\``
-  }
-
-  const opts = Object.assign({}, browserify._options, defaults, pluginOpts)
-  const customPack = createCustomPack(opts)
-  // replace the standard browser-pack with our custom packer
-  browserify.pipeline.splice('pack', 1, customPack)
 }
 
-module.exports.prelude = generatePrelude()
 module.exports.generatePrelude = generatePrelude
+
+function escapeCodeAsString (source) {
+  const escapedSource = source
+    .split('\\').join('\\\\')
+    .split('$').join('\\$')
+    .split('`').join('\\`')
+  return `\`${escapedSource}\``
+}
