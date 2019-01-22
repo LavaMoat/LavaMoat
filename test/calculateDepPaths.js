c4130ce751498635f1ce4ddb023f12eebf65a83b
kumavis <aaron@kumavis.me>
2019-01-22 22:45:27 +0800

    test - add simple tests

---
+++ test/calculateDepPaths.js
@@ -0,0 +1,46 @@
+// a -> [ b, c, d, e ]
+// b -> [ c, d ]
+// c -> [ d ]
+// e -> [ f ]
+// f -> [ g ]
+// g -> [ d ]
+
+// d <- [ a, b, c, g]
+// c <- [ a, b ] 
+// b <- [ a ]
+// e <- [ a ]
+// f <- [ e ]
+// g <- [ f ]
+
+// d:
+// [ a, d ]
+// [ a, b, d ]
+// [ a, c, d ]
+// [ a, b, c, d ]
+// [ a, e, f, g, d ]
+
+const test = require('tape')
+const flatMap = require('lodash.flatmap')
+const { calculateDepPaths } = require('../src/generateConfig')
+
+
+test('walk reverse dep graph', (t) => {
+  const reverseDepGraph = {
+    d: new Set(['a', 'b', 'c', 'g']),
+    c: new Set(['a', 'b']),
+    b: new Set(['a']),
+    e: new Set(['a']),
+    f: new Set(['e']),
+    g: new Set(['f']),
+  }
+  
+  const depPaths = calculateDepPaths('d', reverseDepGraph)
+  t.deepEqual(depPaths, [
+    [ 'a', 'd' ],
+    [ 'a', 'b', 'd' ],
+    [ 'a', 'c', 'd' ],
+    [ 'a', 'b', 'c', 'd' ],
+    [ 'a', 'e', 'f', 'g', 'd' ]
+  ])
+  t.end()
+})
