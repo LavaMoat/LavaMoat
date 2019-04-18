540a25708db6f9e242a467c5cd279d85e0de9052
kumavis <aaron@kumavis.me>
2019-04-18 16:54:41 +0800

    simulation - add forceBoundry

--- packages/lavamoat-viz/src/viz/simulation.js
+++ packages/lavamoat-viz/src/viz/simulation.js
@@ -1,4 +1,5 @@
 const d3 = require('d3')
+const forceBoundry2 = require('./forceBoundry').default
 
 module.exports = { setupSimulation, setupSimulationForces }
 
@@ -11,25 +12,36 @@ function setupSimulation(state) {
 function setupSimulationForces (simulation, state) {
   const nodes = Object.values(state.nodes)
   const links = Object.values(state.links)
-  const graphWidth = state.container.width || 0
-  const graphHeight = state.container.height || 0
+  const width = state.container.width || 0
+  const height = state.container.height || 0
 
   simulation
     .nodes(nodes)
     // pull nodes along links
     .force('link', d3.forceLink().id(d => d.id).links(links).distance(d => d.distance))
     // push nodes away from each other
-    // .force('charge', d3.forceManyBody().strength(-30))
     .force('charge', d3.forceManyBody().strength(d => -4 * d.radius))
     .force('collision', d3.forceCollide().radius(d => 1.2 * d.radius))
     // translate nodes around the center
-    .force('center', d3.forceCenter(graphWidth / 2, graphHeight / 2))
+    .force('center', d3.forceCenter(width / 2, height / 2))
     // push nodes towards the center
-    .force('x', d3.forceX(graphWidth / 2, 0.5))
-    .force('y', d3.forceY(graphHeight / 2, 0.5))
+    .force('x', d3.forceX(width / 2, 1))
+    .force('y', d3.forceY(height / 2, 1))
+    // push nodes back into frame
+    .force('boundry', forceBoundry2(0, 0, width, height))
+    // warm then cool
     .alpha(1)
     .alphaTarget(0)
     .restart()
+}
+
+function createForcePerNode (forceFn) {
+  return createForce((nodes, alpha) => {
+    for (let index = 0; index < nodes.length; index++) {
+      const node = nodes[index]
+      forceFn(node, alpha)
+    }
+  })
 }
 
 function createForce (forceFn) {
