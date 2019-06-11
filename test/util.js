27dd2346d732bb503ad548dc7dd0e232556b1b9c
kumavis <aaron@kumavis.me>
2019-06-11 23:02:10 +0800

    tests - refactor a bit

--- test/util.js
+++ test/util.js
@@ -4,7 +4,12 @@ const miss = require('mississippi')
 const browserify = require('browserify')
 const from = require('from')
 const pify = require('pify')
+const pump = require('pump')
+const toStream = require('mississippi').to.obj
+
+
 const sesifyPlugin = require('../src/index')
+const { createConfigSpy } = require('../src/generateConfig')
 
 const { generatePrelude, createSesifyPacker } = sesifyPlugin
 const basicSesifyPrelude = generatePrelude()
@@ -13,6 +18,8 @@ module.exports = {
   createBundleFromEntry,
   createBundleFromRequiresArray,
   createBundleFromRequiresArrayPath,
+  generateConfigFromFiles,
+  filesToConfigSource,
 }
 
 async function createBundleFromEntry (path) {
@@ -38,6 +45,27 @@ async function createBundleFromRequiresArray (depsArray, sesifyConfig) {
       pack,
       miss.concat((result) => resolve(result.toString())),
       (err) => { if (err) reject(err) },
+    )
+  })
+}
+
+async function generateConfigFromFiles({ files }) {
+  const configSource = await filesToConfigSource({ files })
+  const config = JSON.parse(configSource)
+  return config
+}
+
+async function filesToConfigSource({ files }) {
+  return new Promise((resolve, reject) => {
+    const configSpy = createConfigSpy({ onResult: resolve })
+    const sink = toStream((data, encoding, cb) => cb())
+    pump(
+      from(files),
+      configSpy,
+      sink,
+      (err) => {
+        if (err) return reject(err)
+      }
     )
   })
 }
