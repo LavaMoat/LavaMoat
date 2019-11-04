d73c8d7fe0350cb5a01d27ccf4f1c11ede154b55
kumavis <aaron@kumavis.me>
2019-11-04 15:29:17 +0800

    generatePrelude - use templateRequire for cleaner module injections

--- packages/browserify/src/generatePrelude.js
+++ packages/browserify/src/generatePrelude.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 const jsonStringify = require('json-stable-stringify')
 const preludeTemplate = fs.readFileSync(__dirname + '/preludeTemplate.js', 'utf8')
 const sessDist = fs.readFileSync(__dirname + '/../lib/ses.umd.js', 'utf8')
-const magicCopySrc = fs.readFileSync(__dirname + '/magicCopy.js', 'utf8')
+const makeMagicCopySrc = fs.readFileSync(__dirname + '/magicCopy.js', 'utf8')
 const makeGetEndowmentsForConfigSrc = fs.readFileSync(__dirname + '/makeGetEndowmentsForConfig.js', 'utf8')
 const makePrepareRealmGlobalFromConfigSrc = fs.readFileSync(__dirname + '/makePrepareRealmGlobalFromConfig.js', 'utf8')
 
@@ -19,12 +19,35 @@ function generatePrelude (opts = {}) {
 
   let output = preludeTemplate
   output = output.replace('__sessDist__', sessDist)
-  output = output.replace('__magicCopy__', magicCopySrc)
   output = output.replace('__lavamoatConfig__', lavamoatConfig)
-  output = output.replace('__makeGetEndowmentsForConfig__', makeGetEndowmentsForConfigSrc)
-  output = output.replace('__makePrepareRealmGlobalFromConfig__', makePrepareRealmGlobalFromConfigSrc)
+
+  replaceTemplateRequire('makeMagicCopy', makeMagicCopySrc)
+  replaceTemplateRequire('makeGetEndowmentsForConfig', makeGetEndowmentsForConfigSrc)
+  replaceTemplateRequire('makePrepareRealmGlobalFromConfig', makePrepareRealmGlobalFromConfigSrc)
+
+  function replaceTemplateRequire (moduleName, src) {
+    const wrappedSrc = wrapWithReturnCjsExports(moduleName, src)
+    output = output.replace(`templateRequire('${moduleName}')`, wrappedSrc)
+  }
 
   return output
+}
+
+function wrapWithReturnCjsExports (label, src) {
+  return (
+`// define ${label}
+(function(){
+  const exports = {}
+  const module = { exports }
+  ;(function(){
+// START of injected code from ${label}
+${src}
+// END of injected code from ${label}
+  })()
+  return module.exports
+})()`
+  )
+
 }
 
 function parseConfig (config) {
