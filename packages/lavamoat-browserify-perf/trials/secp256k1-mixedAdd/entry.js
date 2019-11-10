ee0e159a733b88297087496f0cfa80a6a0d0f9f4
kumavis <aaron@kumavis.me>
2019-11-10 16:38:11 +0800

    add secp256k1-mixedAdd trial

---
+++ packages/lavamoat-browserify-perf/trials/secp256k1-mixedAdd/entry.js
@@ -0,0 +1,181 @@
+const ethUtil = require('ethereumjs-util')
+const Buffer = require('safe-buffer').Buffer
+
+// use `global.process` so it works correctly under node
+const nTimes = Number.parseInt(global.process.env.PERF_N || 5, 10)
+
+const passphrase = `brain wallet seed #${0}`
+const privKey = ethUtil.keccak256(Buffer.from(passphrase))
+// const pubKey = ethUtil.privateToPublic(privKey)
+// const address = ethUtil.publicToAddress(pubKey)
+// const addressString = ethUtil.bufferToHex(address)
+const g = require('secp256k1/lib/js/ecpointg')
+const BN = require('secp256k1/lib/js/bn')
+const ECJPoint = require('secp256k1/lib/js/ecjpoint')
+const ECPoint = require('secp256k1/lib/js/ecpoint')
+
+
+const privateKey = privKey
+const d = BN.fromBuffer(privateKey)
+if (d.isOverflow() || d.isZero()) throw new Error(messages.EC_PUBLIC_KEY_CREATE_FAIL)
+
+// // ECPOINTG
+  const __this = {}
+
+//   // constructor
+  __this.x = BN.fromBuffer(Buffer.from('79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798', 'hex'))
+  __this.y = BN.fromBuffer(Buffer.from('483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8', 'hex'))
+  __this.inf = false
+
+//   // _precompute
+  var ecpoint = new ECPoint(__this.x, __this.y)
+
+  var dstep = 4
+  var points = new Array(1 + Math.ceil(257 / dstep))
+  var acc = points[0] = ecpoint
+  for (var i = 1; i < points.length; ++i) {
+    for (var j = 0; j < dstep; j++) acc = acc.dbl()
+    points[i] = acc
+  }
+
+  __this.precomputed = {
+    naf: ecpoint._getNAFPoints(7),
+    doubles: {
+      step: dstep,
+      points: points,
+      negpoints: points.map(function (p) { return p.neg() })
+    }
+  }
+
+  // mul
+  const num = d
+  var step = __this.precomputed.doubles.step
+  var points = __this.precomputed.doubles.points
+  var negpoints = __this.precomputed.doubles.negpoints
+
+  var naf = num.getNAF(1)
+  var I = ((1 << (step + 1)) - (step % 2 === 0 ? 2 : 1)) / 3
+
+  // Translate into more windowed form
+  var repr = []
+  for (var j = 0; j < naf.length; j += step) {
+    var nafW = 0
+    for (var k = j + step - 1; k >= j; k--) nafW = (nafW << 1) + naf[k]
+    repr.push(nafW)
+  }
+
+  var a = new ECJPoint(null, null, null)
+  var b = new ECJPoint(null, null, null)
+
+  // for (var i = I; i > 0; i--) {
+  //   for (var jj = 0; jj < repr.length; jj++) {
+      // if (repr[jj] === i) {
+      //   const i = 10
+        const jj = 45
+      //   b = b.mixedAdd(points[jj])
+      // } else if (repr[jj] === -i) {
+      //   b = b.mixedAdd(negpoints[jj])
+      // }
+    // }
+
+    a = a.add(b)
+  // }
+
+Array(nTimes).fill().forEach((_, index) => {
+  // const pubKey = ethUtil.privateToPublic(privKey)
+  // ethUtil.secp256k1.publicKeyCreate(privKey, false).slice(1);
+  // assert.isBuffer(privateKey, messages.EC_PRIVATE_KEY_TYPE_INVALID)
+  // assert.isBufferLength(privateKey, 32, messages.EC_PRIVATE_KEY_LENGTH_INVALID)
+  // return secp256k1.publicKeyCreate(privateKey, false)
+  //  function publicKeyCreate (privateKey, compressed) {
+
+  // g.mul(d).toPublicKey(false)
+  // g.mul(d)
+
+  // for (var i = I; i > 0; i--) {
+  //   for (var jj = 0; jj < repr.length; jj++) {
+  //     if (repr[jj] === i) {
+        const i = 10
+        const jj = 45
+        b = b.mixedAdd(points[jj])
+    //   } else if (repr[jj] === -i) {
+    //     b = b.mixedAdd(negpoints[jj])
+    //   }
+    // }
+
+    // a = a.add(b)
+  // }
+
+  // return ECPoint.fromECJPoint(a)
+  // O + P = P
+
+  // mixedAdd
+  // const _this = b
+  // const p = points[jj]
+
+  // if (_this.inf) return p.toECJPoint()
+
+  // // P + O = P
+  // if (p.inf) return _this
+
+  // // http://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#addition-add-1998-cmo-2
+  // //   with p.z = 1
+  // // 8M + 3S + 7A
+  // var z2 = _this.z.redSqr()
+  // var u1 = _this.x
+  // var u2 = p.x.redMul(z2)
+  // var s1 = _this.y
+  // var s2 = p.y.redMul(z2).redMul(_this.z)
+
+  // var h = u1.redSub(u2)
+  // var r = s1.redSub(s2)
+  // if (h.isZero()) {
+  //   if (r.isZero()) return _this.dbl()
+  //   return new ECJPoint(null, null, null)
+  // }
+
+  // var h2 = h.redSqr()
+  // var v = u1.redMul(h2)
+  // var h3 = h2.redMul(h)
+
+  // var nx = r.redSqr().redIAdd(h3).redISub(v).redISub(v)
+  // var ny = r.redMul(v.redISub(nx)).redISub(s1.redMul(h3))
+  // var nz = _this.z.redMul(h)
+
+  // return new ECJPoint(nx, ny, nz)
+})
+
+
+// what is slow?
+// cross realm back-and-forth?
+// would package realm-sharing improve perf?
+// seems like `getAddressString` imparts a large slowdown
+// cross realm BN ops?
+
+// (task name): (lavamoat), (lavamoat+harden)
+// full wallet: 4.2, 3.7
+// ----------------------
+// keccak: 2.9, 2.0
+// privateToPublic: 4.6, 4.0
+// publicToAddress: 2.2, 2.0
+// bufferToHex: 2.6, 2.4
+
+// publicKeyCreate: 4.7
+// g.mul(d).toPublicKey(false): 4.8
+// g.mul(d): 4.9
+
+// ( then i started calculating time differently )
+
+// g.mul(d): 5.7
+// full mul: 5.4
+// mul wo from ecj: 5.8
+// naf+repr: 3.0
+// naf: 3.0
+// new ECJPoint + ecj for-for: 5.6
+// new ECJPoint: 184.2, 192.8 (wow! but still fast)
+// ecj for-for: 5.6
+// b-only: 5.6
+// mixedAdd: 6.0, 6.0
+// manual mixedAdd: 1.1, 1.1 (wow! elevating this code has huge speed diff)
+
+// 0.51 -> 35.3 whaat 70x??
