7c104b8cf19f9fb99e36e9afe122dbbf7f8580ec
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-01-02 17:25:24 +0800

    test - add transforms test to ensure workarounds

--- test/pluginOpts.js
+++ test/pluginOpts.js
@@ -1,5 +1,4 @@
 const test = require('tape')
-const index = require('../src/index')
 const { runAutoConfig } = require('./util')
 
 test("pluginOpts - returns empty config if writeAutoConfig is specified", async (t) => {
