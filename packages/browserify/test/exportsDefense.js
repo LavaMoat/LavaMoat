32087e184eaaf65954ed3b3d0ca59c80cb9f9665
kumavis <aaron@kumavis.me>
2020-01-02 15:38:19 +0800

    exportsDefense - configure protections on all moduleExports, not just pacakage boundaries

--- packages/browserify/test/exportsDefense.js
+++ packages/browserify/test/exportsDefense.js
@@ -4,8 +4,7 @@ const test = require('tape-promise').default(require('tape'))
 const clone = require('clone')
 const {
   runSimpleOneTwo,
-  createBundleFromRequiresArray,
-  fnToCodeBlock,
+  runSimpleOneTwoSamePackage,
 } = require('./util')
 
 test('exportsDefense - readOnly restrictions have override workaround fix', async (t) => {
@@ -22,4 +21,31 @@ test('exportsDefense - readOnly restrictions have override workaround fix', asyn
 
   t.equal(one.xyz, 2, 'should update the property correctly')
   t.equal(Object.getPrototypeOf(one).xyz, 1, 'should not update the prototype')
+})
+
+test('exportsDefense - indirectly imported package should be readOnly', async (t) => {
+  function defineOne () {
+    const Thing = require('two')
+    module.exports = {
+      create: () => new Thing()
+    }
+  }
+  function defineTwo () {
+    class Thing {}
+    Thing.prototype.action = () => true
+    module.exports = Thing
+  }
+
+  const one = await runSimpleOneTwoSamePackage({ defineOne, defineTwo })
+  const thing = one.create()
+
+  // corrupt prototype
+  const thingProto = Reflect.getPrototypeOf(thing)
+  try {
+    thingProto.action = () => false
+  } catch (err) {
+    t.ok(err, 'encountered an expected error overriding the prototype')
+  }
+
+  t.equal(thing.action(), true, 'Thing.prototype.action was not corrupted')
 })
