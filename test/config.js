73f799cefc4ab1c01f933857631db5dfe7100fab
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-01-28 16:47:36 +0800

    promise resolves when 'update' is fired

--- test/config.js
+++ test/config.js
@@ -284,21 +284,17 @@ test('Config edits trigger re-bundle if using watchify', async (t) => {
   const configFile = fs.readFileSync(configPath)
   const configFileString = configFile.toString()
   
-  await new Promise(async(resolve) => {
-    bundler.on('update', async () => {
-      await createBundleFromRequiresArray([], {})
-      const updatedConfigFile = fs.readFileSync(configPath)
-      const updatedConfigFileString = updatedConfigFile.toString()
-      rimraf.sync('./lavamoat')
-
-      t.notOk(configFileString.includes('"three": 12345678'))
-      t.ok(updatedConfigFileString.includes('"three": 12345678'))
-      resolve()
-    })
-    
+  await new Promise((resolve) => {
+    bundler.on('update', () => resolve())
     const overrideString = JSON.stringify(configDefault)
     fs.writeFileSync(overridePath, overrideString)
   })
-  
-  
+
+  await createBundleFromRequiresArray([], {})
+  const updatedConfigFile = fs.readFileSync(configPath)
+  const updatedConfigFileString = updatedConfigFile.toString()
+  rimraf.sync('./lavamoat')
+
+  t.notOk(configFileString.includes('"three": 12345678'))
+  t.ok(updatedConfigFileString.includes('"three": 12345678'))
 })
