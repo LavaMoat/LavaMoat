7d394df7aa0127d998880aed817be7a7e22b48e6
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-03-05 13:55:08 +0800

    Removed production and minimize options in webpack config, redundant

--- packages/webpack/test/project1/webpack.config.js
+++ packages/webpack/test/project1/webpack.config.js
@@ -1,9 +1,7 @@
 const LavaMoatPlugin = require('../../src/index')
 
 module.exports = {
-  mode: 'production',
   optimization: {
-    minimize: false,
     concatenateModules: false,
   },
   plugins: [
