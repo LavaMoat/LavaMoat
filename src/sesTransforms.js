0242d5fe123a963632ce6ebd43c7b7569e17200c
kumavis <aaron@kumavis.me>
2020-03-18 14:02:03 +0800

    ses-transforms - remove unneeded special case for _inheritLoose

--- src/sesTransforms.js
+++ src/sesTransforms.js
@@ -1,44 +1,17 @@
 const { makeStringTransform } = require('browserify-transform-tools')
 
 module.exports = {
-  createSesWorkaroundsTransform,
-  applyInheritsLooseWorkaround
+  createSesWorkaroundsTransform
 }
 
 function createSesWorkaroundsTransform () {
   return makeStringTransform('ses-workarounds', { excludeExtension: ['.json'] }, (content, _, cb) => {
-    let moduleContent = content
+    const moduleContent = content
       // html comment
       .split('-->').join('-- >')
       // use indirect eval
       .split(' eval(').join(' (eval)(')
     // "inheritsLoose" workaround
-    moduleContent = applyInheritsLooseWorkaround(moduleContent)
     cb(null, moduleContent)
   })
-}
-
-function applyInheritsLooseWorkaround (sourceString) {
-  // a pattern used by many modules but incompatible with SES due to setting constructor
-  // this horrible regex should catch minified code too
-  const inheritsPattern = new RegExp('function _inheritsLoose\\((\\w+),\\s?(\\w+)\\)\\s?{\\s*?\\1.prototype\\s?=\\s?Object.create\\(\\2.prototype\\)[a-zA-z.=;,\\s\\(\\)]*}', 'g')
-  // gather results
-  const searchResults = []
-  let patternResult = inheritsPattern.exec(sourceString)
-  while (patternResult) {
-    const start = patternResult.index
-    const end = start + patternResult[0].length
-    searchResults.push([start, end])
-    patternResult = inheritsPattern.exec(sourceString)
-  }
-  // replace results
-  const replacementString = 'function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype, { constructor: { value: subClass } }); subClass.__proto__ = superClass; }'
-  let offset = 0
-  searchResults.forEach(([start, end]) => {
-    const origLength = end - start
-    const lengthDiff = replacementString.length - origLength
-    sourceString = sourceString.slice(0, offset + start) + replacementString + sourceString.slice(offset + end)
-    offset += lengthDiff
-  })
-  return sourceString
 }
--- test/transforms.js
+++ test/transforms.js
@@ -18,31 +18,20 @@ test('transforms - Ses transforms work', async (t) => {
   t.end()
 })
 
-test('transforms - inheritsLoose workaround', async (t) => {
-  const input = `${function () {
-    // minified 1
+// this originally required a transform but does not any more
+// we still need to ensure this pattern works
+test('transforms - common pattern "_inheritsLoose" works', async (t) => {
+  function defineOne () {
+    const two = require('two')
+    module.exports = two
+  }
+  function defineTwo () {
+    function SubError () {}
+    _inheritsLoose(SubError, TypeError)
     function _inheritsLoose (t, e) { t.prototype = Object.create(e.prototype), (t.prototype.constructor = t).__proto__ = e }
-    // minified 2
-    function _inheritsLoose (e, r) { e.prototype = Object.create(r.prototype), (e.prototype.constructor = e).__proto__ = r }
-    // formatted
-    function _inheritsLoose (subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass }
-    // newlines
-    function _inheritsLoose (subClass, superClass) {
-      subClass.prototype = Object.create(superClass.prototype)
-      subClass.prototype.constructor = subClass
-      subClass.__proto__ = superClass
-    }
-  }}`
-  const result = applyInheritsLooseWorkaround(input)
-  t.equal(result, `${function () {
-    // minified 1
-    function _inheritsLoose (subClass, superClass) { subClass.prototype = Object.create(superClass.prototype, { constructor: { value: subClass } }); subClass.__proto__ = superClass }
-    // minified 2
-    function _inheritsLoose (subClass, superClass) { subClass.prototype = Object.create(superClass.prototype, { constructor: { value: subClass } }); subClass.__proto__ = superClass }
-    // formatted
-    function _inheritsLoose (subClass, superClass) { subClass.prototype = Object.create(superClass.prototype, { constructor: { value: subClass } }); subClass.__proto__ = superClass }
-    // newlines
-    function _inheritsLoose (subClass, superClass) { subClass.prototype = Object.create(superClass.prototype, { constructor: { value: subClass } }); subClass.__proto__ = superClass }
-  }}`)
+  }
+
+  const one = await runSimpleOneTwo({ defineOne, defineTwo })
+  t.ok(one)
   t.end()
 })
