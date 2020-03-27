b712ab4b1d8c1ecc421cb691bf3bf27580186ca9
kumavis <aaron@kumavis.me>
2020-03-27 12:52:59 +0800

    node/examples - use bad-example-eval-server

---
+++ packages/node/examples/server/README.md
@@ -0,0 +1,13 @@
+### generate config
+
+```bash
+npx lavamoat ./index.js --writeAutoConfig
+```
+
+### run
+
+```bash
+npx lavamoat ./index.js
+```
+
+### interact
--- packages/node/examples/server/index.js
+++ packages/node/examples/server/index.js
@@ -1,14 +1,3 @@
-const http = require('http');
+const createServer = require('bad-example-eval-server')
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+createServer()
