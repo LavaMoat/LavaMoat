af4df838edda1e2490cbe23541493387d11307ca
kumavis <kumavis@users.noreply.github.com>
2020-04-02 14:01:06 +0800

    Merge pull request #11 from LavaMoat/serve-from-project
    Add serve to CLI and switch to Yargs

--- packages/lavamoat-viz/bin/index.js
+++ packages/lavamoat-viz/bin/index.js
@@ -2,19 +2,55 @@
 
 const { promises: fs } = require('fs')
 const path = require('path')
-const argv = require('yargs').argv
+const yargs = require('yargs')
 const { ncp } = require('ncp')
 const pify = require('pify')
+const open = require('open')
 
-const { config, deps, dest } = argv
+const { config, deps, dest, serve } = parseArgs()
 
-if (!dest) throw new Error('missing cli argument "--dest')
-if (!deps) throw new Error('missing cli argument "--deps')
-if (!config) throw new Error('missing cli argument "--config')
+if (!dest) dest = 'viz/'
+if (!deps) deps = './lavamoat/lavamoat-config-debug.json'
+if (!config) config = './lavamoat/lavamoat-config.json'
 
 const source = path.join(__dirname, '/../dist/')
 
 main().catch(err => console.error(err))
+
+function parseArgs() {
+  const argsParser = yargs
+    .command('sesify-viz [config] [debug config] [dest]', 'generate topological visualization for dep graph', (yargs) => {
+      // the path for the config file
+      yargs.option('config', {
+        alias: 'configPath',
+        describe: 'the path for the config file',
+        type: 'string',
+        default: './lavamoat/lavamoat-config.json'
+      })
+      // the path for the debug-config file
+      yargs.option('deps', {
+        describe: 'the path for the debug-config file',
+        type: 'string',
+        default: './lavamoat/lavamoat-config-debug.json'
+      })
+      // path to write viz output
+      yargs.option('dest', {
+        describe: 'path to write viz output',
+        type: 'string',
+        default: 'viz/'
+      })
+      //serve the output dir
+      yargs.option('serve', {
+        describe: 'serve the visualization',
+        type: 'boolean'
+      })
+    })
+    .help()
+
+  const parsedArgs = argsParser.parse()
+  console.log(parsedArgs)
+  return parsedArgs
+}
 
 async function main () {
   console.log(`"${source}", "${dest}"`)
@@ -29,5 +65,8 @@ async function main () {
   self.DEPS = ${depsContent};
   `
   await fs.writeFile(dest + '/data-injection.js', dataInjectionContent)
+  if (serve) {
+    open(`file:///${path.dirname(require.main.path)}/viz/index.html`)
+  }
   console.log(`generated viz in ${dest}`)
 }
