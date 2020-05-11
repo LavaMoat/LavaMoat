fbe3a4cf80f84b25fc4da2b8fdfff7a6db33e008
kumavis <aaron@kumavis.me>
2020-05-11 14:11:31 +0800

    node/test/projects/2 - rewrite for keccak@3

--- packages/node/test/projects/2/index.js
+++ packages/node/test/projects/2/index.js
@@ -1,3 +1,12 @@
+// this is an integration test
+// focusing on TypedArrays
+// between native Buffer and keccak's native modules
+// and also coincidently keccak's classes inheriting
+// from core stream's function class Transform that uses
+// an instanceof check for flexible use of the "new" keyword
+// that can accidently trigger a constructor return override
+
+const { Buffer } = require('buffer')
 const createKeccakHash = require('keccak')
 
 const result = getHash('abcdef1234567890')
