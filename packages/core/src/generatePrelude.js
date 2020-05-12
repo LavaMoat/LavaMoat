aa4d4b68e076885a5504bae31d00b033ae992a36
kumavis <aaron@kumavis.me>
2020-05-12 10:31:35 +0800

    lint - fix

--- packages/core/src/generatePrelude.js
+++ packages/core/src/generatePrelude.js
@@ -81,6 +81,6 @@ ${src}
 
 // String.prototype.replace has special behavior for some characters, so we use split join instead
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Specifying_a_string_as_a_parameter
-function stringReplace(src, target, replacement) {
+function stringReplace (src, target, replacement) {
   return src.split(target).join(replacement)
 }
