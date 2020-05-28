1d15e4fc8bae505e4a728f380cafe45fb0967ea2
kumavis <aaron@kumavis.me>
2020-05-29 01:33:00 +0800

    tofu/parse - export acorn-globals.parse as a convenience

--- packages/tofu/src/index.js
+++ packages/tofu/src/index.js
@@ -1,8 +1,10 @@
+const { parse } = require('acorn-globals')
 const { inspectGlobals, inspectImports } = require('./inspectSource')
 const utils = require('./util')
 
 module.exports = {
   inspectGlobals,
   inspectImports,
-  utils
+  utils,
+  parse,
 }
