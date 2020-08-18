1faf7485deb63ab2a6bc45ef12be988e66a1917f
kumavis <aaron@kumavis.me>
2020-08-18 13:58:27 +0800

    node/importHook - breakup importHook by module type

--- packages/node/src/parseForConfig.js
+++ packages/node/src/parseForConfig.js
@@ -12,7 +12,7 @@ async function parseForConfig ({ cwd, entryId, resolutions, rootPackageName, sho
   const resolveHook = makeResolveHook({ cwd, resolutions, rootPackageName })
   const importHook = makeImportHook({ rootPackageName, shouldResolve, isBuiltin, resolveHook })
   const moduleSpecifier = resolveHook(entryId, `${cwd}/package.json`)
-  return coreParseForConfig({ moduleSpecifier, resolveHook, importHook, isBuiltin, shouldResolve, ...args })
+  return coreParseForConfig({ moduleSpecifier, importHook, isBuiltin, ...args })
 }
 
 function makeResolveHook ({ cwd, resolutions = {}, rootPackageName = '<root>' }) {
@@ -49,55 +49,90 @@ function makeImportHook ({
   rootPackageName = '<root>'
 }) {
   return async (specifier) => {
-    let file; let type; let packageName; let packageVersion; let content; let importMap = {}; let ast
-    // get package name
+    // see if its a builtin
     if (isBuiltin(specifier)) {
-      type = 'builtin'
-      packageName = specifier
-    } else {
-      type = 'js'
-      file = specifier
-      const packageData = packageDataForModule({ id: specifier, file }, rootPackageName)
-      packageName = packageData.packageName
-      packageVersion = packageData.packageVersion
-      // load src
-      content = await fs.readFile(file, 'utf8')
-      // parse
-      const extension = path.extname(file)
-      if (extension === '.js') {
-        ast = parseModule(content, specifier)
-        // get imports
-        const { cjsImports } = inspectImports(ast, null, false)
-        // build import map
-        importMap = Object.fromEntries(cjsImports.map(requestedName => {
-          let depValue
-          if (shouldResolve(requestedName, specifier)) {
-            // try {
-            depValue = resolveHook(requestedName, specifier)
-            // } catch (err) {
-            //   // graceful failure
-            //   console.warn(`lavamoat-node/makeImportHandler - could not resolve "${requestedName}" from "${specifier}"`)
-            //   depValue = requestedName
-            // }
-          } else {
-            // resolving is skipped so put in a dummy value
-            depValue = requestedName
-          }
-          return [requestedName, depValue]
-        }))
-      } else {
-        if (!['.css', '.sass', '.coffee', '.json', '.node'].includes(extension)) {
-          console.warn(`node importHook - ignored unknown extension "${extension}"`)
-        }
-      }
+      return makeBuiltinModuleRecord(specifier)
     }
+
+    // assume specifier is filename
+    const filename = specifier
+    const extension = path.extname(filename)
+    const packageData = packageDataForModule({ id: specifier, file: filename }, rootPackageName)
+
+    if (extension === '.node') {
+      return makeNativeModuleRecord(specifier, filename, packageData)
+    }
+    if (extension === '.js') {
+      return makeJsModuleRecord(specifier, filename, packageData)
+    }
+    // warn if not known to be skippable
+    if (!['.css', '.sass'].includes(extension)) {
+      console.warn(`node importHook - ignored unknown extension "${extension}"`)
+    }
+    // TODO: throw an error here, if ignore flag is not set
+  }
+
+  function makeBuiltinModuleRecord (specifier) {
     return new LavamoatModuleRecord({
+      type: 'builtin',
       specifier,
-      file,
+      file: `builtin/${specifier}`,
+      packageName: specifier,
+      packageVersion: specifier,
+      // special module initializer that directly accesses node's require
+      moduleInitializer: (moduleExportsWrapper) => {
+        moduleExportsWrapper.exports = require(specifier)
+      }
+    })
+  }
+
+  function makeNativeModuleRecord (specifier, filename, packageData) {
+    const { packageName, packageVersion } = packageData
+    return new LavamoatModuleRecord({
+      type: 'native',
+      specifier,
+      file: filename,
+      packageName,
+      packageVersion,
+      // special module initializer that directly accesses node's require
+      moduleInitializer: (moduleExportsWrapper) => {
+        moduleExportsWrapper.exports = require(specifier)
+      }
+    })
+  }
+
+  async function makeJsModuleRecord (specifier, filename, packageData) {
+    const { packageName, packageVersion } = packageData
+    // load src
+    const content = await fs.readFile(filename, 'utf8')
+    // parse
+    const ast = parseModule(content, specifier)
+    // get imports
+    const { cjsImports } = inspectImports(ast, null, false)
+    // build import map
+    const importMap = Object.fromEntries(cjsImports.map(requestedName => {
+      let depValue
+      if (shouldResolve(requestedName, specifier)) {
+        // try {
+        depValue = resolveHook(requestedName, specifier)
+        // } catch (err) {
+        //   // graceful failure
+        //   console.warn(`lavamoat-node/makeImportHandler - could not resolve "${requestedName}" from "${specifier}"`)
+        //   depValue = requestedName
+        // }
+      } else {
+        // resolving is skipped so put in a dummy value
+        depValue = requestedName
+      }
+      return [requestedName, depValue]
+    }))
+    return new LavamoatModuleRecord({
+      type: 'js',
+      specifier,
+      file: filename,
       packageName,
       packageVersion,
       content,
-      type,
       importMap,
       ast
     })
