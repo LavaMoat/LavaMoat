dc89434d9cd927deafd2b15fc7364ca9550a18f5
kumavis <aaron@kumavis.me>
2020-09-08 17:33:57 +0800

    survey/execTest - start of execTest utility for running git repo tests under lavamoat

---
+++ packages/survey/src/execTest.js
@@ -0,0 +1,65 @@
+const { promisify } = require('util')
+const path = require('path')
+const os = require('os')
+const { promises: fs } = require('fs')
+const { spawn, exec: execCb } = require('child_process')
+
+
+const exec = promisify(execCb)
+const makeTempDir = async () => { return await fs.mkdtemp(path.join(os.tmpdir(), 'lavamoat-survey-')) }
+const mitmPath = new URL("../mitm", `file://${__filename}`).pathname;
+
+module.exports = {
+  execTest
+}
+
+execTest({
+  gitRepo: 'git@github.com:MetaMask/eth-sig-util.git',
+}).catch(console)
+
+// 1. clone + cd
+// 2. npm/yarn install
+// 3. npm run test (no lavamoat)
+// 4. run tests (lavamoat gen-and-run)
+async function execTest ({ gitRepo, gitRef }) {
+  const projectDir = await makeTempDir()
+  console.log(`running lavamoat exec test`)
+  console.log(`${gitRepo} #${gitRef ? gitRef : '<default>'} in ${projectDir}`)
+  await prepareRepo({ projectDir, gitRepo, gitRef })
+  await installDependencies({ projectDir })
+  await runPlainTests({ projectDir })
+  await runLavamoatTests({ projectDir })
+}
+
+async function prepareRepo ({ projectDir, gitRepo, gitRef }) {
+  await exec(`git clone ${gitRepo} .`, { cwd: projectDir })
+  if (gitRef) await exec(`git checkout ${gitRef}`, { cwd: projectDir })
+  console.log(`repo prepared ${projectDir}`)
+}
+
+async function installDependencies ({ projectDir }) {
+  const { stdout, stderr } = await exec(`yarn install`, { cwd: projectDir })
+  console.log(`deps installed`)
+}
+
+async function runPlainTests ({ projectDir }) {
+  const { stdout, stderr } = await exec(`yarn run test`, { cwd: projectDir })
+  console.log(`tests passed directly`)
+}
+
+async function runLavamoatTests ({ projectDir }) {
+  // yarn modifies the path, injecting a `node` ref so we avoid it
+  // npx is whitelisted in the node mitm script
+  const testCommand = require(`${projectDir}/package.json`).scripts.test
+  await mitmExec('npx', ['-c', testCommand], { cwd: projectDir })
+}
+
+async function mitmExec(arg, args, opts) {
+  const { env } = process
+  const child = spawn(arg, args, {
+    env: { ...env, PATH: `${mitmPath}:${env.PATH}` },
+    stdio: 'inherit',
+    ...opts
+  });
+  return new Promise(resolve => child.on('exit', resolve));
+}
---
+++ packages/survey/src/prepareHook.js
@@ -0,0 +1,42 @@
+// This postinstall hook creates mitm/node.
+// This attempts to replace all calls to node with lavamoat, provided
+// that the mitm directory is appears before Node.js's bin directory on the
+// environment PATH.
+// this has been borrowed and modified from https://github.com/agoric/ses-shim
+
+const fs = require('fs');
+const path = require('path');
+
+const node = process.env.NVM_BIN ? `${process.env.NVM_BIN}/node` : process.argv[0];
+const npxPath = path.resolve(node, '../npx')
+const lavamoat = require.resolve('lavamoat/src/index.js')
+const lockdown = new URL('../src/lockdown.cjs', `file://${__filename}`).pathname;
+const mitm = new URL('../mitm/node', `file://${__filename}`).pathname;
+
+const script = `#!/bin/bash
+set -xueo pipefail
+
+npxpath="${npxPath}"
+eval yarnpath="~/.yarn/bin/yarn.js"
+
+if [ "$1" = "$yarnpath" ]; then
+    echo "its yarn, running unwrapped"
+    ${node} "$@";
+elif [ "$1" = "$npxpath" ]; then
+  echo "its npx, running unwrapped"
+  ${node} "$@";
+else
+  echo "unknown, running with lavamoat"
+  ${node} ${lavamoat} --writeAutoConfigAndRun "$@"
+fi
+`;
+
+/*
+  /home/xyz/.nvm/versions/node/v12.16.1/bin/node \
+  /home/xyz/Development/lavamoat/packages/survey/node_modules/lavamoat/src/index.js \
+  --writeAutoConfigAndRun \
+  /home/xyz/.yarn/bin/yarn.js 'run test'
+*/
+
+fs.writeFileSync(mitm, script, 'utf-8');
+fs.chmodSync(mitm, 0o755);
