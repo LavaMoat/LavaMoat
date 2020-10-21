cf3a2e9d23743423a04ce1701af0b56eb554a677
kumavis <aaron@kumavis.me>
2020-10-21 08:47:09 +0800

    meta - rename package to perf

--- packages/perf/trials/wallet/index.js
+++ packages/perf/trials/wallet/index.js
@@ -7,19 +7,22 @@ const tasks = {
   'node': {
     run: 'node entry.js',
   },
-  'bify': {
-    prep: 'yarn build:unsafe',
-    run: 'node bundle.js',
+  'lavamoat-node': {
+    run: 'lavamoat entry.js',
   },
+  // 'bify': {
+  //   prep: 'yarn build:unsafe',
+  //   run: 'node bundle.js',
+  // },
   // ses rejects some eval?
   // 'bify+ses': {
   //   // reuse previous build
   //   run: `node -p "require('ses').makeSESRootRealm().evaluate(require('fs').readFileSync('./bundle.js','utf8'), { global })"`,
   // },
-  'bify+lavamoat': {
-    prep: 'yarn build',
-    run: 'node bundle.js',
-  },
+  // 'bify+lavamoat': {
+  //   prep: 'yarn build',
+  //   run: 'node bundle.js',
+  // },
   // 'bify+lavamoat w/ harden': {
   //   prep: 'yarn build:harden',
   //   run: 'node bundle.js',
