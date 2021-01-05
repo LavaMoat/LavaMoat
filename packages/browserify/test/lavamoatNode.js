593290025bf93abebf406071d3e07c641eef3073
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-01-05 14:12:38 +0800

    fix directory for fixture

--- packages/browserify/test/lavamoatNode.js
+++ packages/browserify/test/lavamoatNode.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process');
 const path = require('path')
 
 test('basic - bundle works under lavamoat node', async (t) => {
-  const bundle = execSync("lavamoat ../../../test/fixtures/runBrowserifyWithEntry.js --applyExportsDefense false",
+  const bundle = execSync("lavamoat ../../test/fixtures/runBrowserifyWithEntry.js --applyExportsDefense false",
   {
     cwd: path.resolve(__dirname, '../examples/02-secure-runtime'),
     maxBuffer: 8192 * 10000
