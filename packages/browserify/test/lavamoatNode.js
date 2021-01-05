66a69769eb12177af9d6441413501b116febc7f4
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-01-05 11:10:28 +0800

    turn off debug mode for lavamoat node

--- packages/browserify/test/lavamoatNode.js
+++ packages/browserify/test/lavamoatNode.js
@@ -3,6 +3,6 @@ const { execSync } = require('child_process');
 const path = require('path')
 
 test('basic - bundle works under lavamoat node', async (t) => {
-  const bundle = execSync("lavamoat ../../../test/fixtures/runBrowserifyWithEntry.js --debugMode --applyExportsDefense false", { cwd: path.resolve(__dirname, '../examples/01-simple-js/lavamoat-build'), maxBuffer: 8192 * 10000 })
+  const bundle = execSync("lavamoat ../../../test/fixtures/runBrowserifyWithEntry.js --applyExportsDefense false", { cwd: path.resolve(__dirname, '../examples/01-simple-js/lavamoat-build'), maxBuffer: 8192 * 10000 })
   t.assert(bundle, "Bundle exists")
 })
