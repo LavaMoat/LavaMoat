82ae7c621819f8a6d22084462e08289eda7d33e0
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-01-26 13:00:26 +0800

    add tests for arguments

--- packages/core/test/scenarios/index.js
+++ packages/core/test/scenarios/index.js
@@ -4,9 +4,10 @@ const basic = require('./basic')
 const config = require('./config')
 const endowments = require('./endowments')
 const exportsDefense = require('./exportsDefense')
+const arguments = require('./arguments')
 
 module.exports = { loadScenarios }
-const scenarios = [...autogen, ...security, ...basic, ...config, ...endowments, ...exportsDefense]
+const scenarios = [...autogen, ...security, ...basic, ...config, ...endowments, ...exportsDefense, ...arguments]
 
 async function * loadScenarios () {
   for (const scenarioCreator of scenarios) {
