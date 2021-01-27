21345073c89b254aaeef8f404f870206c7c8c108
kumavis <aaron@kumavis.me>
2021-01-27 21:42:49 +0800

    viz/cli - (wip) accept multiple policies

--- packages/viz/bin/index.js
+++ packages/viz/bin/index.js
@@ -8,7 +8,9 @@ const yargs = require('yargs')
 const { ncp } = require('ncp')
 const pify = require('pify')
 const openUrl = require('open')
-const { mergeConfig } = require('lavamoat-core')
+const { mergeConfig, getDefaultPaths } = require('lavamoat-core')
+
+const defaultPaths = getDefaultPaths('node')
 
 main().catch((err) => console.error(err))
 
@@ -21,25 +23,17 @@ function parseArgs () {
         type: 'string',
         default: './viz/'
       })
-      // the path for the debug-config file
-      yargs.option('debugConfig', {
-        describe: 'the path for the debug-config file',
+      // the directory containing the individual policy directories
+      yargs.option('policiesDir', {
+        describe: 'the directory containing the individual policy directories',
         type: 'string',
-        default: './lavamoat-config-debug.json'
+        default: defaultPaths.policiesDir
       })
-      // the path for the config file
-      yargs.option('config', {
-        alias: 'configPath',
-        describe: 'the path for the config file',
-        type: 'string',
-        default: './lavamoat-config.json'
-      })
-      // the path for the config override file
-      yargs.option('configOverride', {
-        alias: 'configOverridePath',
-        describe: 'the path for the config override file',
-        type: 'string',
-        default: './lavamoat-config-override.json'
+      // the name of the policies to include in the dashboard under the policies directory. default: all
+      yargs.option('policyNames', {
+        describe: 'the name of the policies to include in the dashboard under the policies directory. default: all',
+        type: 'array',
+        default: []
       })
       // open the output dir
       yargs.option('open', {
@@ -55,22 +49,68 @@ function parseArgs () {
 }
 
 async function main () {
-  const { debugConfig, config, configOverride, dest, open } = parseArgs()
+  let { dest, open, policiesDir, policyNames } = parseArgs()
   const fullDest = path.resolve(dest)
   const source = path.join(__dirname, '/../dist/')
   // copy app dir
   await fs.mkdir(fullDest, { recursive: true })
   await pify((cb) => ncp(source, fullDest, cb))()
+  // load policy names if not specified
+  if (!policyNames.length) {
+    policyNames = await getDirectories(policiesDir)
+  }
+  // write each policy data injection
+  const policyDataInjectionFilePath = await Promise.all(policyNames.map(async (policyName) => {
+    return await addPolicyComprehension({ policyName, fullDest })
+  }))
   // add data-injection file
-  const debugConfigContent = await fs.readFile(debugConfig, 'utf8')
-  const configContent = await fs.readFile(config, 'utf8')
-  const configOverrideContent = await fs.readFile(configOverride, 'utf8')
-  const finalConfigString = JSON.stringify(mergeConfig(JSON.parse(configContent), JSON.parse(configOverrideContent)))
-  const dataInjectionContent = `globalThis.CONFIG_DEBUG = ${debugConfigContent}; globalThis.CONFIG = ${configContent}; globalThis.CONFIG_OVERRIDE = ${configOverrideContent}; globalThis.CONFIG_FINAL = ${finalConfigString};`
-
+  const dataInjectionContent = policyDataInjectionFilePath
+    // .map(filepath => { console.log(filepath, fullDest, path.relative(fullDest, filepath)); return filepath;})
+    .map(filepath => path.relative(fullDest, filepath))
+    .map(relPath => `import "./${relPath}";`)
+    .join('\n')
   await fs.writeFile(`${fullDest}/injectConfigDebugData.js`, dataInjectionContent)
+  // trigger opening of file
   if (open) {
     openUrl(`file:///${fullDest}/index.html`)
   }
+  // report done
   console.log(`generated viz in "${dest}"`)
+}
+
+async function addPolicyComprehension ({ policyName, fullDest }) {
+  const defaultPaths = getDefaultPaths(policyName)
+  const [
+    policyDebugContent,
+    policyPrimary,
+    policyOverride,
+  ] = await Promise.all([
+    fs.readFile(defaultPaths.debug, 'utf8'),
+    loadPolicyFile(defaultPaths.primary),
+    loadPolicyFile(defaultPaths.override),
+  ])
+  const mergedPolicy = mergeConfig(policyPrimary, policyOverride)
+  const policyDataInjectionContent = `
+  globalThis.CONFIG_DEBUG = ${policyDebugContent};
+  globalThis.CONFIG = ${JSON.stringify(policyPrimary)};
+  globalThis.CONFIG_OVERRIDE = ${JSON.stringify(policyOverride)};
+  globalThis.CONFIG_FINAL = ${JSON.stringify(mergedPolicy)};
+  `
+  // write to disk
+  const filepath = path.join(fullDest, `policy-${policyName}.js`)
+  await fs.writeFile(filepath, policyDataInjectionContent)
+  return filepath
+}
+
+async function loadPolicyFile (filepath) {
+  const content = await fs.readFile(filepath, 'utf8')
+  const policy = JSON.parse(content)
+  return policy
+}
+
+async function getDirectories (filepath) {
+  const dirEntries = await fs.readdir(filepath, { withFileTypes: true })
+  return dirEntries
+    .filter(entry => entry.isDirectory())
+    .map(dir => dir.name)
 }
