e409af6edf0e38a868e9aefd2e3057bd7de65f05
kumavis <aaron@kumavis.me>
2021-01-28 13:57:00 +0800

    node/clean - small cleanup

--- packages/node/src/index.js
+++ packages/node/src/index.js
@@ -39,16 +39,15 @@ async function runLava () {
     const config = await parseForConfig({ cwd, entryId, resolutions, includeDebugInfo })
     // write config debug file
     if (includeDebugInfo) {
-      const serializedConfig = jsonStringify(config, { space: 2 })
       fs.mkdirSync(path.dirname(configDebugPath), { recursive: true })
-      fs.writeFileSync(configDebugPath, serializedConfig)
+      fs.writeFileSync(configDebugPath, jsonStringify(config, { space: 2 }))
       console.log(`LavaMoat wrote config debug to "${configDebugPath}"`)
     }
-    // write config file
+    // cleanup debug info
     delete config.debugInfo
-    const serializedConfig = jsonStringify(config, { space: 2 })
+    // write config file
     fs.mkdirSync(path.dirname(configPath), { recursive: true })
-    fs.writeFileSync(configPath, serializedConfig)
+    fs.writeFileSync(configPath, jsonStringify(config, { space: 2 }))
     console.log(`LavaMoat wrote config to "${configPath}"`)
   }
   if (shouldRunApplication) {
