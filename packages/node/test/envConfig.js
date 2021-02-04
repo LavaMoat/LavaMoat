45f8265f1f1956e1e04fe6570d88183eacf5533c
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-02-04 12:48:36 +0800

    disable prototype mutation lint rule

--- packages/node/test/envConfig.js
+++ packages/node/test/envConfig.js
@@ -6,28 +6,29 @@ const { createScenarioFromScaffold } = require('lavamoat-core/test/util')
 
 test('envConfig - intrinsic protoytype mutating package running in unfrozen realm does not pollute other package intrinsics', async (t) => {
   const scenario = createScenarioFromScaffold({
-      defineOne: () => {
-        const two = require('two')
-        module.exports = {
-          one: Function.prototype.xyz,
-          two
-        }
-      },
-      defineTwo: () => {
-        Function.prototype.xyz = 'Hello'
-        module.exports = Function.prototype.xyz
-      },
-      expectedResult: {
-        two: 'Hello'
-      },
-      configOverride: {
-        resources: {
-          two: {
-            env: 'unfrozen'
-          }
+    defineOne: () => {
+      const two = require('two')
+      module.exports = {
+        one: Function.prototype.xyz,
+        two
+      }
+    },
+    defineTwo: () => {
+      /* eslint-disable-next-line */
+      Function.prototype.xyz = 'Hello'
+      module.exports = Function.prototype.xyz
+    },
+    expectedResult: {
+      two: 'Hello'
+    },
+    configOverride: {
+      resources: {
+        two: {
+          env: 'unfrozen'
         }
       }
-    })
+    }
+  })
   const testResult = await runScenario({ scenario })
   t.deepEqual(testResult, scenario.expectedResult)
 })
