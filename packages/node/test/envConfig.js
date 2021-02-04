35b48fac974a2286c90a7110db76563ad24abee8
kumavis <aaron@kumavis.me>
2021-02-04 16:12:23 +0800

    node/test - test that Realm of module.exports matches Compartment

--- packages/node/test/envConfig.js
+++ packages/node/test/envConfig.js
@@ -1,10 +1,8 @@
 const test = require('ava')
-
 const { runScenario } = require('./util')
-
 const { createScenarioFromScaffold } = require('lavamoat-core/test/util')
 
-test('envConfig - intrinsic protoytype mutating package running in unfrozen realm does not pollute other package intrinsics', async (t) => {
+test('envConfig - intrinsic prototype mutating package running in unfrozen realm does not pollute other package intrinsics', async (t) => {
   const scenario = createScenarioFromScaffold({
     defineOne: () => {
       const two = require('two')
@@ -20,6 +18,36 @@ test('envConfig - intrinsic protoytype mutating package running in unfrozen real
     },
     expectedResult: {
       two: 'Hello'
+    },
+    configOverride: {
+      resources: {
+        two: {
+          env: 'unfrozen'
+        }
+      }
+    }
+  })
+  const testResult = await runScenario({ scenario })
+  t.deepEqual(testResult, scenario.expectedResult)
+})
+
+test('envConfig - module.exports from the same Realm as its Compartment', async (t) => {
+  const scenario = createScenarioFromScaffold({
+    defineOne: () => {
+      const two = require('two')
+      module.exports = {
+        one: module.exports instanceof Object,
+        two,
+        cross: two instanceof Object
+      }
+    },
+    defineTwo: () => {
+      module.exports = module.exports instanceof Object
+    },
+    expectedResult: {
+      one: true,
+      two: true,
+      cross: false
     },
     configOverride: {
       resources: {
