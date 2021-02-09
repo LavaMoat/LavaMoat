1971b78660cd0ef58bde75a4fe77b0f97047b5f6
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-02-09 11:52:52 +0800

    put back createPolicyDataFile

--- bin/index.js
+++ bin/index.js
@@ -107,9 +107,9 @@ async function generateViz (args) {
     policyNames = await getDirectories(policiesDir)
   }
   // write each policy data injection
-  // const policyDataInjectionFilePath = await Promise.all(policyNames.map(async (policyName) => {
-  //   return await createPolicyDataFile({ policyName, fullDest })
-  // }))
+  await Promise.all(policyNames.map(async (policyName) => {
+    return await createPolicyDataFile({ policyName, fullDest })
+  }))
   // // add data-injection file
   // const dataInjectionContent = policyDataInjectionFilePath
   //   // .map(filepath => { console.log(filepath, fullDest, path.relative(fullDest, filepath)); return filepath;})
