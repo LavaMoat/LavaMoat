f2fbadfb8bdf8249b2bff50bb6288aa6a1cb90e0
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-02-11 20:40:01 +0800

    convert globalref to scenario

--- packages/browserify/test/globalRef.js
+++ packages/browserify/test/globalRef.js
@@ -1,106 +1,12 @@
 const test = require('ava')
 const {
-  runSimpleOneTwo,
   runScenario
 } = require('./util')
 const { createScenarioFromScaffold } = require('lavamoat-core/test/util')
 
-
-test('globalRef - check default containment', async (t) => {
-  function defineOne () {
-    let objCheckThis, objCheckSelf, objCheckGlobal, thisIsExports
-    const isUndefined = {}
-
-    try { objCheckThis = this.Object === Object } catch (_) { }
-    try { objCheckSelf = self.Object === Object } catch (_) { }
-    try { objCheckGlobal = global.Object === Object } catch (_) { }
-    try { thisIsExports = exports === this } catch (_) { }
-
-    isUndefined.global = typeof global === "undefined"
-    isUndefined.self = typeof self === "undefined"
-    isUndefined.window = typeof window === "undefined"
-
-    module.exports = { objCheckThis, objCheckSelf, objCheckGlobal, thisIsExports, isUndefined }
-  }
-
-  const result = await runSimpleOneTwo({ defineOne })
-
-  // this is how it behaves in browser via browserify
-  t.deepEqual(result, {
-    objCheckThis: false,
-    objCheckSelf: true,
-    objCheckGlobal: true,
-    thisIsExports: true,
-    isUndefined: {
-      global: false,
-      self: false,
-      window: false
-    }
-  }, 'test result matches expected')
-
-  // // this is how it behaves in node (browserify or not)
-  // t.deepEqual(result, {
-  //   objCheckThis: false,
-  //   objCheckSelf: undefined,
-  //   objCheckGlobal: true,
-  //   thisIsExports: true,
-  // }, 'test result matches expected')
-
-  // // this is how it behaves in node (only toplevel in REPL)
-  // t.deepEqual(result, {
-  //   objCheckThis: true,
-  //   objCheckSelf: undefined,
-  //   objCheckGlobal: true,
-  //   thisIsExports: false,
-  // }, 'test result matches expected')
-})
-
-test('globalRef - ensure endowments are accessible on globals', async (t) => {
-  function defineOne () {
-    let checkSelf, checkThis, checkWindow, checkGlobal, contextHasPostMessage, selfHasPostMessage
-
-    contextHasPostMessage = typeof postMessage !== 'undefined'
-    selfHasPostMessage = !!self.postMessage
-    try { checkThis = this.postMessage === postMessage } catch (err) { checkThis = err.message }
-    try { checkSelf = self.postMessage === postMessage } catch (err) { checkSelf = err.message }
-    try { checkWindow = window.postMessage === postMessage } catch (err) { checkWindow = err.message }
-    try { checkGlobal = global.postMessage === postMessage } catch (err) { checkGlobal = err.message }
-
-    module.exports = { checkThis, checkSelf, checkWindow, checkGlobal, contextHasPostMessage, selfHasPostMessage }
-  }
-
-  const config = {
-    resources: {
-      '<root>': {
-        packages: {
-          one: true
-        }
-      },
-      one: {
-        globals: {
-          postMessage: true
-        }
-      }
-    }
-  }
-
-  const testGlobal = { postMessage: () => { throw new Error('this should never be called') } }
-  const result = await runSimpleOneTwo({ defineOne, config, testGlobal })
-
-  // this is how it behaves in browser via browserify
-  t.deepEqual(result, {
-    // "this" is module.exports
-    checkThis: false,
-    checkSelf: true,
-    checkWindow: true,
-    checkGlobal: true,
-    contextHasPostMessage: true,
-    selfHasPostMessage: true
-  }, 'test result matches expected')
-})
-
 test('globalRef - has only the expected global circular refs', async (t) => {
   const scenario = createScenarioFromScaffold({
+    name: 'globalRef - has only the expected global circular refs',
     defineOne: () => {
       const circularKeys = Reflect.ownKeys(globalThis)
         .filter(key => {
@@ -118,5 +24,71 @@ test('globalRef - has only the expected global circular refs', async (t) => {
   })
   const testResult = await runScenario({ scenario })
   t.is(Array.isArray(testResult), true)
-  t.deepEqual(testResult.sort(), scenario.expectedResult.sort())
+  scenario.expectedResult.sort()
+  scenario.checkResult(t, testResult.sort(), scenario)
+})
+
+test('globalRef - globalRef - check default containment', async (t) => {
+  const scenario = createScenarioFromScaffold({
+    name: 'globalRef - check default containment',
+    defineOne: () => {
+      const testResults = {}
+      try { testResults.objCheckThis = this.Object === Object } catch (_) { }
+      try { testResults.objCheckSelf = self.Object === Object } catch (_) { }
+      try { testResults.objCheckGlobal = global.Object === Object } catch (_) { }
+      try { testResults.thisIsExports = exports === this } catch (_) { }
+      module.exports = testResults
+    },
+    expectedResult: {
+      objCheckThis: false,
+      objCheckSelf: true,
+      objCheckGlobal: true,
+      thisIsExports: true,
+    }
+  })
+  const testResult = await runScenario({ scenario })
+  scenario.checkResult(t, testResult, scenario)
+})
+test('globalRef - ensure endowments are accessible on globals', async (t) => {
+  const scenario = createScenarioFromScaffold({
+    name: 'globalRef - ensure endowments are accessible on globals',
+    defineOne: () => {
+      const testResults = {}
+      testResults.contextHasPostMessage = typeof postMessage !== 'undefined'
+      testResults.selfHasPostMessage = !!self.postMessage
+      try { testResults.checkThis = this.postMessage === postMessage } catch (err) { checkThis = err.message }
+      try { testResults.checkSelf = self.postMessage === postMessage } catch (err) { checkSelf = err.message }
+      try { testResults.checkWindow = window.postMessage === postMessage } catch (err) { checkWindow = err.message }
+      try { testResults.checkGlobal = global.postMessage === postMessage } catch (err) { checkGlobal = err.message }
+      module.exports = testResults
+    },
+    config: {
+      resources: {
+        '<root>': {
+          packages: {
+            one: true
+          }
+        },
+        one: {
+          globals: {
+            postMessage: true
+          }
+        }
+      }
+    },
+    expectedResult: {
+      // "this" is module.exports
+      checkThis: false,
+      checkSelf: true,
+      checkWindow: true,
+      checkGlobal: true,
+      contextHasPostMessage: true,
+      selfHasPostMessage: true
+    },
+    context: {
+      postMessage: () => { throw new Error('this should never be called') }
+    }
+  })
+  const testResult = await runScenario({ scenario })
+  scenario.checkResult(t, testResult, scenario)
 })
--- packages/node/test/globals.js
+++ packages/node/test/globals.js
@@ -19,5 +19,27 @@ test('globals - has only the expected global circular refs', async (t) => {
   })
   const testResult = await runScenario({ scenario })
   t.is(Array.isArray(testResult), true)
-  t.deepEqual(testResult.sort(), scenario.expectedResult.sort())
+  scenario.expectedResult.sort()
+  scenario.checkResult(t, testResult.sort(), scenario)
+})
+
+test('globalRef - globalRef - check default containment', async (t) => {
+  const scenario = createScenarioFromScaffold({
+    name: 'globalRef - check default containment',
+    defineOne: () => {
+      const testResults = {}
+      try { testResults.objCheckThis = this.Object === Object } catch (_) { }
+      try { testResults.objCheckSelf = self.Object === Object } catch (_) { }
+      try { testResults.objCheckGlobal = global.Object === Object } catch (_) { }
+      try { testResults.thisIsExports = exports === this } catch (_) { }
+      module.exports = testResults
+    },
+    expectedResult: {
+      objCheckThis: false,
+      objCheckGlobal: true,
+      thisIsExports: true,
+    }
+  })
+  const testResult = await runScenario({ scenario })
+  scenario.checkResult(t, testResult, scenario)
 })
