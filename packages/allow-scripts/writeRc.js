c645f8b02dfc54ff3874c5a7be084867136e9e30
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-02-26 15:31:01 +0800

    add npm support

--- packages/allow-scripts/writeRc.js
+++ packages/allow-scripts/writeRc.js
@@ -3,29 +3,38 @@ const { promisify } = require('util')
 
 const RCENTRY = 'ignore-scripts true'
 const isYarn = existsSync('./yarn.lock')
-const rcExists = existsSync('./.yarnrc')
+const isNpm = existsSync('./package-lock.json')
+const yarnRcExists = existsSync('./.yarnrc')
+const npmRcExists = existsSync('./.npmrc')
 
 const readFilePromise = promisify(readFile)
+let rcFile
+if (isYarn) {
+  rcFile = './.yarnrc'
+} else if (isNpm) {
+  rcFile = './.npmrc'
+} else {
+  rcFile = './.yarnrc'
+}
+
 
 async function writeRcFile () {
-  if (isYarn) {
-    if (!rcExists) {
-      writeFileSync('./.yarnrc', RCENTRY)
-      console.log('@lavamoat/allow-scripts: created .yarnrc file with entry: ignore-scripts true. Run \'yarn add @lavamoat/preinstall-always-fail\' to make sure this configuration is correct.')
+  if (!yarnRcExists && !npmRcExists) {
+    writeFileSync(rcFile, RCENTRY)
+    console.log('@lavamoat/allow-scripts: created .yarnrc file with entry: ignore-scripts true. Run \'yarn add @lavamoat/preinstall-always-fail\' to make sure this configuration is correct.')
+    return
+  } else {
+    const rcFileContents = await readFilePromise(rcFile, 'utf8')
+    if (rcFileContents.includes(RCENTRY)) {
       return
-    } else {
-      const rcFileContents = await readFilePromise('./.yarnrc', 'utf8')
-      if (rcFileContents.includes(RCENTRY)) {
-        return
-      }
-      const rcStream = createWriteStream('./.yarnrc', { flags:'a' });
-      if (rcFileContents.length) {
-        rcStream.write('\n' + RCENTRY)
-      } else {
-        rcStream.write(RCENTRY)
-      }
-      console.log('@lavamoat/allow-scripts: added entry to .yarnrc: ignore-scripts true. Run \'yarn add @lavamoat/preinstall-always-fail\' to make sure this configuration is correct.')
     }
+    const rcStream = createWriteStream(rcFile, { flags:'a' });
+    if (rcFileContents.length) {
+      rcStream.write('\n' + RCENTRY)
+    } else {
+      rcStream.write(RCENTRY)
+    }
+    console.log('@lavamoat/allow-scripts: added entry to .yarnrc: ignore-scripts true. Run \'yarn add @lavamoat/preinstall-always-fail\' to make sure this configuration is correct.')
   }
 }
 
