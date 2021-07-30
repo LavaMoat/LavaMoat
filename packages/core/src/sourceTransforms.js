245e400fc012f3e26b57c104266ad2a74dd81705
kumavis <aaron@kumavis.me>
2021-07-30 13:25:50 -0700

    core/sourceTransforms - transform eval to indirect eval

--- packages/core/src/sourceTransforms.js
+++ packages/core/src/sourceTransforms.js
@@ -12,7 +12,7 @@ function evadeDirectEvalExpressions (source) {
   /* eslint-disable-next-line prefer-regex-literals */
   const someDirectEvalPattern = new RegExp('\\beval(\\s*\\()', 'g')
 
-  const replaceFn = (_, p1) => `(eval)${p1}`
+  const replaceFn = (_, p1) => `(0,eval)${p1}`
   return source.replace(someDirectEvalPattern, replaceFn)
 }
 
