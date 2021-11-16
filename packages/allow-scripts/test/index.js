523f3d78e933006e917461de8ceeb3aa97ff9aca
Bento Box <bentobox19@protonmail.com>
2021-11-16 12:40:58 -0300

    Add Test: cli - auto command

--- packages/allow-scripts/test/index.js
+++ packages/allow-scripts/test/index.js
@@ -1,7 +1,30 @@
 const test = require('ava')
+const fs = require('fs')
+const path = require('path')
+const { spawnSync } = require('child_process')
 
 test('cli - auto command', async (t) => {
-  /*
+  // set up the directories
+  let allowScriptsSrcRoot = path.join(__dirname, '..', 'src')
+  let projectRoot = `${__dirname}/projects/1`
 
-  */
+  // delete any existing package.json
+  fs.unlink(path.join(projectRoot, 'package.json'), err => {
+    if (err && err.code !== 'ENOENT') {
+      throw err
+    }
+  })
+
+  // npm init -y
+  spawnSync('npm', ['init', '-y'], {cwd: projectRoot})
+
+  // run the auto command
+  let cmd = path.join(allowScriptsSrcRoot, 'cli.js')
+  let result = spawnSync(cmd, ['auto'], {cwd: projectRoot})
+
+  // get the package.json
+  const packageJsonContents = JSON.parse(fs.readFileSync(path.join(projectRoot, 'package.json'), 'utf8'))
+
+  // assert its contents
+  t.deepEqual(packageJsonContents.lavamoat, {allowScripts: {'bbb/evil_dep': false}})
 })
