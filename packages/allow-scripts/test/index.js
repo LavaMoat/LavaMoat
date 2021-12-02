4adc882ac827fc9e04303021271b18ba5fc250b7
Bento Box <bentobox19@protonmail.com>
2021-12-01 22:46:30 -0300

    Fix tests

--- packages/allow-scripts/test/index.js
+++ packages/allow-scripts/test/index.js
@@ -26,7 +26,7 @@ test('cli - auto command', async (t) => {
   const packageJsonContents = JSON.parse(fs.readFileSync(path.join(projectRoot, 'package.json'), 'utf8'))
 
   // assert its contents
-  t.deepEqual(packageJsonContents.lavamoat, {allowScripts: {'bbb/evil_dep': false}})
+  t.deepEqual(packageJsonContents.lavamoat, {allowScripts: {'bbb>evil_dep': false}})
 })
 
 test('cli - run command - good dep at the root', async (t) => {
@@ -55,6 +55,7 @@ test('cli - run command - good dep at the root', async (t) => {
   // "preinstall": "touch /tmp/$( date '+%Y-%m-%d_%H-%M-%S' )"
 })
 
+
 test('cli - run command - good dep as a sub dep', async (t) => {
   // set up the directories
   let allowScriptsSrcRoot = path.join(__dirname, '..', 'src')
@@ -67,7 +68,7 @@ test('cli - run command - good dep as a sub dep', async (t) => {
   // assert the output
   t.deepEqual(result.stdout.toString().split('\n'), [
     'running lifecycle scripts for event \"preinstall\"',
-    '- bbb/good_dep',
+    '- bbb>good_dep',
     'running lifecycle scripts for event \"install\"',
     'running lifecycle scripts for event \"postinstall\"',
     'running lifecycle scripts for top level package',
