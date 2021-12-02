59ab0af1b6aeaec0d29a4d60c9bbaf3b9e2f37c3
kumavis <aaron@kumavis.me>
2021-12-02 12:06:26 -1000

    allow-scripts - use @lavamoat/aa

--- packages/allow-scripts/src/index.js
+++ packages/allow-scripts/src/index.js
@@ -1,30 +1,7 @@
 const { promises: fs } = require('fs')
 const path = require('path')
 const npmRunScript = require('@npmcli/run-script')
-const { createRequire } = require('module')
-const resolve = require('resolve')
-const { promisify } = require('util')
-const resolveAsync = promisify(resolve)
-
-class SetMap {
-  constructor () {
-    this.map = new Map()
-  }
-  add (key, value) {
-    let set = this.map.get(key)
-    if (set === undefined) {
-      set = new Set()
-      this.map.set(key, set)
-    }
-    set.add(value)
-  }
-  get (key) {
-    return this.map.get(key)
-  }
-  entries() {
-    return this.map.entries()
-  }
-}
+const { loadCanonicalNameMap } = require('@lavamoat/aa')
 
 module.exports = {
   runAllowedPackages,
@@ -183,57 +160,10 @@ async function printPackagesList ({ rootDir }) {
   }
 }
 
-async function loadFilePathToShortestLogicalPath({ rootDir = process.cwd(), includeDevDeps } = {}) {
-  const filePathToLogicalPaths = new SetMap()
-  const filePathToShortestLogicalPath = new Map()
-  // walk tree
-  for await (const packageData of eachPackageInLogicalTree({ packageDir: rootDir, includeDevDeps })) {
-    const logicalPathString = packageData.logicalPath.join('>')
-    filePathToLogicalPaths.add(packageData.packageDir, logicalPathString)
-  }
-  // find shortest logical path
-  for (const [packageDir, logicalPathSet] of filePathToLogicalPaths.entries()) {
-    const shortestLogicalPathString = Array.from(logicalPathSet.values()).reduce((a,b) => a.length > b.length ? b : a)
-    filePathToShortestLogicalPath.set(packageDir, shortestLogicalPathString)
-  }
-  return filePathToShortestLogicalPath
-}
-
-async function * eachPackageInLogicalTree ({ packageDir, logicalPath = [], includeDevDeps = false, visited = new Set() }) {
-  const packageJsonPath = path.join(packageDir, 'package.json')
-  const rawPackageJson = await fs.readFile(packageJsonPath, 'utf8')
-  const packageJson = JSON.parse(rawPackageJson)
-  const depsToWalk = [
-    ...Object.keys(packageJson.dependencies || {}),
-    ...Object.keys(includeDevDeps ? packageJson.devDependencies || {} : {}),
-  ]
-  for (const depName of depsToWalk) {
-    const depRelativePackageJsonPath = path.join(depName, 'package.json')
-    let depPackageJsonPath
-    try {
-      // sync seems slightly faster
-      // depPackageJsonPath = await resolveAsync(depRelativePackageJsonPath, { basedir: packageJsonPath })
-      depPackageJsonPath = resolve.sync(depRelativePackageJsonPath, { basedir: packageJsonPath })
-    } catch (err) {
-      console.error(err)
-      continue
-    }
-    const depPath = path.dirname(depPackageJsonPath)
-    // avoid cycles
-    if (visited.has(depPath)) {
-      continue
-    }
-    visited.add(depPath)
-    const childLogicalPath = [...logicalPath, depName]
-    yield { packageDir: depPath, logicalPath: childLogicalPath }
-    yield* eachPackageInLogicalTree({ packageDir: depPath, logicalPath: childLogicalPath, includeDevDeps: false, visited })
-  }
-}
-
 async function loadAllPackageConfigurations ({ rootDir }) {
   const packagesWithLifecycleScripts = new Map()
 
-  const dependencyMap = await loadFilePathToShortestLogicalPath({ rootDir, includeDevDeps: true })
+  const dependencyMap = await loadCanonicalNameMap({ rootDir, includeDevDeps: true })
   const sortedDepEntries = Array.from(dependencyMap.entries()).sort(sortBy(([filePath, canonicalName]) => canonicalName))
   for (const [filePath, canonicalName] of sortedDepEntries) {
     // const canonicalName = getCanonicalNameForPath({ rootDir, filePath: filePath })
