c455cacd12e84c9fdfa24ae15ea4abd3580a4e55
kumavis <aaron@kumavis.me>
2022-02-14 16:05:11 -1000

    browserify/test - clean cache before installing deps

--- packages/browserify/test/util.js
+++ packages/browserify/test/util.js
@@ -124,6 +124,7 @@ async function prepareBrowserifyScenarioOnDisk ({ scenario }) {
     )
     runBrowserifyPath = `${__dirname}/fixtures/runBrowserifyBundleFactor.js`
   }
+  spawnSync('yarn', ['cache','clean'], { cwd: projectDir })
   const installDevDepsResult = spawnSync('yarn', ['add','-D', ...depsToInstall], { cwd: projectDir })
   if (installDevDepsResult.status !== 0) {
     const msg = `Error while installing browserify:\n${installDevDepsResult.stderr.toString()}`
