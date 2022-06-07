72da6a933827bedaae1ed7c7955e2fa5fac36bae
Mark Stacey <markjstacey@gmail.com>
2022-06-07 18:37:39 -0230

    Add Yarn v3 plugin for running `@lavamoat/allow-scripts` after install (#345)
    A new workspace has been added for a Yarn plugin that runs
    `allow-scripts` automatically after installation. This plugin is an
    alternative to the "yarn setup" strategy described in the
    `allow-scripts` README.

---
+++ packages/yarn-plugin-allow-scripts/sources/index.ts
@@ -0,0 +1,19 @@
+import {Plugin} from '@yarnpkg/core';
+import {execute} from '@yarnpkg/shell';
+
+const plugin: Plugin = {
+  hooks: {
+    afterAllInstalled: async () => {
+      const exitCode = await execute('yarn run allow-scripts');
+
+      if (exitCode !== 0) {
+        // We have to use `process.exit` here rather than setting `process.exitCode`
+        // because Yarn will override any exit code set in this hook.
+        // eslint-disable-next-line node/no-process-exit
+        process.exit(exitCode);
+      }
+    },
+  },
+};
+
+export default plugin;
