6f81eee46e128c633d7a9e8f2ffda0673b5316e2
Zbyszek Tenerowicz <naugtur@gmail.com>
2022-09-07 15:37:18 +0200

    feat: merge overrides into policy on each run, not just on autopolicy (#366)
    * feat: merge overrides into policy on each run, not just on autopolicy
    
    * extract loading policy with overrides into a core function

--- packages/node/src/index.js
+++ packages/node/src/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const fs = require('fs')
 const yargs = require('yargs')
 const jsonStringify = require('json-stable-stringify')
-const { loadPolicy, getDefaultPaths } = require('lavamoat-core')
+const { loadPolicy, loadPolicyAndApplyOverrides, getDefaultPaths } = require('lavamoat-core')
 const { loadCanonicalNameMap } = require('@lavamoat/aa')
 const { parseForPolicy } = require('./parseForPolicy')
 const { createKernel } = require('./kernel')
@@ -36,7 +36,7 @@ async function runLava () {
   if (shouldParseApplication) {
     // parse mode
     const includeDebugInfo = Boolean(writeAutoPolicyDebug)
-    const policyOverride= await loadPolicy({ debugMode, policyPath: policyOverridePath })
+    const policyOverride = await loadPolicy({ debugMode, policyPath: policyOverridePath })
     console.warn(`LavaMoat generating policy from entry "${entryId}"...`)
     const policy = await parseForPolicy({ projectRoot, entryId, policyOverride, includeDebugInfo })
     // write policy debug file
@@ -54,9 +54,8 @@ async function runLava () {
   }
   if (shouldRunApplication) {
     // execution mode
-    const lavamoatPolicy = await loadPolicy({ debugMode, policyPath })
+    const lavamoatPolicy = await loadPolicyAndApplyOverrides({ debugMode, policyPath, policyOverridePath })
     const canonicalNameMap = await loadCanonicalNameMap({ rootDir: projectRoot, includeDevDeps: true })
-      // process.exit(420)
     const kernel = createKernel({ projectRoot, lavamoatPolicy, canonicalNameMap, debugMode, statsMode })
     // patch process.argv so it matches the normal pattern
     // e.g. [runtime path, entrypoint, ...args]
