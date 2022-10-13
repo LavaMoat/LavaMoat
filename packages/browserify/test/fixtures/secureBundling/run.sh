e32d2659e0aca4de778aabd062c924b3b6305cd2
naugtur <naugtur@gmail.com>
2022-10-13 16:00:44 +0200

    test: fix the test setup to avoid confusing paths

--- packages/browserify/test/fixtures/secureBundling/run.sh
+++ packages/browserify/test/fixtures/secureBundling/run.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$WRITE_AUTO_POLICY" == "1" ]; then
-  ../../../../node/src/cli.js build.js --projectRoot '../../../' --writeAutoPolicy
+  ../node/src/cli.js test/fixtures/secureBundling/build.js --projectRoot './' --policyPath test/fixtures/secureBundling/lavamoat/node/policy.json --writeAutoPolicy
 else
-  ../../../../node/src/cli.js build.js --projectRoot '../../../'
+  ../node/src/cli.js test/fixtures/secureBundling/build.js --projectRoot './' --policyPath test/fixtures/secureBundling/lavamoat/node/policy.json
 fi
