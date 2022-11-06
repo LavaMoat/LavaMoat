a7228057b2065ffa22d2562e088a13d3d585f9d8
weizman <weizmangal@gmail.com>
2022-11-06 14:03:56 +0200

    remove logs

--- packages/browserify/test/util.js
+++ packages/browserify/test/util.js
@@ -158,7 +158,6 @@ async function prepareBrowserifyScenarioOnDisk ({ scenario }) {
   copyFolderSync(nodePath, `${projectDir}/node_modules/lavamoat/`, {skip: ['node_modules']})
   copyFolderSync(corePath, `${projectDir}/node_modules/lavamoat-core/`, {skip: ['node_modules']})
   copyFolderSync(browserifyPath, `${projectDir}/node_modules/lavamoat-browserify/`, {skip: ['node_modules']})
-  console.warn(555, fs2.readFileSync(`${lavapackPath}/src/pack.js`).indexOf('GWGW2323'))
   if (installDevDepsResult.status !== 0) {
     const msg = `Error while installing browserify:\n${installDevDepsResult.stderr.toString()}`
     throw new Error(msg)
@@ -194,7 +193,6 @@ async function createBundleForScenario ({
   if (stderr.length) {
     console.warn(stderr)
   }
-  console.warn(444, bundle.indexOf(`["process"`))
   return { bundleForScenario: bundle, policyDir: policy }
 }
 
--- packages/lavapack/src/pack.js
+++ packages/lavapack/src/pack.js
@@ -79,13 +79,10 @@ function createPacker({
   }
   assert(policy, 'must specify a policy')
 
-  const xxx = JSON.stringify({
-    scuttleGlobalThis, /*GWGW2323*/
+  prelude = prelude.replace('__lavamoatSecurityOptions__', JSON.stringify({
+    scuttleGlobalThis,
     scuttleGlobalThisExceptions,
-  })
-  console.warn('KOKOKOKO', prelude.indexOf('__lavamoatSecurityOptions__'), 234789897234, xxx)
-  prelude = prelude.replace('__lavamoatSecurityOptions__', xxx)
-  console.warn('KOKOKOKO', prelude.indexOf('__lavamoatSecurityOptions__'), 234789897234)
+  }))
 
   // note: pack stream cant started emitting data until its received its first module
   // this is because the browserify pipeline is leaky until its finished being setup
