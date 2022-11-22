c94ec9c71dd8292f4f8543f50ae7df2c53579d33
legobt <6wbvkn0j@anonaddy.me>
2022-11-22 02:30:52 +0000

    protect,allow-scripts: post-main-merge lint

--- packages/protect/src/lib/effect.js
+++ packages/protect/src/lib/effect.js
@@ -1,18 +1,18 @@
-let dryRun = false;
+let dryRun = false
 module.exports = {
   isDryRun: () => dryRun,
   setDryRun: (v) => {
     if (dryRun && !v) {
-      throw Error("Can not undo switching to dry-run");
+      throw Error('Can not undo switching to dry-run')
     }
-    dryRun = !!v;
+    dryRun = !!v
   },
   performEffect: (description, effectFunc) => {
     if (dryRun === false) {
-      return effectFunc();
+      return effectFunc()
     } else {
-      console.log(`dry run, skipping ${description}`);
+      console.log(`dry run, skipping ${description}`)
     }
   },
   resetDryRunIKnowWhatImDoing: () => (dryRun = false),
-};
+}
