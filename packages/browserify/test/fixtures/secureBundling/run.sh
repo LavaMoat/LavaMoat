750ccc591df948ec18cecc9265e9d8edb47c416e
legobt <6wbvkn0j@anonaddy.me>
2022-12-22 16:21:10 +0000

    browserify: test: make secureBundling script POSIX-compliant
    Prior version was not valid syntax in Dash. This browserify test, and test:prep,
    have therefore not been working on Debian/Ubuntu as of #402.

--- packages/browserify/test/fixtures/secureBundling/run.sh
+++ packages/browserify/test/fixtures/secureBundling/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ "$WRITE_AUTO_POLICY" == "1" ]; then
+if [ "$WRITE_AUTO_POLICY" = "1" ]; then
   ../node/src/cli.js test/fixtures/secureBundling/build.js --projectRoot './' --policyPath test/fixtures/secureBundling/lavamoat/node/policy.json --writeAutoPolicy
 else
   ../node/src/cli.js test/fixtures/secureBundling/build.js --projectRoot './' --policyPath test/fixtures/secureBundling/lavamoat/node/policy.json
