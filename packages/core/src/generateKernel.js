ffce98b3f3ba6481d6b4ff3068f7a071dd247617
weizman <weizmangal@gmail.com>
2023-03-05 18:28:16 +0200

    minor reorder

--- packages/core/src/generateKernel.js
+++ packages/core/src/generateKernel.js
@@ -41,7 +41,7 @@ function generateKernel (opts = {}) {
   if (opts.hasOwnProperty('scuttleGlobalThis')) {
     // scuttleGlobalThis config placeholder should be set only if ordered so explicitly.
     // if not, should be left as is to be replaced by a later processor (e.g. LavaPack).
-    const {scuttleGlobalThis, scuttleGlobalThisExceptions, useSnow} = opts
+    const {useSnow, scuttleGlobalThis, scuttleGlobalThisExceptions} = opts
     if (scuttleGlobalThisExceptions) {
       // toString regexps if there's any
       for (let i = 0; i < scuttleGlobalThisExceptions.length; i++) {
