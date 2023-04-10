555ba4c5216d825eebb2ba17a0358e0278e2b525
LeoTM <1881059+leotm@users.noreply.github.com>
2023-04-10 17:17:33 +0100

    packageJson.scripts['allow-scripts'] thoughts

--- packages/allow-scripts/src/setup.js
+++ packages/allow-scripts/src/setup.js
@@ -151,7 +151,16 @@ function editPackageJson () {
     }
     // If you think `node ` is redundant below, be aware that `./cli.js` won't work on Windows, 
     // but passing a unix-style path to node on Windows works fine.
-    packageJson.scripts['allow-scripts'] = 'node ./node_modules/@lavamoat/allow-scripts/src/cli.js --experimental-bins'
+
+    // why only do this for FEATURE.bins?
+    packageJson.scripts['allow-scripts'] = 'node ./node_modules/@lavamoat/allow-scripts/src/cli.js --experimental-bins' // why --experimental-bins being ignored?
+    // and not by default:
+    // packageJson.scripts['allow-scripts'] = 'node ./node_modules/@lavamoat/allow-scripts/src/cli.js'
+
+    // why not this?
+    // packageJson.scripts['allow-scripts'] = 'yarn allow-scripts --experimental-bins'
+    // in case yarn or allow-scripts is compromised?
+
     console.log('@lavamoat/allow-scripts: Adding allow-scripts as a package.json script with direct path.')
     writeFileSync(addInstallParentDir('package.json'), JSON.stringify(packageJson, null, 2))
   }
