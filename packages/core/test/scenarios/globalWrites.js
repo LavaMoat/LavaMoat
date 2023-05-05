40f9343de858b9c01f5e4fb1c26b3899b106e5c3
kumavis <aaron@kumavis.me>
2023-05-04 14:18:25 -1000

    core/test - add failing test for read access with write perm

--- packages/core/test/scenarios/globalWrites.js
+++ packages/core/test/scenarios/globalWrites.js
@@ -39,5 +39,34 @@ module.exports = [
       expectedResult: true
     })
     return scenario
-  }
+  },
+  async () => {
+    const scenario = createScenarioFromScaffold({
+      name: 'globalWrites - and reads',
+      defineOne: () => {
+        module.exports = {
+          type: typeof globalThis.xyz,
+          value: globalThis.xyz
+        }
+      },
+      config: {
+        resources: {
+          one: {
+            globals: {
+              xyz: 'write'
+            }
+          },
+        }
+      },
+      // populate evaluation realm global
+      beforeCreateKernel: (scenario) => {
+        scenario.globalThis.xyz = 123
+      },
+      expectedResult: {
+        type: 'number',
+        value: 123,
+      },
+    })
+    return scenario
+  },
 ]
