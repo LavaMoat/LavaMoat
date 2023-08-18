96c905bb398c8ffe9ee640fff6af39803bd63b2b
Christopher Hiller <boneskull@boneskull.com>
2023-08-17 17:18:09 -0700

    chore: revert failure output in runtimeEcma test

--- packages/lavapack/test/runtimeEcma.spec.js
+++ packages/lavapack/test/runtimeEcma.spec.js
@@ -6,9 +6,16 @@ const ECMA_VERSION = 2020;
 
 test('runtime.js parses as ECMA 2020', (t) => {
   const code = fs.readFileSync(require.resolve('../src/runtime.js'), 'utf8')
-  t.notThrows(() => {
-    espree.parse(code, {
-    ecmaVersion: ECMA_VERSION,
+  try {
+    t.notThrows(() => {
+      espree.parse(code, {
+        ecmaVersion: ECMA_VERSION,
+      })
     })
-  })
+  } catch (err) {
+    const { message, lineNumber, column } = err
+    console.error(message, `at line ${lineNumber}, column ${column}`)
+    console.error(`Failed to parse runtime.js as ECMA ${ECMA_VERSION}`)
+    throw err
+  }
 })
