2f08189fe7af5738c1a996776f83f62791499eeb
Christopher Hiller <boneskull@boneskull.com>
2023-10-27 14:52:45 -0700

    chore(endomoat): apply some fixes

---
+++ packages/endomoat/test/policy-converter.spec.js
@@ -0,0 +1,52 @@
+import test from 'ava'
+import { ENDO_ROOT_POLICY, ENDO_WILDCARD_POLICY } from '../src/index.js'
+import { toEndoPolicy } from '../src/policy-converter.js'
+
+/**
+ * @typedef {import('@endo/compartment-mapper').PolicyItem & import('../src/policy-converter.js').RootPolicy} LavaMoatPackagePolicyItem
+ */
+
+test('toEndoPolicy - basic', (t) => {
+  /** @type {import('lavamoat-core').LavaMoatPolicy} */
+  const lmPolicy = {
+    resources: {
+      a: {
+        packages: {
+          b: true,
+        },
+        globals: {
+          console: true,
+        },
+        builtin: {
+          'fs.readFile': true,
+        },
+      },
+    },
+  }
+  /**
+   * @type {import('@endo/compartment-mapper').Policy<LavaMoatPackagePolicyItem>}
+   */
+  const expected = {
+    defaultAttenuator: '@lavamoat/endomoat/attenuator/default',
+    entry: {
+      globals: [ENDO_ROOT_POLICY],
+      noGlobalFreeze: true,
+      packages: ENDO_WILDCARD_POLICY,
+      builtins: ENDO_WILDCARD_POLICY,
+    },
+    resources: {
+      a: {
+        packages: { b: true },
+        globals: { console: true },
+        builtins: {
+          fs: {
+            attenuate: '@lavamoat/endomoat/attenuator/property',
+            params: ['readFile'],
+          },
+        },
+      },
+    },
+  }
+
+  t.deepEqual(toEndoPolicy(lmPolicy), expected)
+})
