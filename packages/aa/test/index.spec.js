574e942c7ed22f720823b362f459683410a4ad4f
Christopher Hiller <boneskull@boneskull.com>
2023-12-20 17:36:29 -0800

    feat(aa): add custom resolver
    It also accepts a custom resolver, which is needed for actually resolving ESM packages, due to the inability of the `resolve` package to do so.  Adding this natively would break the custom resolver; we may need to abandon it, because `package.json` _must_ be read in order to resolve any package having an `exports` field.

--- packages/aa/test/index.spec.js
+++ packages/aa/test/index.spec.js
@@ -1,19 +1,26 @@
+const resolve = require('resolve')
 const path = require('path')
 const test = require('ava')
 const { loadCanonicalNameMap } = require('../src/index.js')
-
-test('project 1', async (t) => {
-  const canonicalNameMap = await loadCanonicalNameMap({
-    rootDir: path.join(__dirname, 'projects', '1'),
-  })
-  // normalize results to be relative
-  const normalizedMapEntries = Array.from(canonicalNameMap.entries())
+const { Module } = require('node:module')
+/**
+ *
+ * @param {import('../src/index.js').CanonicalNameMap} map
+ */
+function normalizeEntries(map) {
+  return [...map.entries()]
     .sort()
     .map(([packagePath, canonicalName]) => [
       path.relative(__dirname, packagePath),
       canonicalName,
     ])
-  t.deepEqual(normalizedMapEntries, [
+}
+
+test('project 1', async (t) => {
+  const canonicalNameMap = await loadCanonicalNameMap({
+    rootDir: path.join(__dirname, 'projects', '1'),
+  })
+  t.deepEqual(normalizeEntries(canonicalNameMap), [
     ['projects/1', '$root$'],
     ['projects/1/node_modules/aaa', 'aaa'],
     ['projects/1/node_modules/bbb', 'bbb'],
@@ -25,14 +32,7 @@ test('project 2', async (t) => {
   const canonicalNameMap = await loadCanonicalNameMap({
     rootDir: path.join(__dirname, 'projects', '2'),
   })
-  // normalize results to be relative
-  const normalizedMapEntries = Array.from(canonicalNameMap.entries())
-    .sort()
-    .map(([packagePath, canonicalName]) => [
-      path.relative(__dirname, packagePath),
-      canonicalName,
-    ])
-  t.deepEqual(normalizedMapEntries, [
+  t.deepEqual(normalizeEntries(canonicalNameMap), [
     ['projects/2', '$root$'],
     ['projects/2/node_modules/aaa', 'aaa'],
     ['projects/2/node_modules/bbb', 'bbb'],
@@ -45,18 +45,33 @@ test('project 3', async (t) => {
   const canonicalNameMap = await loadCanonicalNameMap({
     rootDir: path.join(__dirname, 'projects', '3'),
   })
-  // normalize results to be relative
-  const normalizedMapEntries = Array.from(canonicalNameMap.entries())
-    .sort()
-    .map(([packagePath, canonicalName]) => [
-      path.relative(__dirname, packagePath),
-      canonicalName,
-    ])
-  t.deepEqual(normalizedMapEntries, [
+  t.deepEqual(normalizeEntries(canonicalNameMap), [
     ['projects/3', '$root$'],
     ['projects/3/node_modules/aaa', 'aaa'],
     ['projects/3/node_modules/bbb', 'bbb'],
     ['projects/3/node_modules/bbb/node_modules/good_dep', 'bbb>good_dep'],
     ['projects/3/node_modules/evil_dep', 'evil_dep'],
+  ])
+})
+
+test('custom Resolver', async (t) => {
+  const rootDir = path.join(__dirname, 'projects', '1')
+  /** @type {import('../src/index.js').Resolver} */
+  const resolver = {
+    sync: (moduleId, { basedir }) => {
+      return Module.createRequire(path.join(basedir, 'dummy.js')).resolve(
+        moduleId
+      )
+    },
+  }
+  const canonicalNameMap = await loadCanonicalNameMap({
+    rootDir,
+    resolve: resolver,
+  })
+  t.deepEqual(normalizeEntries(canonicalNameMap), [
+    ['projects/1', '$root$'],
+    ['projects/1/node_modules/aaa', 'aaa'],
+    ['projects/1/node_modules/bbb', 'bbb'],
+    ['projects/1/node_modules/bbb/node_modules/evil_dep', 'bbb>evil_dep'],
   ])
 })
