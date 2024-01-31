581ce9265d4c5aae00ad7096292af1188f71cde0
Christopher Hiller <boneskull@boneskull.com>
2024-01-31 14:22:42 -0800

    chore(laverna): fix eslint config

--- packages/laverna/.eslintrc.js
+++ packages/laverna/.eslintrc.js
@@ -1,24 +1,31 @@
+// @ts-check
+
 /**
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
-  rules: {
-    'no-restricted-globals': [
-      'error',
-      {
-        name: 'console',
-        message: 'Use console from capabilities object',
+  overrides: [
+    {
+      files: ['src/**/*.js'],
+      rules: {
+        'no-restricted-globals': [
+          'error',
+          {
+            name: 'console',
+            message: 'Use console from capabilities object',
+          },
+        ],
+        'no-restricted-properties': [
+          'error',
+          {
+            object: 'JSON',
+            property: 'parse',
+            message: 'Use parseJson from capabilities object',
+          },
+        ],
       },
-    ],
-    'no-restricted-properties': [
-      'error',
-      {
-        object: 'JSON',
-        property: 'parse',
-        message: 'Use parseJson from capabilities object',
-      },
-    ],
-  },
+    },
+  ],
   // this file is essentially vendored
   ignorePatterns: 'test/types.d.ts',
 }
