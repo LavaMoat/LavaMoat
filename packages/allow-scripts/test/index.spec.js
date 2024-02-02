64eaeac5b6355820d1180941647c8f77618ad0c4
legobt <6wbvkn0j@anonaddy.me>
2024-02-02 12:31:58 +0000

    chore(allow-scripts): skip experimental-bins test on Windows
    The feature does not work on win32 atm

--- packages/allow-scripts/test/index.spec.js
+++ packages/allow-scripts/test/index.spec.js
@@ -5,7 +5,14 @@ const path = require('node:path')
 const { spawnSync } = require('node:child_process')
 const { pathToFileURL } = require('node:url')
 
-const NPM_CMD = os.platform() === 'win32' ? 'npm.cmd' : 'npm'
+const isWindows = os.platform() === 'win32'
+const NPM_CMD = isWindows ? 'npm.cmd' : 'npm'
+/* eslint-disable ava/no-skip-test */
+const skipOnWindows =
+  isWindows
+    ? (description, ...args) =>
+        test.skip('[no Windows support] ' + description, ...args)
+    : test
 
 /**
  * For fat fingers
@@ -14,25 +21,22 @@ const PACKAGE_JSON = 'package.json'
 
 /**
  * Execute allow-scripts binaries with the given arguments
- * @param {any} t ava test object
+ *
+ * @param {any} t Ava test object
  * @param {string[]} args Arguments to pass
  * @param {string} cwd Working directory to execute command from
- * @returns {{stderr: string, stdout: string, status: number}} Process result
+ * @returns {{ stderr: string; stdout: string; status: number }} Process result
  */
 const run = (t, args, cwd) => {
   // Path to the allow-scripts executable
   const ALLOW_SCRIPTS_BIN = require.resolve('../src/cli')
   const options = realisticEnvOptions(cwd)
-  const result = spawnSync(
-    'node',
-    [
-      ALLOW_SCRIPTS_BIN,
-      ...args,
-    ],
-    options,
-  )
+  const result = spawnSync('node', [ALLOW_SCRIPTS_BIN, ...args], options)
 
-  if (typeof result.stderr === 'undefined' || typeof result.status !== 'number') {
+  if (
+    typeof result.stderr === 'undefined' ||
+    typeof result.status !== 'number'
+  ) {
     t.fail(
       `Failed calling 'node ${ALLOW_SCRIPTS_BIN} ${args.join(' ')}': ${JSON.stringify(
         {
@@ -55,7 +59,6 @@ const run = (t, args, cwd) => {
     stdout: result.stdout,
   }
 }
-
 
 test('cli - auto command', (t) => {
   // set up the directories
@@ -109,7 +112,7 @@ test('cli - auto command', (t) => {
   })
 })
 
-test('cli - auto command with experimental bins', (t) => {
+skipOnWindows('cli - auto command with experimental bins', (t) => {
   // set up the directories
   let projectRoot = path.join(__dirname, 'projects', '1')
 
@@ -173,43 +176,46 @@ test('cli - run command - good dep at the root', (t) => {
   // with
   // "preinstall": "touch /tmp/$( date '+%Y-%m-%d_%H-%M-%S' )"
 })
-test('cli - run command - good dep at the root with experimental bins', (t) => {
-  // set up the directories
-  let projectRoot = path.join(__dirname, 'projects', '2')
+skipOnWindows(
+  'cli - run command - good dep at the root with experimental bins',
+  (t) => {
+    // set up the directories
+    let projectRoot = path.join(__dirname, 'projects', '2')
 
-  // clean up from a previous run
-  // the force option is only here to stop rm complaining if target is missing
-  fs.rmSync(path.join(projectRoot, 'node_modules', '.bin'), {
-    recursive: true,
-    force: true,
-  })
+    // clean up from a previous run
+    // the force option is only here to stop rm complaining if target is missing
+    fs.rmSync(path.join(projectRoot, 'node_modules', '.bin'), {
+      recursive: true,
+      force: true,
+    })
 
-  // run the "run" command
-  const result = run(t, ['run', '--experimental-bins'], projectRoot)
+    // run the "run" command
+    const result = run(t, ['run', '--experimental-bins'], projectRoot)
 
-  // assert the output
-  t.deepEqual(result.stdout.toString().split('\n'), [
-    'installing bin scripts',
-    '- good - from package: good_dep',
-    'running lifecycle scripts for event "preinstall"',
-    '- good_dep',
-    'running lifecycle scripts for event "install"',
-    'running lifecycle scripts for event "postinstall"',
-    'running lifecycle scripts for top level package',
-    '',
-  ])
+    // assert the output
+    t.deepEqual(result.stdout.toString().split('\n'), [
+      'installing bin scripts',
+      '- good - from package: good_dep',
+      'running lifecycle scripts for event "preinstall"',
+      '- good_dep',
+      'running lifecycle scripts for event "install"',
+      'running lifecycle scripts for event "postinstall"',
+      'running lifecycle scripts for top level package',
+      '',
+    ])
 
-  t.assert(
-    fs.existsSync(path.join(projectRoot, 'node_modules', '.bin', 'good')),
-    'Expected a bin script to be installed in top level node_modules'
-  )
+    t.assert(
+      fs.existsSync(path.join(projectRoot, 'node_modules', '.bin', 'good')),
+      'Expected a bin script to be installed in top level node_modules'
+    )
 
-  // note
-  // we could also test whether the preinstall script is
-  // actually executing. we did it manually by replacing
-  // with
-  // "preinstall": "touch /tmp/$( date '+%Y-%m-%d_%H-%M-%S' )"
-})
+    // note
+    // we could also test whether the preinstall script is
+    // actually executing. we did it manually by replacing
+    // with
+    // "preinstall": "touch /tmp/$( date '+%Y-%m-%d_%H-%M-%S' )"
+  }
+)
 
 test('cli - run command - good dep as a sub dep', (t) => {
   // set up the directories
@@ -244,65 +250,68 @@ test('cli - run command - good dep as a sub dep', (t) => {
   ])
 })
 
-test('cli - run command - good dep as a sub dep with experimental bins', (t) => {
-  // set up the directories
-  let projectRoot = path.join(__dirname, 'projects', '3')
+skipOnWindows(
+  'cli - run command - good dep as a sub dep with experimental bins',
+  (t) => {
+    // set up the directories
+    let projectRoot = path.join(__dirname, 'projects', '3')
 
-  // clean up from a previous run
-  // the force option is only here to stop rm complaining if target is missing
-  fs.rmSync(
-    path.join(projectRoot, 'node_modules', 'bbb', '.goodscriptworked'),
-    { force: true }
-  )
-  fs.rmSync(path.join(projectRoot, 'node_modules', '.bin'), {
-    recursive: true,
-    force: true,
-  })
-  // run the "run" command
-  const result = run(t, ['run', '--experimental-bins'], projectRoot)
+    // clean up from a previous run
+    // the force option is only here to stop rm complaining if target is missing
+    fs.rmSync(
+      path.join(projectRoot, 'node_modules', 'bbb', '.goodscriptworked'),
+      { force: true }
+    )
+    fs.rmSync(path.join(projectRoot, 'node_modules', '.bin'), {
+      recursive: true,
+      force: true,
+    })
+    // run the "run" command
+    const result = run(t, ['run', '--experimental-bins'], projectRoot)
 
-  t.assert(
-    fs.existsSync(
-      path.join(
-        projectRoot,
-        'node_modules',
-        'bbb',
-        'node_modules',
-        '.bin',
-        'good'
-      )
-    ),
-    'Expected a nested bin script to be installed in bbb/node_modules/.bin'
-  )
-  const errarr = result.stderr.toString().split('\n')
-  t.assert(
-    errarr.every((line) => !line.includes('you shall not pass')),
-    'Should not have run the parent script from the nested package postinstall'
-  )
-  t.assert(
-    errarr.some((line) => line.includes('"good": "node_modules/')),
-    'Expected to see instructions on how to enable a bin script1'
-  )
-  t.assert(
-    errarr.some((line) => line.includes('node_modules/good_dep/cli.js')),
-    'Expected to see instructions on how to enable a bin script2'
-  )
-  t.assert(
-    errarr.some((line) => line.includes('node_modules/aaa/shouldntrun.sh')),
-    'Expected to see instructions on how to enable a bin script3'
-  )
-  // assert the output
-  t.deepEqual(result.stdout.toString().split('\n'), [
-    'installing bin scripts',
-    '- good - from package: aaa',
-    'running lifecycle scripts for event "preinstall"',
-    '- bbb>good_dep',
-    'running lifecycle scripts for event "install"',
-    'running lifecycle scripts for event "postinstall"',
-    '- bbb',
-    '',
-  ])
-})
+    t.assert(
+      fs.existsSync(
+        path.join(
+          projectRoot,
+          'node_modules',
+          'bbb',
+          'node_modules',
+          '.bin',
+          'good'
+        )
+      ),
+      'Expected a nested bin script to be installed in bbb/node_modules/.bin'
+    )
+    const errarr = result.stderr.toString().split('\n')
+    t.assert(
+      errarr.every((line) => !line.includes('you shall not pass')),
+      'Should not have run the parent script from the nested package postinstall'
+    )
+    t.assert(
+      errarr.some((line) => line.includes('"good": "node_modules/')),
+      'Expected to see instructions on how to enable a bin script1'
+    )
+    t.assert(
+      errarr.some((line) => line.includes('node_modules/good_dep/cli.js')),
+      'Expected to see instructions on how to enable a bin script2'
+    )
+    t.assert(
+      errarr.some((line) => line.includes('node_modules/aaa/shouldntrun.sh')),
+      'Expected to see instructions on how to enable a bin script3'
+    )
+    // assert the output
+    t.deepEqual(result.stdout.toString().split('\n'), [
+      'installing bin scripts',
+      '- good - from package: aaa',
+      'running lifecycle scripts for event "preinstall"',
+      '- bbb>good_dep',
+      'running lifecycle scripts for event "install"',
+      'running lifecycle scripts for event "postinstall"',
+      '- bbb',
+      '',
+    ])
+  }
+)
 
 /**
  * @param {string} projectRoot
