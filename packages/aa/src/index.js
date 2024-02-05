66996c7964fecee08e4fcb0f01ee66047c8d204d
naugtur <naugtur@gmail.com>
2024-02-05 13:59:44 +0100

    feat(aa): support symlinks for workspace and devtime linking compatibility

--- packages/aa/src/index.js
+++ packages/aa/src/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { readFileSync } = require('node:fs')
+const { readFileSync, realpathSync, lstatSync } = require('node:fs')
 const path = require('node:path')
 const nodeResolve = require('resolve')
 
@@ -143,6 +143,14 @@ function getDependencies(packageDir, includeDevDeps) {
   return depsToWalk
 }
 
+/**
+ * @param {string} location
+ */
+function isSymlink(location) {
+  const info = lstatSync(location)
+  return info.isSymbolicLink()
+}
+
 /** @type {WalkDepTreeOpts[]} */
 let currentLevelTodos
 
@@ -176,6 +184,15 @@ function walkDependencyTreeForBestLogicalPaths({
     }
   } while (currentLevelTodos.length > 0)
 
+  for (const [
+    packageDir,
+    logicalPath,
+  ] of preferredPackageLogicalPathMap.entries()) {
+    if (isSymlink(packageDir)) {
+      const realPath = realpathSync(packageDir)
+      preferredPackageLogicalPathMap.set(realPath, logicalPath)
+    }
+  }
   return preferredPackageLogicalPathMap
 }
 
