7c61a294073b20648d7e3aa376f45f705e88258e
naugtur <naugtur@gmail.com>
2024-02-07 13:35:34 +0100

    test(aa): microbenchmark to ensure checking for symlink before resolving it is worth it

---
+++ packages/aa/test/benchmark1.spec.js
@@ -0,0 +1,61 @@
+const { realpathSync, lstatSync } = require('node:fs')
+const path = require('node:path')
+const test = require('ava')
+
+function isSymlink(location) {
+  const info = lstatSync(location)
+  return info.isSymbolicLink()
+}
+
+const symlink = path.join(__dirname, './projects/4/node_modules/aaa')
+const notsymlink = path.join(__dirname, './projects/4/node_modules/bbb')
+
+const bench = (fn, name) => {
+  const t0 = performance.now()
+  for (let i = 0; i < 10000; i++) {
+    fn()
+  }
+  const t1 = performance.now()
+  return [name, t1 - t0]
+}
+
+const ratioIsBelow = (a, b, expected) => {
+  const ratio = a / b
+  return ratio < expected
+}
+
+test('[bench] isSymlink is significantly faster than realpathSync in a naive microbenchmark', (t) => {
+  const entries = [
+    bench(() => {
+      isSymlink(symlink)
+    }, 'isSymlink(symlink)'),
+    bench(() => {
+      isSymlink(notsymlink)
+    }, 'isSymlink(notsymlink)'),
+    bench(() => {
+      realpathSync(symlink)
+    }, 'realpathSync(symlink)'),
+    bench(() => {
+      realpathSync(notsymlink)
+    }, 'realpathSync(notsymlink)'),
+  ]
+
+  const results = Object.fromEntries(entries)
+  t.log({ results })
+  t.assert(
+    ratioIsBelow(
+      results['isSymlink(symlink)'],
+      results['realpathSync(symlink)'],
+      0.2 // the tradeoff is worth it up until close to 1, so this number can be increased.
+    ),
+    'expected isSymlink(symlink) to be much faster than realpathSync(symlink)'
+  )
+  t.assert(
+    ratioIsBelow(
+      results['isSymlink(notsymlink)'],
+      results['realpathSync(notsymlink)'],
+      0.2 // the tradeoff is worth it up until close to 1, so this number can be increased.
+    ),
+    'expected isSymlink(notsymlink) to be much faster than realpathSync(notsymlink)'
+  )
+})
