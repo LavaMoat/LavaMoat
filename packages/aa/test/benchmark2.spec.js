3c6ef381c1afc24d335e61b68296bfa59205fe3f
naugtur <naugtur@gmail.com>
2024-02-21 12:33:54 +0100

    test(aa): add memory spike recording to benchmark

--- packages/aa/test/benchmark2.spec.js
+++ packages/aa/test/benchmark2.spec.js
@@ -1,6 +1,6 @@
 const test = require('ava')
 const path = require('node:path')
-const { benchAsync } = require('./utils')
+const { benchAsync, benchAsyncAll } = require('./utils')
 const { loadCanonicalNameMap } = require('../src/index.js')
 
 test('[bench] loadCanonicalNameMap on workspace root', async (t) => {
@@ -13,11 +13,16 @@ test('[bench] loadCanonicalNameMap on workspace root', async (t) => {
     map.size > 100,
     'Expected map to have many entries. Check the rootDir path. It should point to the workspace root of the repo for this benchmark to be effective.'
   )
-
   const result = await benchAsync(subject, 'loadCanonicalNameMap', 10)
   t.log({ result })
   t.assert(
     result['loadCanonicalNameMap'] < 500, // arbitrary large number so we notice when we degrade by orders of magnitude. Watch the results when developing.
     'expected loadCanonicalNameMap to be faster than 500ms'
   )
+  // For development - ensure you're not breaking the result
+  // t.snapshot(
+  //   Array.from(map.entries())
+  //     .map(([k, v]) => k + v)
+  //     .sort()
+  // )
 })
--- packages/aa/test/utils.js
+++ packages/aa/test/utils.js
@@ -2,6 +2,53 @@ const path = require('node:path')
 const { symlink } = require('node:fs/promises')
 const { existsSync } = require('node:fs')
 
+const translateHeapStats = (stats = []) => {
+  const result = {}
+  for (const { space_name, space_used_size } of stats) {
+    result[space_name] = space_used_size
+  }
+  return result
+}
+
+const updateMaxEachKey = (current, update) => {
+  for (const key in current) {
+    current[key] = Math.max(current[key], update[key])
+  }
+}
+
+const diffEachKey = (a, b) => {
+  const result = {}
+  for (const key in a) {
+    result[key] = b[key] - a[key]
+  }
+  return result
+}
+
+const toHumanReadable = (obj) => {
+  const result = {}
+  for (const key in obj) {
+    if (obj[key] > 0) result[key] = `+${(obj[key] / 1024 / 1024).toFixed(4)} MB`
+  }
+  return result
+}
+
+const recordMemorySpike = (frequency = 10) => {
+  const v8 = require('v8')
+  const initial = translateHeapStats(v8.getHeapSpaceStatistics())
+  const result = { ...initial }
+  const collect = () =>
+    updateMaxEachKey(result, translateHeapStats(v8.getHeapSpaceStatistics()))
+  const interval = setInterval(collect, frequency)
+  return {
+    collect,
+    getResult: () => {
+      clearInterval(interval)
+      collect()
+      return toHumanReadable(diffEachKey(initial, result))
+    },
+  }
+}
+
 /**
  * Normalizes paths in canonical name map entries
  *
@@ -28,19 +75,35 @@ exports.createProject4Symlink = async () => {
 }
 
 exports.bench = (fn, name, iterations = 10000) => {
+  const s = recordMemorySpike()
   const t0 = performance.now()
   for (let i = 0; i < iterations; i++) {
     fn()
+    s.collect()
   }
   const t1 = performance.now()
-  return { [name]: (t1 - t0) / iterations }
+  return { [name]: (t1 - t0) / iterations, MEMORY_SPIKE: s.getResult() }
 }
 
 exports.benchAsync = async (fn, name, iterations = 1000) => {
+  const s = recordMemorySpike()
   const t0 = performance.now()
   for (let i = 0; i < iterations; i++) {
     await fn()
+    s.collect()
   }
   const t1 = performance.now()
-  return { [name]: (t1 - t0) / iterations }
+  return { [name]: (t1 - t0) / iterations, MEMORY_SPIKE: s.getResult() }
 }
+
+exports.benchAsyncAll = async (fn, name, iterations = 1000) => {
+  const s = recordMemorySpike()
+
+  const calls = new Array(iterations).fill(0)
+  const t0 = performance.now()
+  await Promise.all(calls.map(() => fn()))
+  const t1 = performance.now()
+  return { [name]: (t1 - t0) / iterations, MEMORY_SPIKE: s.getResult() }
+}
+
+exports.recordMemorySpike = recordMemorySpike
