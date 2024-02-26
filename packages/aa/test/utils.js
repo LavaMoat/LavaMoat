68d54b2f930503bb58b8ea1ecc639a41403f9e30
Zbyszek Tenerowicz <naugtur@gmail.com>
2024-02-26 13:10:22 +0100

    chore(aa): Apply suggestions from code review
    Co-authored-by: Christopher Hiller <boneskull@boneskull.com>

--- packages/aa/test/utils.js
+++ packages/aa/test/utils.js
@@ -2,6 +2,9 @@ const path = require('node:path')
 const { symlink } = require('node:fs/promises')
 const { existsSync } = require('node:fs')
 
+/**
+ * @param {{space_name: string, space_used_size: number}[]} stats
+ */
 const translateHeapStats = (stats = []) => {
   const result = {}
   for (const { space_name, space_used_size } of stats) {
@@ -10,13 +13,22 @@ const translateHeapStats = (stats = []) => {
   return result
 }
 
+/**
+ * @param {Record<string, number>} current
+ * @param {Record<string, number>} update
+ */
 const updateMaxEachKey = (current, update) => {
   for (const key in current) {
     current[key] = Math.max(current[key], update[key])
   }
 }
 
+/**
+ * @param {Record<string, number>} a
+ * @param {Record<string, number>} b
+ */
 const diffEachKey = (a, b) => {
+  /** @type {Record<string, number>} */
   const result = {}
   for (const key in a) {
     result[key] = b[key] - a[key]
@@ -24,6 +36,9 @@ const diffEachKey = (a, b) => {
   return result
 }
 
+/**
+ * @param {Record<string, number>} obj
+ */
 const toHumanReadable = (obj) => {
   const result = {}
   for (const key in obj) {
@@ -32,13 +47,16 @@ const toHumanReadable = (obj) => {
   return result
 }
 
+/**
+ * @param {number} frequency
+ */
 const recordMemorySpike = (frequency = 10) => {
   const v8 = require('v8')
   const initial = translateHeapStats(v8.getHeapSpaceStatistics())
   const result = { ...initial }
   const collect = () =>
     updateMaxEachKey(result, translateHeapStats(v8.getHeapSpaceStatistics()))
-  const interval = setInterval(collect, frequency)
+  const interval = setInterval(collect, frequency).unref()
   return {
     collect,
     getResult: () => {
@@ -74,6 +92,12 @@ exports.createProject4Symlink = async () => {
   await exports.osIndependentSymlink(target, src)
 }
 
+/**
+ * @param {(() => void)} fn
+ * @param {string} name
+ * @param {iterations} number
+ * @returns {{MEMORY_SPIKE: string, [x: string]: number}}
+ */
 exports.bench = (fn, name, iterations = 10000) => {
   const s = recordMemorySpike()
   const t0 = performance.now()
@@ -85,6 +109,12 @@ exports.bench = (fn, name, iterations = 10000) => {
   return { [name]: (t1 - t0) / iterations, MEMORY_SPIKE: s.getResult() }
 }
 
+/**
+ * @param {(() => void | Promise<void>)} fn
+ * @param {string} name
+ * @param {iterations} number
+ * @returns {Promise<{MEMORY_SPIKE: string, [x: string]: number}>
+ */
 exports.benchAsync = async (fn, name, iterations = 1000) => {
   const s = recordMemorySpike()
   const t0 = performance.now()
@@ -96,6 +126,12 @@ exports.benchAsync = async (fn, name, iterations = 1000) => {
   return { [name]: (t1 - t0) / iterations, MEMORY_SPIKE: s.getResult() }
 }
 
+/**
+ * @param {(() => void | Promise<void>)} fn
+ * @param {string} name
+ * @param {iterations} number
+ * @returns {Promise<{MEMORY_SPIKE: string, [x: string]: number}>
+ */
 exports.benchAsyncAll = async (fn, name, iterations = 1000) => {
   const s = recordMemorySpike()
 
