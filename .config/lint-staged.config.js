8fe2123661f08f21797b3072847e9926b60c3f4a
Christopher Hiller <boneskull@boneskull.com>
2024-02-27 13:08:02 -0800

    chore: remove dirty-check from lint-staged cfg

--- .config/lint-staged.config.js
+++ .config/lint-staged.config.js
@@ -5,8 +5,4 @@ module.exports = {
   '*.js': ['eslint --fix', 'prettier --write'],
   '*.(ts|md|ya?ml|json)': ['prettier --write'],
   '!((package-lock|policy)*).json': ['prettier --write'],
-  'packages/*/src/**/*.js|package?(-lock).json': () => [
-    'npm run test:prep',
-    'npm run test:dirty-check',
-  ],
 }
