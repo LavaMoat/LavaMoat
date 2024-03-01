5820d63367389d79c67c16665f8493690d185966
Christopher Hiller <boneskull@boneskull.com>
2024-03-01 12:34:47 -0800

    feat(node): create @lavamoat/node

---
+++ packages/endomoat/src/module-transforms.js
@@ -0,0 +1,43 @@
+import { evadeCensorSync } from '@endo/evasive-transform'
+import { applySourceTransforms } from 'lavamoat-core'
+
+export const decoder = new TextDecoder()
+export const encoder = new TextEncoder()
+
+/**
+ * @import {SyncModuleTransform, Language} from '@endo/compartment-mapper'
+ */
+
+/**
+ * Create module transform which performs source transforms to evade SES
+ * restrictions
+ *
+ * @param {Language} parser
+ * @returns {SyncModuleTransform}
+ * @internal
+ */
+const createModuleTransform = (parser) => {
+  return (sourceBytes, specifier, location, _packageLocation, opts) => {
+    let source = decoder.decode(sourceBytes)
+    // FIXME: this function calls stuff we could get in `ses/tools.js`
+    // except `evadeDirectEvalExpressions`. unclear if we should be using this from `lavamoat-core`
+    source = applySourceTransforms(source)
+    const { code, map } = evadeCensorSync(source, {
+      sourceMap: opts?.sourceMap,
+      sourceUrl: new URL(specifier, location).href,
+      sourceType: 'module',
+    })
+    const objectBytes = encoder.encode(code)
+    return { bytes: objectBytes, parser, map }
+  }
+}
+
+/**
+ * Standard set of module transforms for our purposes
+ *
+ * @internal
+ */
+export const syncModuleTransforms = /** @type {const} */ ({
+  cjs: createModuleTransform('cjs'),
+  mjs: createModuleTransform('mjs'),
+})
---
+++ packages/endomoat/test/policy-gen/generate-policy.spec.js
@@ -0,0 +1,115 @@
+import 'ses'
+
+import test from 'ava'
+import { createGeneratePolicyMacros } from './macros.js'
+
+const { testPolicyForModule, testPolicyForScript, testPolicyForJSON } =
+  createGeneratePolicyMacros(test)
+
+test(testPolicyForJSON, 'builtins.json')
+
+test(testPolicyForJSON, 'kitchen-sink.json')
+
+test(testPolicyForJSON, 'phony-native.json')
+
+// NOTE: this will not generate a "dynamic" prop because `require` is set to a
+// different identifier (`runtimeRequire`) in node-gyp-build
+test(testPolicyForJSON, 'native.json')
+
+test(testPolicyForJSON, 'dynamic.json')
+
+test('basic nested global access', testPolicyForModule, 'location.href', {
+  resources: {
+    test: {
+      globals: {
+        'location.href': true,
+      },
+    },
+  },
+})
+
+test(
+  'CJS-specific ignores',
+  testPolicyForScript,
+  `
+const js = [this]
+const ignored = [global, require, module, exports, arguments]
+const globalRefs = [typeof globalThis, typeof self, typeof window]
+const xyz = nonIgnoredGlobal
+`,
+  {
+    resources: {
+      test: {
+        globals: {
+          nonIgnoredGlobal: true,
+        },
+      },
+    },
+  }
+)
+
+test(
+  'ESM-specific ignores',
+  testPolicyForModule,
+  `
+const js = [this]
+const ignored = [global, require, module, exports, arguments]
+const globalRefs = [typeof globalThis, typeof self, typeof window]
+const xyz = nonIgnoredGlobal
+export {xyz}
+`,
+  {
+    resources: {
+      test: {
+        globals: {
+          nonIgnoredGlobal: true,
+          require: true,
+          module: true,
+          exports: true,
+        },
+      },
+    },
+  }
+)
+
+test(
+  'ignored global refs',
+  testPolicyForModule,
+  `
+  const href = window.location.href
+  const xhr = new window.XMLHttpRequest()
+  `,
+  {
+    resources: {
+      test: {
+        globals: {
+          'location.href': true,
+          XMLHttpRequest: true,
+        },
+      },
+    },
+  }
+)
+
+test(
+  'ignored global refs when properties are not accessed',
+  testPolicyForModule,
+  'typeof window !== "undefined"',
+  { resources: {} }
+)
+
+test(
+  'ignored global refs accessed w/ whitelist items',
+  testPolicyForModule,
+  `window.Object === Object`,
+  {
+    resources: {},
+  }
+)
+
+test(
+  'generatePolicy - ignore newer intrinsics',
+  testPolicyForModule,
+  'BigInt(123)',
+  { resources: {} }
+)
