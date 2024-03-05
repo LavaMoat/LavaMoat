18840ea153376e2bf300a3dd36077b09ee91f74c
naugtur <naugtur@gmail.com>
2024-03-05 09:51:35 +0100

    fix(webpack): fix global aliases in non-root compartments

---
+++ packages/webpack/test/e2e-manual.js
@@ -0,0 +1,35 @@
+// run this with node to produce the test build to disk
+const fs = require('fs')
+const path = require('path')
+const { scaffold, runScriptWithSES } = require('./scaffold.js')
+const webpackConfigDefault = require('./fixtures/main/webpack.config.js')
+const webpackConfig = {
+  ...webpackConfigDefault,
+  // mode: 'development',
+  // devtool: false,
+}
+
+const now = new Date().toISOString()
+
+const cache = new Set()
+function assertFolderFor(file) {
+  const folder = path.dirname(file)
+  if (cache.has(folder)) return
+  const target = folder.replace(/^\//, '')
+  fs.mkdirSync(path.join('.', 'tmp', now, target), { recursive: true })
+  cache.add(folder)
+}
+
+function writeFromSnapshot(snapshot) {
+  const files = Object.keys(snapshot)
+  for (const file of files) {
+    assertFolderFor(file)
+    fs.writeFileSync(path.join('.', 'tmp', now, file), snapshot[file])
+  }
+}
+
+scaffold(webpackConfig).then(({ stdout, snapshot }) => {
+  console.log(stdout)
+  writeFromSnapshot(snapshot)
+  runScriptWithSES(snapshot['/dist/app.js'])
+})
