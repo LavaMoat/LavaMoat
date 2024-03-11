32bf504fc76ba99edb4add6e3dfa2fc727c81e12
Christopher Hiller <boneskull@boneskull.com>
2024-03-11 15:51:19 -0700

    chore(node, core): move LavaMoatOpts typedef into lavamoat-core
    this removes the implicit dependency on `lavamoat` from `lavamoat-core`

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,5 +1,6 @@
+export type * from './generatePolicy'
 export * from './index'
 export type * from './moduleRecord'
+export type * from './options'
 export type * from './parseForPolicy'
 export type * from './schema'
-export type * from './generatePolicy'
--- packages/node/src/defaults.js
+++ packages/node/src/defaults.js
@@ -2,32 +2,7 @@ const { getDefaultPaths } = require('lavamoat-core')
 
 const defaultPaths = getDefaultPaths('node')
 
-/**
- * @typedef LavaMoatScuttleOpts
- * @property {boolean} [enabled]
- * @property {string[]} [exceptions]
- * @property {string} [scuttlerName]
- */
-
-/**
- * @typedef LavaMoatOpts
- * @property {LavaMoatScuttleOpts} [scuttleGlobalThis] Enable or disable
- *   scuttling of `globalThis`
- * @property {string[]} [scuttleGlobalThisExceptions]
- * @property {boolean} [writeAutoPolicy] Automatically write a policy file
- * @property {boolean} [writeAutoPolicyDebug] Automatically write a debug policy
- *   file
- * @property {boolean} [writeAutoPolicyAndRun] Automatically write a policy file
- *   and run the application
- * @property {string} [policyPath] Path to policy file
- * @property {string} [policyDebugPath] Path to policy debug file
- * @property {string} [policyOverridePath] Path to policy override file
- * @property {string} [projectRoot] Path to project root
- * @property {boolean} [debugMode] Enable debug mode
- * @property {boolean} [statsMode] Enable stats mode
- */
-
-/** @type {LavaMoatOpts} */
+/** @type {import('lavamoat-core').LavaMoatOpts} */
 const defaults = {
   scuttleGlobalThis: {},
   scuttleGlobalThisExceptions: [],
