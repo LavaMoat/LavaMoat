0b5808b09c8af0e3bfb0e99fe607a300578aaedc
Christopher Hiller <boneskull@boneskull.com>
2024-03-26 11:55:26 -0700

    feat(endomoat): support native modules in policy generation [ci skip]

--- packages/endomoat/src/import-hook.js
+++ packages/endomoat/src/import-hook.js
@@ -1,3 +1,6 @@
+import { Module } from 'node:module'
+import { fileURLToPath } from 'node:url'
+
 const { freeze, keys, assign } = Object
 
 /**
@@ -8,6 +11,23 @@ const { freeze, keys, assign } = Object
 export const importHook = async (specifier) => {
   /** @type {object} */
   const ns = await import(specifier)
+  return freeze(
+    /** @type {import('ses').ThirdPartyStaticModuleInterface} */ ({
+      imports: [],
+      exports: keys(ns),
+      execute: (moduleExports) => {
+        moduleExports.default = ns
+        assign(moduleExports, ns)
+      },
+    })
+  )
+}
+
+/** @type {import('@endo/compartment-mapper').DynamicImportHook} */
+export const importNowHook = (specifier, packageLocation) => {
+  const require = Module.createRequire(fileURLToPath(packageLocation))
+  /** @type {object} */
+  const ns = require(specifier)
   return freeze(
     /** @type {import('ses').ThirdPartyStaticModuleInterface} */ ({
       imports: [],
