57d91a98ad4798480117af39efe9726023030c6b
Christopher Hiller <boneskull@boneskull.com>
2024-03-26 11:46:56 -0700

    fixup! feat(endomoat): support native modules in policy generation [ci skip]

--- packages/endomoat/test/index.spec.js
+++ packages/endomoat/test/index.spec.js
@@ -1,10 +1,7 @@
 import 'ses'
 
-import { makeReadPowers } from '@endo/compartment-mapper/node-powers.js'
 import test from 'ava'
-import url from 'node:url'
 import { run } from '../src/index.js'
-import { loadJSONFixture } from './fixture-util.js'
 
 test('basic operation - run an app without dependencies', async (t) => {
   const entryFile = new URL('./fixture/no-deps/app.js', import.meta.url)
@@ -15,4 +12,3 @@ test('basic operation - run an app without dependencies', async (t) => {
   // this is needed because `result` has a `Symbol.toStringTag` prop which confuses AVA
   t.deepEqual({ .../** @type {object} */ (result) }, { hello: 'world' })
 })
-
