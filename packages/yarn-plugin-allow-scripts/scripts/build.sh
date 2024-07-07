d0bed433f2fc245c62ab6bdb6342f1e73db225a5
legobt <6wbvkn0j@anonaddy.me>
2024-07-07 05:01:57 +0000

    chore(yarn-plugin-allow-scripts): macos fix

--- packages/yarn-plugin-allow-scripts/scripts/build.sh
+++ packages/yarn-plugin-allow-scripts/scripts/build.sh
@@ -3,7 +3,12 @@
 builder build plugin $@
 
 # yarn berry does not recognize node: prefixed builtin references
-sed -i \
-  -e "s/\(require\s*(\s*['\"]\)node:/\1/g" \
-  -e 's/npm_config_node_gyp = __require.resolve/npm_config_node_gyp = resolve/' \
+sed \
+  's/npm_config_node_gyp = __require.resolve/npm_config_node_gyp = resolve/' \
+  -i \
+  bundles/*/*.js
+
+sed \
+  "s/\(require\s*(\s*['\"]\)node:/\1/g" \
+  -i \
   bundles/*/*.js
