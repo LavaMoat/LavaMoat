4b4763a77b142e9a1e854dadf77cdd0faed74bdc
naugtur <naugtur@gmail.com>
2024-08-13 15:45:10 +0200

    test(core): improve error message from ses test in core

---
+++ packages/core/test/ses.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+diff -wq ../../node_modules/ses/dist/lockdown.umd.js ./lib/lockdown.umd.js 
+if [[ $? -gt 0 ]]; then
+    echo 'ERROR: vendored SES in core differs from dependency.'
+    echo ' To fix, run: npm run lib:ses'
+    exit 1
+fi
