9387cc46cb242e07e32e95595c704af368fdfabc
naugtur <naugtur@gmail.com>
2024-08-13 15:58:00 +0200

    chore(core): run lib:ses to finish the ses update

--- packages/core/test/ses.sh
+++ packages/core/test/ses.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# note this works in bash on windows too.
 diff -wq ../../node_modules/ses/dist/lockdown.umd.js ./lib/lockdown.umd.js 
 if [[ $? -gt 0 ]]; then
     echo 'ERROR: vendored SES in core differs from dependency.'
