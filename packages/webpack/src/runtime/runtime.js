c07bc6f7ac47dea36f949b5a008522621c2cd166
naugtur <naugtur@gmail.com>
2024-08-23 14:16:38 +0200

    feat(webpack): avoid errors when code is instrumented for HMR despite HMR not being enabled

--- packages/webpack/src/runtime/runtime.js
+++ packages/webpack/src/runtime/runtime.js
@@ -275,6 +275,8 @@ const lavamoatRuntimeWrapper = (resourceId, runtimeKit) => {
         return module
       }
     }
+    // override hmd to silently disable it
+    policyRequire.hmd = (/** @type {any} */ mo) => mo
 
     overrides.__webpack_require__ = policyRequire
   }
