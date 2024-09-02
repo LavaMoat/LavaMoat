2ff8efc511e91309c76eebb4cfdca38c46397130
naugtur <naugtur@gmail.com>
2024-09-02 11:38:02 +0200

    fix(webpack): deny handling of a null-id module

--- packages/webpack/src/plugin.js
+++ packages/webpack/src/plugin.js
@@ -291,6 +291,15 @@ class LavaMoatPlugin {
                 const moduleClass =
                   Object.getPrototypeOf(module).constructor.name
 
+                if (moduleId === null) {
+                  diag.rawDebug(
+                    2,
+                    `LavaMoatPlugin: module ${module.identifier()} has no moduleId, cannot cover it with policy.`
+                  )
+                  diag.rawDebug(4, { module })
+                  return
+                }
+
                 if (isIgnoredModule(module)) {
                   unenforceableModuleIds.push(moduleId)
                 } else {
