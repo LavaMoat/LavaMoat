deeb484b3d98c57b484e94f4207a01a0820b88d6
legobt <6wbvkn0j@anonaddy.me>
2024-09-24 06:44:33 +0000

    fix(yarn-plugin-allow-script)!: properly block preinstall scripts
    - use native allow-scripts API instead of hooking in via shell
    - block scripts just before execution. instead of after all
    
    fix(yarn-plugin-allow-scripts): strip `node:` prefix from builtin requires in bundle
    
    - no minification of plugin bundle
    - update @lavamoat/allow-scripts
    
    feat!(yarn-plugin-allow-script): support yarn v4; unsupport yarn v3
    
    fix(yarn-plugin-allow-scripts): windows support
    
    fix(yarn-plugin-allow-scripts): block transitive lifecycle scripts for yarn v1 packages
    
    chore(yarn-plugin-allow-scripts): add yarn plugin test
    
    chore(yarn-plugin-allow-scripts): corepack test options
    
    chore: corepack is devDependency of yarn-plugin-allow-scripts
    
    chore(yarn-plugin-allow-scripts): build before test
    
    chore(yarn-plugin-allow-scripts): disable yarn hardened mode in tests
        https://yarnpkg.com/features/security#hardened-mode
    
    chore(yarn-plugin-allow-scripts): set test nodeLinker
    
    chore(yarn-plugin-allow-scripts): portable build script
    
    chore(yarn-plugin-allow-scripts): update bundle
    
    chore(yarn-plugin-allow-scripts): portable tests
    
    chore(yarn-plugin-allow-scripts): update tests to call yarn directly
    
    chore: sync lockfile
    
    chore(yarn-plugin-allow-scripts): use npm-run-path to set PATH
    
    chore(yarn-plugin-allow-scripts): call yarn via corepack in test
    
    chore(yarn-plugin-allow-scripts): shell=true for execs on Windows

--- packages/yarn-plugin-allow-scripts/src/index.ts
+++ packages/yarn-plugin-allow-scripts/src/index.ts
@@ -1,17 +1,141 @@
-import { Plugin } from '@yarnpkg/core'
-import { execute } from '@yarnpkg/shell'
+import {
+  loadAllPackageConfigurations,
+  printMissingPoliciesIfAny,
+} from '@lavamoat/allow-scripts'
+import { Plugin, type Project, type Locator } from '@yarnpkg/core'
+import { dirname } from 'node:path'
+
+// Disable lifecycle scripts for transitive git deps with a yarn classic lockfile
+if (typeof process.env['YARN_IGNORE_SCRIPTS'] === 'undefined') {
+  process.env['YARN_IGNORE_SCRIPTS'] = 'true'
+}
+
+interface ExtendedProject extends Project {
+  allowScriptsConfig?: Map<string, boolean>
+}
+
+// Yarn inserts spurious / for absolute paths on Windows
+const yarnCwdToPath = (cwd: string): string =>
+  // eslint-disable-next-line no-useless-escape
+  process.platform === 'win32' ? cwd.replace(/^[\/]/, '') : cwd
+
+const isPackageScriptAllowed = async (
+  project: ExtendedProject,
+  npm_package_json: string
+): Promise<boolean> => {
+  if (!project.allowScriptsConfig) {
+    project.allowScriptsConfig = new Map<string, boolean>()
+  }
+  const {
+    canonicalNamesByPath,
+    configs: { lifecycle },
+  } = await loadAllPackageConfigurations({
+    rootDir: yarnCwdToPath(project.cwd),
+  })
+  const scriptDir = dirname(npm_package_json)
+  const packageName = canonicalNamesByPath.get(scriptDir)
+  if (!packageName) {
+    process.stderr.write(
+      `FATAL ERROR: allow-scripts blocked execution of unexpected package script. ${JSON.stringify(
+        {
+          package: packageName,
+          manifestPath: npm_package_json,
+        }
+      )}`
+    )
+    process.exit(1)
+  }
+  if (lifecycle.disallowedPatterns.includes(packageName)) {
+    project.allowScriptsConfig.set(packageName, false)
+    return false
+  }
+  if (lifecycle.allowedPatterns.includes(packageName)) {
+    project.allowScriptsConfig.set(packageName, true)
+    return true
+  }
+  return null
+}
 
 const plugin: Plugin = {
   hooks: {
-    afterAllInstalled: async () => {
-      const exitCode = await execute('yarn run allow-scripts')
+    wrapScriptExecution: (
+      executor: () => Promise<number>,
+      project: ExtendedProject,
+      _locator: Locator,
+      npm_lifecycle_event: string,
+      { env: { npm_package_json, npm_package_name } }: { env: any }
+    ): Promise<() => Promise<number>> => {
+      return new Promise((resolve, _reject) =>
+        isPackageScriptAllowed(project, npm_package_json).then((isAllowed) => {
+          if (!isAllowed) {
+            console.error(
+              isAllowed === null
+                ? `  allow-scripts blocking execution of unconfigured package script. ${JSON.stringify([npm_package_name, npm_lifecycle_event, npm_package_json])}`
+                : `  allow-scripts blocking execution of disallowed package script. ${JSON.stringify([npm_package_name, npm_lifecycle_event, npm_package_json])}`
+            )
+            return resolve(() => Promise.resolve(0))
+          }
+          return resolve(executor)
+        })
+      )
+    },
+    setupScriptEnvironment: (
+      project: ExtendedProject,
+      { npm_lifecycle_event, npm_package_json, npm_package_name }
+    ) => {
+      isPackageScriptAllowed(project, npm_package_json).then((isAllowed) => {
+        if (!isAllowed) {
+          console.error(
+            isAllowed === null
+              ? `  allow-scripts detected attempted execution of unconfigured package script. ${JSON.stringify([npm_package_name, npm_lifecycle_event, npm_package_json])}`
+              : `  allow-scripts detected attempted execution of disallowed package script. ${JSON.stringify([npm_package_name, npm_lifecycle_event, npm_package_json])}`
+          )
+        }
+      })
+    },
+    afterAllInstalled: (project: ExtendedProject) => {
+      loadAllPackageConfigurations({
+        rootDir: yarnCwdToPath(project.cwd),
+      }).then((result) => {
+        const {
+          configs: { lifecycle },
+          somePoliciesAreMissing,
+        } = result
+        const extraMissingPolicies = project.allowScriptsConfig
+          ? (() => {
+              const missingPolicies = new Set<string>()
+              for (const [name, _isAllowed] of project.allowScriptsConfig) {
+                const policyValue = lifecycle.allowConfig[name]
+                switch (typeof policyValue) {
+                  case 'boolean':
+                    continue
+                  case 'undefined':
+                    missingPolicies.add(name)
+                    continue
+                  default:
+                    throw new Error(
+                      `Undefined type ${typeof policyValue} for allow-scripts policy value '${policyValue}'`
+                    )
+                }
+              }
+              return missingPolicies
+            })()
+          : new Set<string>()
 
-      if (exitCode !== 0) {
-        // We have to use `process.exit` here rather than setting `process.exitCode`
-        // because Yarn will override any exit code set in this hook.
-        // eslint-disable-next-line n/no-process-exit
-        process.exit(exitCode)
-      }
+        if (extraMissingPolicies.size || somePoliciesAreMissing) {
+          console.error(
+            '\n@lavamoat/allow-scripts has detected dependencies without configuration. explicit configuration required.'
+          )
+          console.error(
+            'run "allow-scripts auto" from the project root to automatically populate the configuration.\n'
+          )
+          printMissingPoliciesIfAny(lifecycle)
+          for (const policy of extraMissingPolicies) {
+            console.error(policy)
+          }
+          process.exit(1)
+        }
+      })
     },
   },
 }
