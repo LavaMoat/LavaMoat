ab311f876248ea4207cceebef90ef5cbad9bce68
naugtur <naugtur@gmail.com>
2024-09-25 16:34:29 +0200

    feat(webpack): unlockedChunksUnsafe option to refrain from protecting selected chunks

--- packages/webpack/src/plugin.js
+++ packages/webpack/src/plugin.js
@@ -444,45 +444,67 @@ class LavaMoatPlugin {
                   )
                   return
                 }
-                diag.rawDebug(
-                  1,
-                  '> adding runtime (additionalChunkRuntimeRequirements)'
-                )
-                // narrow down the policy and map to module identifiers
-                const policyData = identifierLookup.getTranslatedPolicy()
+                let runtimeChunks = []
+                if (
+                  chunk.name &&
+                  options.unlockedChunksUnsafe?.test(chunk.name)
+                ) {
+                  diag.rawDebug(
+                    1,
+                    `> adding UNLOCKED runtime for chunk ${chunk.name}`
+                  )
+                  runtimeChunks = [
+                    {
+                      name: 'ENUM',
+                      file: path.join(__dirname, './ENUM.json'),
+                      json: true,
+                    },
+                    {
+                      name: 'runtime',
+                      file: path.join(
+                        __dirname,
+                        './runtime/runtimeUnlocked.js'
+                      ),
+                    },
+                  ]
+                } else {
+                  diag.rawDebug(1, `> adding runtime for chunk ${chunk.name}`)
+                  // narrow down the policy and map to module identifiers
+                  const policyData = identifierLookup.getTranslatedPolicy()
 
-                const runtimeChunks = [
-                  {
-                    name: 'root',
-                    data: identifierLookup.root || null,
-                    json: true,
-                  },
-                  {
-                    name: 'idmap',
-                    data: identifierLookup.identifiersForModuleIds || null,
-                    json: true,
-                  },
-                  {
-                    name: 'unenforceable',
-                    data: identifierLookup.unenforceableModuleIds || null,
-                    json: true,
-                  },
-                  { name: 'options', data: runtimeOptions, json: true },
-                  { name: 'policy', data: policyData, json: true },
-                  {
-                    name: 'ENUM',
-                    file: path.join(__dirname, './ENUM.json'),
-                    json: true,
-                  },
-                  {
-                    name: 'endowmentsToolkit',
-                    shimRequire: 'lavamoat-core/src/endowmentsToolkit.js',
-                  },
-                  {
-                    name: 'runtime',
-                    file: path.join(__dirname, './runtime/runtime.js'),
-                  },
-                ]
+                  runtimeChunks = [
+                    {
+                      name: 'root',
+                      data: identifierLookup.root || null,
+                      json: true,
+                    },
+                    {
+                      name: 'idmap',
+                      data: identifierLookup.identifiersForModuleIds || null,
+                      json: true,
+                    },
+                    {
+                      name: 'unenforceable',
+                      data: identifierLookup.unenforceableModuleIds || null,
+                      json: true,
+                    },
+                    { name: 'options', data: runtimeOptions, json: true },
+                    { name: 'policy', data: policyData, json: true },
+                    {
+                      name: 'ENUM',
+                      file: path.join(__dirname, './ENUM.json'),
+                      json: true,
+                    },
+                    {
+                      name: 'endowmentsToolkit',
+                      shimRequire: 'lavamoat-core/src/endowmentsToolkit.js',
+                    },
+                    {
+                      name: 'runtime',
+                      file: path.join(__dirname, './runtime/runtime.js'),
+                    },
+                  ]
+                }
 
                 if (options.debugRuntime) {
                   runtimeChunks.push({
@@ -576,6 +598,8 @@ module.exports = LavaMoatPlugin
  * @property {boolean} [HtmlWebpackPluginInterop] - Add a script tag to the html
  *   output for lockdown.js if HtmlWebpackPlugin is in use
  * @property {RegExp} [inlineLockdown] - Prefix the matching files with lockdown
+ * @property {RegExp} [unlockedChunksUnsafe] - Give matching chunks an unsafe
+ *   runtime with no policy enforcement
  * @property {number} [diagnosticsVerbosity] - A number representing diagnostics
  *   output verbosity, the larger the more overwhelming
  * @property {LockdownOptions} lockdown - Options to pass to SES lockdown
