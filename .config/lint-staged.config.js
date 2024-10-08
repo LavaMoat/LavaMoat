1e94ceae7003435639e7d1b9c0abe52901704c28
Christopher Hiller <boneskull@boneskull.com>
2024-10-08 14:36:42 -0700

    chore(node): move to node dir

--- .config/lint-staged.config.js
+++ .config/lint-staged.config.js
@@ -5,7 +5,7 @@ module.exports = {
   '*.js': ['eslint --fix', 'prettier --write'],
   '*.(ts|md|ya?ml|json)': ['prettier --write'],
   '!((package-lock|policy)*).json': ['prettier --write'],
-  'packages/endomoat/src/default-attenuator.js': () => [
+  'packages/node/src/default-attenuator.js': () => [
     'npm run update-policy -w @lavamoat/node',
   ],
 }
