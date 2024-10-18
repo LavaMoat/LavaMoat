1fc39e1bc53d79fbb9e053c0cb5d3f3f24624282
Christopher Hiller <boneskull@boneskull.com>
2024-10-18 14:50:20 -0700

    fix(node): fix a bunch of problems
    - remove own policy generation; attenuator now executed as an exit module
    - fix missing `attenuateModule` in Endo options
    - rename a bunch of constants to hopefully be more obvious whether it's an Endo or LavaMoat thing
    - fix bad call to `getEndowmentsForConfig()`
    - fix reading & validation of policies in `toEndoPolicy()`; it now accepts either a LM policy or a path to it _and_ a policy override _xor_ a policy override path
    - refactor `loadPolicies()` to support `toEndoPolicy`'s behavior
    - unit tests for `toEndoPolicy()`

--- packages/node/src/constants.js
+++ packages/node/src/constants.js
@@ -8,33 +8,29 @@
 import path from 'node:path'
 
 /**
+ * Const string to identify the internal attenuators compartment
+ *
+ * Copied from `@endo/compartment-mapper`
+ *
+ * @privateRemarks
+ * TODO: Export from `@endo/compartment-mapper`
+ */
+export const ATTENUATORS_COMPARTMENT = '<ATTENUATORS>'
+
+/**
+ * Specifier of the default attenuator
+ *
+ * If we wanted Endo to load and execute the code in attenuators compartment, we
+ * could pass `@lavamoat/endomoat/attenuator` as default attenuator and have it
+ * loaded statelessly. We're using an impossible specifier to match with an
+ * external module instead.
+ */
+export const DEFAULT_ATTENUATOR = '@attenuator@'
+
+/**
  * Default policy filename
  */
-export const DEFAULT_POLICY_FILENAME = 'policy.json'
-
-/**
- * Default policy override filename
- */
-export const DEFAULT_POLICY_OVERRIDE_FILENAME = 'policy-override.json'
-
-/**
- * Default policy debug filename
- */
 export const DEFAULT_POLICY_DEBUG_FILENAME = 'policy-debug.json'
-
-/**
- * Relative path to the default policy file
- */
-export const DEFAULT_POLICY_PATH = path.normalize(
-  `lavamoat/node/${DEFAULT_POLICY_FILENAME}`
-)
-
-/**
- * Relative path to the default policy override file
- */
-export const DEFAULT_POLICY_OVERRIDE_PATH = path.normalize(
-  `lavamoat/node/${DEFAULT_POLICY_OVERRIDE_FILENAME}`
-)
 
 /**
  * Relative path to the default policy debug file
@@ -44,59 +40,98 @@ export const DEFAULT_POLICY_DEBUG_PATH = path.normalize(
 )
 
 /**
- * Policy item for the root entry
+ * Default policy filename
  */
-export const POLICY_ITEM_ROOT = 'root'
+export const DEFAULT_POLICY_FILENAME = 'policy.json'
 
 /**
- * Policy item for a writable global
+ * Relative path to the default policy file
  */
-export const POLICY_ITEM_WRITE = 'write'
+export const DEFAULT_POLICY_PATH = path.normalize(
+  `lavamoat/node/${DEFAULT_POLICY_FILENAME}`
+)
+
+/**
+ * Default policy override filename
+ */
+export const DEFAULT_POLICY_OVERRIDE_FILENAME = 'policy-override.json'
+
+/**
+ * Relative path to the default policy override file
+ */
+export const DEFAULT_POLICY_OVERRIDE_PATH = path.normalize(
+  `lavamoat/node/${DEFAULT_POLICY_OVERRIDE_FILENAME}`
+)
+
+/**
+ * When passed into the attenuator, writable globals have this policy item
+ */
+export const ENDO_GLOBAL_POLICY_ITEM_WRITE = 'write'
+
+/**
+ * The `attenuate` prop of an Endo policy
+ */
+export const ENDO_POLICY_ITEM_ATTENUATE = 'attenuate'
+
+/**
+ * Policy item for the root entry
+ */
+export const ENDO_POLICY_ITEM_ROOT = 'root'
 
 /**
  * Policy item for any access
  */
-export const POLICY_ITEM_WILDCARD = 'any'
+export const ENDO_POLICY_ITEM_WILDCARD = 'any'
 
 /**
- * The `dynamic` flag on a LavaMoat `ResourcePolicy`
+ * The `params` prop of an Endo policy
  */
-export const LAVAMOAT_RESOURCE_FLAG_DYNAMIC = 'dynamic'
+export const ENDO_POLICY_ITEM_PARAMS = 'params'
 
 /**
- * The `native` flag on a LavaMoat `ResourcePolicy`
+ * The `defaultAttenuator` prop of an Endo policy
  */
-export const LAVAMOAT_RESOURCE_FLAG_NATIVE = 'native'
+export const ENDO_POLICY_DEFAULT_ATTENUATOR = 'defaultAttenuator'
 
 /**
- * Designator for the root policy item in a LavaMoat policy
+ * The `entry` prop of an Endo policy
  */
-export const LAVAMOAT_PKG_POLICY_ROOT = '$root$'
+export const ENDO_POLICY_ENTRY = 'entry'
 
 /**
- * Name of the `packages` property of a `LavaMoatPackagePolicy`
+ * The `resources` prop of an Endo policy
  */
-export const RSRC_POLICY_PKGS = 'packages'
+export const ENDO_POLICY_RESOURCES = 'resources'
+
+/**
+ * The `noGlobalFreeze` prop of an Endo packge policy
+ */
+export const ENDO_PKG_POLICY_NO_GLOBAL_FREEZE = 'noGlobalFreeze'
 
 /**
  * Name of the `builtins` property of a `LavaMoatPackagePolicy`
  */
-export const RSRC_POLICY_BUILTINS = 'builtins'
+export const ENDO_PKG_POLICY_BUILTINS = 'builtins'
 
 /**
  * Name of the `globals` property of a `LavaMoatPackagePolicy`
  */
-export const RSRC_POLICY_GLOBALS = 'globals'
-
-/**
- * Name of the `options` property of a `LavaMoatPackagePolicy`
- */
-export const RSRC_POLICY_OPTIONS = 'options'
+export const ENDO_PKG_POLICY_GLOBALS = 'globals'
 
 /**
  * The `native` option on a `LavaMoatPackagePolicy`
  */
-export const RSRC_POLICY_OPTION_NATIVE = 'native'
+export const ENDO_PKG_POLICY_OPTION_NATIVE = 'native'
+
+/**
+ * Name of the `options` property of a `LavaMoatPackagePolicy`
+ */
+export const ENDO_PKG_POLICY_OPTIONS = 'options'
+
+/**
+ * Name of the `packages` property of a `LavaMoatPackagePolicy`
+ */
+export const ENDO_PKG_POLICY_PACKAGES = 'packages'
 
 /**
  * `builtin` module type for a `LavamoatModuleRecord`
@@ -114,29 +149,29 @@ export const LMR_TYPE_SOURCE = 'js'
 export const LMR_TYPE_NATIVE = 'native'
 
 /**
+ * Flag for root policy item in a LavaMoat policy
+ */
+export const LAVAMOAT_PKG_POLICY_ROOT = '$root$'
+
+/**
+ * The `native` flag on a LavaMoat `ResourcePolicy`
+ */
+export const LAVAMOAT_PKG_POLICY_NATIVE = 'native'
+
+/**
+ * Policy item for a writable global
+ */
+export const LAVAMOAT_POLICY_ITEM_WRITE = 'write'
+
+/**
+ * Extension for native modules
+ */
+export const NATIVE_PARSER_FILE_EXT = 'node'
+
+/**
  * Name of our custom `native` parser
  *
  * @remarks
  * Used by policy gen
  */
 export const NATIVE_PARSER_NAME = 'native'
-
-/**
- * Extension for native modules
- */
-export const NATIVE_FILE_EXT = 'node'
-
-/**
- * If we wanted Endo to load and execute the code in attenuators compartment, we
- * could pass `@lavamoat/endomoat/attenuator` as default attenuator and have it
- * loaded statelessly. We're using an impossible specifier to match with an
- * external module instead.
- */
-export const DEFAULT_ATTENUATOR = '@attenuator@'
-
-/**
- * Const string to identify the internal attenuators compartment
- *
- * Copied from `@endo/compartment-mapper`
- */
-export const ATTENUATORS_COMPARTMENT = '<ATTENUATORS>'
--- packages/node/test/policy-converter.spec.js
+++ packages/node/test/policy-converter.spec.js
@@ -1,75 +1,189 @@
 import 'ses'
 
 import test from 'ava'
-import { POLICY_ITEM_ROOT, POLICY_ITEM_WILDCARD } from '../src/constants.js'
-import { toEndoPolicy } from '../src/policy-converter.js'
+import { fileURLToPath } from 'node:url'
+import {
+  DEFAULT_ATTENUATOR,
+  ENDO_PKG_POLICY_BUILTINS,
+  ENDO_PKG_POLICY_GLOBALS,
+  ENDO_PKG_POLICY_OPTION_NATIVE,
+  ENDO_PKG_POLICY_OPTIONS,
+  ENDO_PKG_POLICY_PACKAGES,
+  ENDO_POLICY_ITEM_ATTENUATE,
+  ENDO_POLICY_ITEM_PARAMS,
+  ENDO_POLICY_RESOURCES,
+} from '../src/constants.js'
+import {
+  ENDO_POLICY_BOILERPLATE,
+  toEndoPolicy,
+} from '../src/policy-converter.js'
 
 /**
- * @typedef {import('@endo/compartment-mapper').PolicyItem &
- *   import('../src/types.js').RootPolicy} LavaMoatPackagePolicyItem
+ * @import {Policy} from '@endo/compartment-mapper'
+ * @import {LavaMoatPackagePolicyItem} from '../src/types.js'
+ * @import {LavaMoatPolicy, LavaMoatPolicyOverrides} from 'lavamoat-core'
  */
 
-test('toEndoPolicy - basic', async (t) => {
-  /** @type {import('lavamoat-core').LavaMoatPolicy} */
-  const lmPolicy = {
-    resources: {
-      a: {
-        packages: {
-          b: true,
-        },
-        globals: {
-          console: true,
-        },
-        builtin: {
-          'fs.readFile': true,
-        },
-      },
-    },
-  }
+test('toEndoPolicy() - kitchen sink', async (t) => {
+  // this should test all the various bits of a lavamoat policy
+
+  const lmPolicyUrl = new URL(
+    './fixture/lavamoat-policy/kitchen-sink.json',
+    import.meta.url
+  )
+
   /**
-   * @type {import('@endo/compartment-mapper').Policy<LavaMoatPackagePolicyItem>}
+   * @type {Policy<LavaMoatPackagePolicyItem>}
    */
   const expected = {
-    defaultAttenuator: '@lavamoat/node/attenuator',
-    entry: {
-      globals: [POLICY_ITEM_ROOT],
-      noGlobalFreeze: true,
-      packages: POLICY_ITEM_WILDCARD,
-      builtins: POLICY_ITEM_WILDCARD,
-    },
-    resources: {
-      'lavamoat-core': {
-        globals: {
-          'console.warn': true,
-        },
-        builtins: undefined,
-        packages: undefined,
-        options: undefined,
-      },
-      '@lavamoat/node': {
-        packages: {
-          'lavamoat-core': true,
-        },
-        globals: undefined,
-        builtins: {
-          'node:path': true,
-        },
-        options: undefined,
-      },
+    ...ENDO_POLICY_BOILERPLATE,
+    [ENDO_POLICY_RESOURCES]: {
       a: {
-        packages: { b: true },
-        globals: { console: true },
-        builtins: {
+        [ENDO_PKG_POLICY_PACKAGES]: { b: true },
+        [ENDO_PKG_POLICY_GLOBALS]: [{ console: true }],
+        [ENDO_PKG_POLICY_BUILTINS]: {
           fs: {
-            attenuate: '@lavamoat/node/attenuator',
-            params: ['readFile'],
+            [ENDO_POLICY_ITEM_ATTENUATE]: DEFAULT_ATTENUATOR,
+            [ENDO_POLICY_ITEM_PARAMS]: ['readFile'],
           },
         },
-        options: undefined,
+        [ENDO_PKG_POLICY_OPTIONS]: undefined,
+      },
+      'a>b': {
+        [ENDO_PKG_POLICY_BUILTINS]: undefined,
+        [ENDO_PKG_POLICY_OPTIONS]: {
+          [ENDO_PKG_POLICY_OPTION_NATIVE]: true,
+        },
+        [ENDO_PKG_POLICY_PACKAGES]: undefined,
+        [ENDO_PKG_POLICY_GLOBALS]: [{ XMLHttpRequest: 'write' }],
       },
     },
   }
 
+  const actual = await toEndoPolicy(lmPolicyUrl)
+
+  // this may be stable; if it is, use a snapshot maybe
+  t.deepEqual(actual, expected)
+})
+
+test('toEndoPolicy() - no policy', async (t) => {
+  const lmPolicy = undefined
+
+  await t.throwsAsync(toEndoPolicy(/** @type {any} */ (lmPolicy)), {
+    message: 'Expected a policy or policy path',
+  })
+})
+
+test('toEndoPolicy() - empty policy', async (t) => {
+  /** @type {LavaMoatPolicy} */
+  const lmPolicy = { resources: {} }
+
   const actual = await toEndoPolicy(lmPolicy)
+  t.deepEqual(actual, ENDO_POLICY_BOILERPLATE)
+})
+
+test('toEndoPolicy() - policy path as URL', async (t) => {
+  const lmPolicyUrl = new URL(
+    './fixture/lavamoat-policy/empty.json',
+    import.meta.url
+  )
+
+  const actual = await toEndoPolicy(lmPolicyUrl)
+  t.deepEqual(actual, ENDO_POLICY_BOILERPLATE)
+})
+
+test('toEndoPolicy() - policy path as string', async (t) => {
+  const lmPolicyPath = fileURLToPath(
+    new URL('./fixture/lavamoat-policy/empty.json', import.meta.url)
+  )
+
+  const actual = await toEndoPolicy(lmPolicyPath)
+  t.deepEqual(actual, ENDO_POLICY_BOILERPLATE)
+})
+
+test('toEndoPolicy() - invalid policy', async (t) => {
+  await t.throwsAsync(toEndoPolicy(/** @type {any} */ ([1, 2, 3])), {
+    message: 'Invalid LavaMoat policy',
+  })
+})
+
+test('toEndoPolicy() - invalid policy override', async (t) => {
+  /** @type {LavaMoatPolicy} */
+  const lmPolicy = { resources: {} }
+
+  /** @type {LavaMoatPolicyOverrides} */
+  const lmPolicyOverride = /** @type {any} */ ([1, 2, 3])
+
+  const actual = await toEndoPolicy(lmPolicy, {
+    policyOverride: lmPolicyOverride,
+  })
+  t.deepEqual(actual, ENDO_POLICY_BOILERPLATE)
+})
+
+test('toEndoPolicy() - path to invalid policy override', async (t) => {
+  /** @type {LavaMoatPolicy} */
+  const lmPolicy = { resources: {} }
+
+  const lmPolicyOverridePath = new URL(
+    './fixture/lavamoat-policy/invalid.json',
+    import.meta.url
+  )
+
+  await t.throwsAsync(
+    toEndoPolicy(lmPolicy, {
+      policyOverridePath: lmPolicyOverridePath,
+    }),
+    { message: 'Invalid LavaMoat policy overrides' }
+  )
+})
+
+test('toEndoPolicy() - merging', async (t) => {
+  const lmPolicyUrl = new URL(
+    './fixture/lavamoat-policy/kitchen-sink.json',
+    import.meta.url
+  )
+  const lmPolicyOverrideUrl = new URL(
+    './fixture/lavamoat-policy/kitchen-sink-override.json',
+    import.meta.url
+  )
+
+  const actual = await toEndoPolicy(lmPolicyUrl, {
+    policyOverridePath: lmPolicyOverrideUrl,
+  })
+
+  /**
+   * @type {Policy<LavaMoatPackagePolicyItem>}
+   */
+  const expected = {
+    ...ENDO_POLICY_BOILERPLATE,
+    [ENDO_POLICY_RESOURCES]: {
+      a: {
+        [ENDO_PKG_POLICY_PACKAGES]: { b: true },
+        [ENDO_PKG_POLICY_GLOBALS]: [{ console: true }],
+        [ENDO_PKG_POLICY_BUILTINS]: {
+          fs: {
+            [ENDO_POLICY_ITEM_ATTENUATE]: DEFAULT_ATTENUATOR,
+            [ENDO_POLICY_ITEM_PARAMS]: ['readFile'],
+          },
+        },
+        [ENDO_PKG_POLICY_OPTIONS]: undefined,
+      },
+      'a>b': {
+        [ENDO_PKG_POLICY_BUILTINS]: undefined,
+        [ENDO_PKG_POLICY_OPTIONS]: {
+          [ENDO_PKG_POLICY_OPTION_NATIVE]: true,
+        },
+        [ENDO_PKG_POLICY_PACKAGES]: { c: true },
+        [ENDO_PKG_POLICY_GLOBALS]: [{ XMLHttpRequest: 'write' }],
+      },
+      c: {
+        [ENDO_PKG_POLICY_BUILTINS]: { 'node:fs': true },
+        [ENDO_PKG_POLICY_OPTIONS]: undefined,
+        [ENDO_PKG_POLICY_PACKAGES]: undefined,
+        [ENDO_PKG_POLICY_GLOBALS]: undefined,
+      },
+    },
+  }
+
   t.deepEqual(actual, expected)
 })
