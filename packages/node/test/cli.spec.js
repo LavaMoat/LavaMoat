e48ec1bd1ed4391bf85c32ba960914c756d0d886
Christopher Hiller <boneskull@boneskull.com>
2024-10-25 17:31:04 -0700

    chore(node): test updates
    - new tests
    - new macros
    - move `index.spec.js` to `execution.spec.js`
    - enable scenarios with writable globals!

--- packages/node/test/cli.spec.js
+++ packages/node/test/cli.spec.js
@@ -1,183 +1,49 @@
+import 'ses'
+
 import test from 'ava'
-import { execFile } from 'node:child_process'
 import fs from 'node:fs/promises'
 import { tmpdir } from 'node:os'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { promisify } from 'node:util'
 import { DEFAULT_POLICY_FILENAME } from '../src/constants.js'
+import { isPolicy, readPolicy } from '../src/policy.js'
 import { readJsonFile } from '../src/util.js'
-
-const execFileAsync = promisify(execFile)
-
-/**
- * RegExp to match ANSI escape codes
- *
- * @see {@link https://github.com/chalk/ansi-regex}
- */
-const ANSI_REGEX = new RegExp(
-  // eslint-disable-next-line no-control-regex
-  '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))',
-  'g'
-)
+import { runCli } from './fixture-util.js'
+import { createMacros } from './macros.js'
 
 /**
- * Path to the CLI entry point
+ * @import {PackageJson} from 'type-fest'
  */
-const CLI_PATH = fileURLToPath(new URL('../src/cli.js', import.meta.url))
 
+const { testCLI } = createMacros(test)
+
+/**
+ * Path to the "basic" fixture entry point
+ */
 const BASIC_ENTRYPOINT = fileURLToPath(
   new URL('./fixture/basic/app.js', import.meta.url)
 )
+
+/**
+ * The "basic" fixture's directory
+ */
 const BASIC_ENTRYPOINT_CWD = path.dirname(BASIC_ENTRYPOINT)
 
-/**
- * @typedef {import('node:child_process').ExecFileException['code']} ExitCode
- */
+test('"--help" prints help', testCLI, ['--help'])
 
-/**
- * Possible types for the `expected` argument of the {@link testCLI} macro.
- *
- * Execution output will be automatically trimmed and stripped of ANSI escape
- * codes (colors).
- *
- * If a `string`, it is compared to `stdout`.
- *
- * @template [Ctx=unknown] Default is `unknown`
- * @typedef {string
- *   | { code: ExitCode }
- *   | {
- *       stdout: string
- *       code?: ExitCode
- *     }
- *   | {
- *       stderr: string
- *       code?: ExitCode
- *     }
- *   | {
- *       stdout: string
- *       stderr: string
- *       code?: ExitCode
- *     }
- *   | ((
- *       t: import('ava').ExecutionContext<Ctx>,
- *       result: {
- *         stdout: string
- *         stderr: string
- *         code?: ExitCode
- *       }
- *     ) => void | Promise<void>)} ExecLavamoatNodeExpectation
- */
-
-/**
- * Run the `@lavamoat/node` CLI with the provided arguments
- *
- * @param {string[]} args CLI arguments
- * @returns {Promise<{ stdout: string; stderr: string; code: ExitCode }>}
- */
-async function runCli(args) {
-  await Promise.resolve()
-
-  /** @type {string} */
-  let stdout
-  /** @type {string} */
-  let stderr
-  /** @type {import('node:child_process').ExecFileException['code']} */
-  let code
-
-  try {
-    ;({ stdout, stderr } = await execFileAsync(
-      process.execPath,
-      [CLI_PATH, ...args],
-      { encoding: 'utf8' }
-    ))
-  } catch (err) {
-    ;({ stdout, stderr, code } = /**
-     * @type {import('node:child_process').ExecFileException & {
-     *   stdout: string
-     *   stderr: string
-     * }}
-     */ (err))
-  }
-  return { stdout, stderr, code }
-}
-
-/**
- * Macro to run the `@lavamoat/node` CLI with the provided arguments, and
- * optionally perform assertions on the output.
- *
- * If no assertions are provided, the output is snapshotted.
- *
- * @todo Could create a factory function so it could be generic for any CLI
- */
-const testCLI = test.macro(
-  /**
-   * @template [Ctx=unknown] Default is `unknown`
-   * @param {import('ava').ExecutionContext<Ctx>} t Exec context
-   * @param {string[]} args CLI arguments
-   * @param {ExecLavamoatNodeExpectation<Ctx>} [expected] Expected output or
-   *   callback
-   * @returns {Promise<void>}
-   */
-  async (t, args, expected) => {
-    await Promise.resolve()
-
-    t.log(`executing: ${process.execPath} ${CLI_PATH} ${args.join(' ')}`)
-
-    const { stdout, stderr, code } = await runCli(args)
-
-    const trimmedStdout = stdout.trim().replace(ANSI_REGEX, '')
-    const trimmedStderr = stderr.trim().replace(ANSI_REGEX, '')
-
-    switch (typeof expected) {
-      case 'string':
-        t.is(trimmedStdout, expected, 'stdout does not match expected value')
-        break
-      case 'function':
-        await expected(t, {
-          stdout: trimmedStdout,
-          stderr: trimmedStderr,
-          code,
-        })
-        break
-      case 'object':
-        if ('stdout' in expected) {
-          t.is(
-            trimmedStdout,
-            expected.stdout,
-            'stdout does not match expected value'
-          )
-        }
-        if ('stderr' in expected) {
-          t.is(
-            trimmedStderr,
-            expected.stderr,
-            'stderr does not match expected value'
-          )
-        }
-        if ('code' in expected) {
-          t.is(code, expected.code, 'exit code does not match expected value')
-        }
-        break
-      default:
-        t.snapshot(
-          { trimmedStderr, trimmedStdout, code },
-          'execution output does not match expected snapshot'
-        )
-    }
+test(
+  '"--version" matches package descriptor',
+  testCLI,
+  ['--version'],
+  async (t, { stdout }) => {
+    const { version } = /** @type {PackageJson} */ (
+      await readJsonFile(new URL('../package.json', import.meta.url))
+    )
+    t.is(stdout, `${version}`)
   }
 )
 
-test('--help prints help', testCLI, ['--help'])
-
-test('--version', testCLI, ['--version'], async (t, { stdout }) => {
-  const { version } = /** @type {import('type-fest').PackageJson} */ (
-    await readJsonFile(new URL('../package.json', import.meta.url))
-  )
-  t.is(stdout, `${version}`)
-})
-
-test('run - --help prints help', testCLI, ['run', '--help'])
+test('run - "run --help" prints help', testCLI, ['run', '--help'])
 
 test(
   'run - missing entrypoint',
@@ -198,18 +64,23 @@ test(
   'hello world'
 )
 
-test('generate - --help prints help', testCLI, ['generate', '--help'])
+test('generate - "generate --help" prints help', testCLI, [
+  'generate',
+  '--help',
+])
 
 test('generate - basic policy generation', async (t) => {
   const tempdir = await fs.mkdtemp(
     path.join(tmpdir(), 'lavamoat-node-cli-test-')
   )
+
   const policyPath = path.join(tempdir, DEFAULT_POLICY_FILENAME)
+
   try {
     await runCli(['generate', BASIC_ENTRYPOINT, '--policy', policyPath])
-    const json = await fs.readFile(policyPath, 'utf8')
-    const policy = JSON.parse(json)
-    t.deepEqual(policy, { resources: {} })
+    const policy = await readPolicy(policyPath)
+    t.true(isPolicy(policy))
+    // t.deepEqual(policy, { resources: {} })
   } finally {
     await fs.rm(tempdir, { recursive: true, force: true })
   }
--- packages/node/test/fixture-util.js
+++ packages/node/test/fixture-util.js
@@ -1,16 +1,62 @@
 import { memfs } from 'memfs'
 import { fromJsonSnapshot } from 'memfs/lib/snapshot/index.js'
 import { Volume } from 'memfs/lib/volume.js'
+import { execFile } from 'node:child_process'
 import fs from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import { promisify } from 'node:util'
 import { makeReadPowers } from '../src/power.js'
 
 /**
  * @import {ReadNowPowers, FsInterface} from '@endo/compartment-mapper'
- * @import {DirectoryJSON} from 'memfs';
+ * @import {DirectoryJSON} from 'memfs'
+ * @import {ExecutionContext} from 'ava'
  * @import {SnapshotNode} from 'memfs/lib/snapshot/types.js'
  * @import {JsonUint8Array} from 'memfs/lib/snapshot/json.js'
+ * @import {ExecFileException} from 'node:child_process'
  */
 
+/**
+ * Possible types for the `expected` argument of the {@link testCLI} macro.
+ *
+ * Execution output will be automatically trimmed and stripped of ANSI escape
+ * codes (colors).
+ *
+ * If a `string`, it is compared to `stdout`.
+ *
+ * @template [Ctx=unknown] Default is `unknown`
+ * @typedef {string
+ *   | { code: ExitCode }
+ *   | {
+ *       stdout: string
+ *       code?: ExitCode
+ *     }
+ *   | {
+ *       stderr: string
+ *       code?: ExitCode
+ *     }
+ *   | {
+ *       stdout: string
+ *       stderr: string
+ *       code?: ExitCode
+ *     }
+ *   | ((
+ *       t: ExecutionContext<Ctx>,
+ *       result: {
+ *         stdout: string
+ *         stderr: string
+ *         code?: ExitCode
+ *       }
+ *     ) => void | Promise<void>)} ExecLavamoatNodeExpectation
+ */
+
+/**
+ * Possible exit code values, as defined by Node.js
+ *
+ * @typedef {ExecFileException['code']} ExitCode
+ */
+
+const execFileAsync = promisify(execFile)
 const { isArray } = Array
 
 /**
@@ -108,9 +154,52 @@ export async function loadJSONFixture(pathOrJson) {
   const readPowers = makeReadPowers(/** @type {FsInterface} */ (vol))
   return { vol, readPowers }
 }
+
 /**
  * Parsed JSON cache. Can be a `DirectoryJSON` or CJSON snapshot
  *
  * @type {Map<string | URL, object>}
  */
 loadJSONFixture.cache = new Map()
+
+/**
+ * Run the `@lavamoat/node` CLI with the provided arguments
+ *
+ * @param {string[]} args CLI arguments
+ * @returns {Promise<{ stdout: string; stderr: string; code: ExitCode }>}
+ */
+export const runCli = async (args) => {
+  await Promise.resolve()
+
+  /** @type {string} */
+  let stdout
+  /** @type {string} */
+  let stderr
+  /** @type {ExecFileException['code']} */
+  let code
+
+  try {
+    ;({ stdout, stderr } = await execFileAsync(
+      process.execPath,
+      [CLI_PATH, ...args],
+      { encoding: 'utf8' }
+    ))
+  } catch (err) {
+    ;({ stdout, stderr, code } =
+      /**
+       * @type {ExecFileException & {
+       *   stdout: string
+       *   stderr: string
+       * }}
+       */
+
+      (err))
+  }
+  return { stdout, stderr, code }
+}
+
+/**
+ * Path to the CLI entry point
+ */
+
+export const CLI_PATH = fileURLToPath(new URL('../src/cli.js', import.meta.url))
