83f4e52723414a627dc6d683693e3c091728edb4
LeoTM <1881059+leotm@users.noreply.github.com>
2024-12-17 12:53:46 +0000

    test: add more yarn and npm lockfiles

--- packages/lockfile-lint/test/manual/run.sh
+++ packages/lockfile-lint/test/manual/run.sh
@@ -2,9 +2,13 @@
 
 time node ../../src/cli.js ./yarn.lock
 echo "_________________________________________________________ exit" $?
-time node ../../src/cli.js ./yarn.lock --ignore=yarn.lock.ignore.json
+time node ../../src/cli.js ./package-lock.json
 echo "_________________________________________________________ exit" $?
 time node ../../src/cli.js ./yarn2.lock --type yarn
 echo "_________________________________________________________ exit" $?
-time node ../../src/cli.js ./package-lock.json
+time node ../../src/cli.js ./yarn2.lock --type yarn --ignore=yarn.lock.ignore.json
+echo "_________________________________________________________ exit" $?
+time node ../../src/cli.js ./yarn3.lock --type yarn
+echo "_________________________________________________________ exit" $?
+time node ../../src/cli.js ./package-lock2.json --type npm
 echo "_________________________________________________________ exit" $?
