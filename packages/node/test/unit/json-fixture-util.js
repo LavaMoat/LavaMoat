9bab3d356f27e4564eb7b4c6f8969bfc6a70ce0a
Christopher Hiller <boneskull@boneskull.com>
2024-12-19 18:07:36 -0800

    chore(node): test reorganization, cleanup, docs

---
+++ packages/node/test/unit/json-fixture-util.js
@@ -0,0 +1,128 @@
+import { memfs } from 'memfs'
+import { fromJsonSnapshot } from 'memfs/lib/snapshot/index.js'
+import { Volume } from 'memfs/lib/volume.js'
+import fs from 'node:fs'
+import { makeReadPowers } from '../../src/power.js'
+import { isString } from '../../src/util.js'
+
+/**
+ * @import {ReadNowPowers, FsInterface} from '@endo/compartment-mapper'
+ * @import {DirectoryJSON} from 'memfs'
+ * @import {SnapshotNode} from 'memfs/lib/snapshot/types.js'
+ * @import {JsonUint8Array} from 'memfs/lib/snapshot/json.js'
+ */
+
+/**
+ * Type guard for determining what kind of JSON fixture we have.
+ *
+ * This is _very_ loose, but the main difference is that the "Compact JSON"
+ * format is a JSON array and `DirectoryJSON` is an JSON object. Further, the
+ * former is always "compact" (like the output of `JSON.stringify()` without
+ * indents).
+ *
+ * @param {DirectoryJSON | Uint8Array} value
+ * @returns {value is JsonUint8Array<SnapshotNode>}
+ * @see {@link https://jsonjoy.com/specs/compact-json}
+ */
+const isCompactJson = (value) => {
+  return ArrayBuffer.isView(value) && value.subarray(0, 1).toString() === '['
+}
+
+/**
+ * Creates a `Volume` from a "Compact JSON" snapshot.
+ *
+ * @param {JsonUint8Array<SnapshotNode>} snapshotJson - "Compact JSON" snapshot
+ * @returns {Promise<Volume>} New `Volume`
+ */
+async function createVolFromSnapshot(snapshotJson) {
+  const { vol } = memfs()
+  await fromJsonSnapshot(snapshotJson, { fs: vol.promises, path: '/' })
+  return vol
+}
+
+/**
+ * Loads a fixture JSON file or `DirectoryJSON` object or a "Compact JSON" value
+ * and resolves w/ a `Volume` and read powers.
+ *
+ * If a "Compact JSON" value, it must be a `Uint8Array`.
+ *
+ * Caches anything it loads from disk.
+ *
+ * @remarks
+ * `memfs` provides two (or three) different snapshot formats; the first is a
+ * plain JSON object which can be loaded into a `Volume` via
+ * {@link Volume.fromJSON}. The newer "snapshot" format is a binary JSON type
+ * which looks to be more performant which has better handling of binary data
+ * (e.g., _native modules_) and supports symlinks. That's why we load the
+ * fixture into a `Buffer` instead of a `string`; once we've determined the
+ * value is _not_ a "Compact JSON" snapshot, we can parse it as a
+ * `DirectoryJSON` object using {@link JSON.parse}.
+ * @param {string | URL | DirectoryJSON | JsonUint8Array<SnapshotNode>} pathOrJson
+ *   Path to fixture JSON or directory object
+ * @returns {Promise<{
+ *   vol: Volume
+ *   readPowers: ReadNowPowers
+ * }>}
+ * @see {@link https://github.com/streamich/memfs/blob/master/docs/snapshot/index.md}
+ * @todo Standardize entry point filename and return the entry point path.
+ */
+export async function loadJSONFixture(pathOrJson) {
+  /** @type {Volume} */
+  let vol
+  await Promise.resolve()
+  if (isString(pathOrJson) || pathOrJson instanceof URL) {
+    if (loadJSONFixture.dirJsonCache.has(pathOrJson)) {
+      vol = Volume.fromJSON(
+        /** @type {DirectoryJSON} */ (
+          loadJSONFixture.dirJsonCache.get(pathOrJson)
+        )
+      )
+    } else if (loadJSONFixture.snapshotCache.has(pathOrJson)) {
+      vol = await createVolFromSnapshot(
+        /** @type {JsonUint8Array<SnapshotNode>} */ (
+          loadJSONFixture.snapshotCache.get(pathOrJson)
+        )
+      )
+    } else {
+      const filepath = pathOrJson
+      const rawJson = await fs.promises.readFile(filepath)
+      if (isCompactJson(rawJson)) {
+        loadJSONFixture.snapshotCache.set(pathOrJson, rawJson)
+        vol = await createVolFromSnapshot(rawJson)
+      } else {
+        const dirJson = JSON.parse(rawJson.toString('utf-8'))
+        loadJSONFixture.dirJsonCache.set(pathOrJson, dirJson)
+        vol = Volume.fromJSON(dirJson)
+      }
+    }
+  } else {
+    const json = pathOrJson
+    vol = isCompactJson(json)
+      ? await createVolFromSnapshot(json)
+      : Volume.fromJSON(json)
+  }
+
+  const readPowers = makeReadPowers(/** @type {FsInterface} */ (vol))
+  return { vol, readPowers }
+}
+
+/**
+ * Cache of filepath/URL to {@link DirectoryJSON} object.
+ *
+ * @type {Map<string | URL, DirectoryJSON>}
+ */
+loadJSONFixture.dirJsonCache = new Map()
+/**
+ * Cache of filepath/URL to {@link JsonUint8Array} (for snapshots).
+ *
+ * @type {Map<string | URL, JsonUint8Array<SnapshotNode>>}
+ */
+loadJSONFixture.snapshotCache = new Map()
+
+/**
+ * URL path to JSON fixtures.
+ *
+ * @remarks
+ * The trailing slash is load-bearing.
+ */
+export const JSON_FIXTURE_DIR_URL = new URL('./json-fixture/', import.meta.url)
---
+++ packages/node/test/unit/scenario.spec.js
@@ -0,0 +1,51 @@
+import '../../src/preamble.js'
+
+/* eslint-disable ava/no-skip-test */
+import test from 'ava'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore probably broken
+import { loadScenarios } from 'lavamoat-core/test/scenarios/index.js'
+// @ts-expect-error - needs types
+import { runAndTestScenario } from 'lavamoat-core/test/util.js'
+import { createScenarioRunner } from './scenario-util.js'
+
+/**
+ * Macro to test a scenario
+ */
+const testScenario = test.macro(
+  /**
+   * @param {import('ava').ExecutionContext} t
+   * @param {any} scenario
+   */
+  async (t, scenario) => {
+    await runAndTestScenario(t, scenario, createScenarioRunner(t.log.bind(t)))
+  }
+)
+
+// TIL: you can dynamically create tests in an async iterator
+// as long as you use `test.serial()`
+for await (const scenario of loadScenarios()) {
+  if (
+    !(
+      scenario.context &&
+      Object.keys(scenario.context).length === 0 &&
+      scenario.context.constructor === Object
+    )
+  ) {
+    // these will never work. I don't know what they are, though.
+    test.skip(
+      `${scenario.name} - incompatible platform`,
+      testScenario,
+      scenario
+    )
+    continue
+  }
+
+  // TODO implement scuttling
+  if (scenario.name?.startsWith('scuttle')) {
+    test.todo(`${scenario.name} - scuttling unsupported`)
+    continue
+  }
+
+  test.serial(scenario.name ?? '(unknown scenario)', testScenario, scenario)
+}
