4f621d320ead482f468dc7d1d08b6d07cd704a3e
weizman <weizmangal@gmail.com>
2025-01-20 16:16:36 +0200

    add types

--- packages/webpack/src/runtime/runtime-namespace.ts
+++ packages/webpack/src/runtime/runtime-namespace.ts
@@ -7,7 +7,7 @@ type DebugTools = {
 
 type RequiredProperty<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
 export interface RuntimeNamespace {
-  scuttling: {scuttle: <T, U>(arg1: T, arg2: U) => {}}
+  scuttling: {scuttle: (globalThis: any, scuttleGlobalThis: LavaMoatPluginOptions["scuttleGlobalThis"]) => {}}
   root: string
   idmap: [string, string[]][]
   unenforceable: (string | number)[]
