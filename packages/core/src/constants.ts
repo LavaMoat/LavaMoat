5fb20adbeb8d64856779cdf51f9eb330bf719d57
weizman <weizmangal@gmail.com>
2025-02-16 18:42:34 +0200

    try

---
+++ packages/core/src/constants.ts
@@ -0,0 +1,9 @@
+const DEFAULT_GLOBAL_THIS_REFS = Object.freeze(/** @type {const} */({
+  WINDOW: 'window',
+  SELF: 'self',
+  GLOBAL: 'global',
+  GLOBAL_THIS: 'globalThis',
+  TOP: 'top',
+  FRAMES: 'frames',
+  PARENT: 'parent',
+}));
