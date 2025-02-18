677abbaf36ac044563da6837caf9aacc5d67f666
weizman <weizmangal@gmail.com>
2025-02-18 14:36:21 +0200

    fix: remove extra exports

--- packages/core/src/constants.js
+++ packages/core/src/constants.js
@@ -1,20 +1,19 @@
-const GLOBAL_THIS_REFS = Object.freeze(/** @type {const} */({
-  WINDOW: 'window',
-  SELF: 'self',
-  GLOBAL: 'global',
-  GLOBAL_THIS: 'globalThis',
-  TOP: 'top',
-  FRAMES: 'frames',
-  PARENT: 'parent',
-}))
+const GLOBAL_THIS_REFS = Object.freeze(
+  /** @type {const} */ ({
+    WINDOW: 'window',
+    SELF: 'self',
+    GLOBAL: 'global',
+    GLOBAL_THIS: 'globalThis',
+    TOP: 'top',
+    FRAMES: 'frames',
+    PARENT: 'parent',
+  })
+)
 
 const DEFAULT_GLOBAL_THIS_REFS = Object.freeze(Object.values(GLOBAL_THIS_REFS))
 
 /**
- * @typedef {typeof DEFAULT_GLOBAL_THIS_REFS[number]} GlobalThisRef
+ * @typedef {(typeof DEFAULT_GLOBAL_THIS_REFS)[number]} GlobalThisRef
  */
 
-
-module.exports = {GLOBAL_THIS_REFS, DEFAULT_GLOBAL_THIS_REFS}
-
-module.exports = {DEFAULT_GLOBAL_THIS_REFS}
+module.exports = { GLOBAL_THIS_REFS, DEFAULT_GLOBAL_THIS_REFS }
