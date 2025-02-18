80db84a695d02f00aa200c84d2d50bc87bcff64f
weizman <weizmangal@gmail.com>
2025-02-18 14:34:33 +0200

    Update packages/core/src/constants.js
    Co-authored-by: Christopher Hiller <boneskull@boneskull.com>

--- packages/core/src/constants.js
+++ packages/core/src/constants.js
@@ -1,4 +1,4 @@
-const DEFAULT_GLOBAL_THIS_REFS = Object.freeze(/** @type {readonly string[]} */(Object.values({
+const GLOBAL_THIS_REFS = Object.freeze(/** @type {const} */({
   WINDOW: 'window',
   SELF: 'self',
   GLOBAL: 'global',
@@ -6,6 +6,15 @@ const DEFAULT_GLOBAL_THIS_REFS = Object.freeze(/** @type {readonly string[]} */(
   TOP: 'top',
   FRAMES: 'frames',
   PARENT: 'parent',
-})));
+}))
+
+const DEFAULT_GLOBAL_THIS_REFS = Object.freeze(Object.values(GLOBAL_THIS_REFS))
+
+/**
+ * @typedef {typeof DEFAULT_GLOBAL_THIS_REFS[number]} GlobalThisRef
+ */
+
+
+module.exports = {GLOBAL_THIS_REFS, DEFAULT_GLOBAL_THIS_REFS}
 
 module.exports = {DEFAULT_GLOBAL_THIS_REFS}
