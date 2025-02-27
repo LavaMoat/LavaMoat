8ff92d83c19ee777fe29c6ab2a4d851e3f3e7998
Christopher Hiller <boneskull@boneskull.com>
2025-02-27 13:49:28 -0800

    feat(node): add custom errors
    This adds a type definition and factory for custom error classes, including a set of custom errors.  Custom errors may or may not have specific implementations, but they all have a `code` property.

---
+++ packages/node/src/error-code.js
@@ -0,0 +1,28 @@
+/**
+ * Provides {@link ErrorCodes} enum
+ *
+ * @packageDocumentation
+ */
+
+/**
+ * Enum of error codes thrown by this package
+ *
+ * Each one of these corresponds to a specific implementation of
+ * `LavaMoatError`.
+ */
+export const ErrorCodes = Object.freeze(
+  /** @type {const} */ ({
+    AttenuationFailure: 'LMN_ATTENUATION_FAILURE',
+    GenerationFailure: 'LMN_GENERATION_FAILURE',
+    ExecutionFailure: 'LMN_EXECUTION_FAILURE',
+    ConversionFailure: 'LMN_CONVERSION_FAILURE',
+    InvalidArguments: 'LMN_INVALID_ARGUMENTS',
+    InvalidCompartment: 'LMN_INVALID_COMPARTMENT',
+    InvalidPolicy: 'LMN_INVALID_POLICY',
+    NoExecutable: 'LMN_NO_EXECUTABLE',
+    NoPolicy: 'LMN_NO_POLICY',
+    NoWorkspace: 'LMN_NO_WORKSPACE',
+    PermissionDenied: 'LMN_PERMISSION_DENIED',
+    TrustMismatch: 'LMN_TRUST_MISMATCH',
+  })
+)
