c3521cb2b0404380e1b4703d38e41c458cbf139e
LeoTM <1881059+leotm@users.noreply.github.com>
2025-03-28 18:22:29 +0000

    feat: @lavamoat/react-native-lockdown
    Co-authored-by: naugtur <naugtur@gmail.com>

---
+++ packages/react-native-lockdown/src/makeGetPolyfills.js
@@ -0,0 +1,41 @@
+/**
+ * Generates a list of polyfills with SES to include in the Metro bundle. This
+ * sets up a Hardened JavaScript environment on React Native apps at earliest
+ * point of entry.
+ *
+ * @example Metro.config.js const makeGetPolyfills =
+ * require('@lavamoat/react-native-lockdown/makeGetPolyfills'); module.exports =
+ * { serializer: { getPolyfills: ({platform}) => makeGetPolyfills({engine:
+ * 'hermes', polyfills: ['reflect-metadata']}), }, };
+ *
+ * TODO: Implement ({platform: ?string}) from
+ * https://metrobundler.dev/docs/configuration/#getpolyfills
+ *
+ * TODO: De-duplicate polyfills if '@react-native/polyfills' provided.
+ *
+ * NB: Sets up Hardened JS before
+ * https://github.com/facebook/react-native/blob/main/packages/react-native/Libraries/Core/InitializeCore.js
+ * NB: Since we override the default React Native polyfills, we slot them in as
+ * SES vetted shims here.
+ *
+ * @param {Object} options - An object containing the following properties:
+ * @param {string} options.engine - The JavaScript engine to secure. If not
+ *   'hermes', vanilla flavoured SES will be used.
+ * @param {Array} [options.polyfills=[]] - An array of additional polyfills to
+ *   include as SES vetted shims. Defaults to an empty array. Default is `[]`
+ * @returns {Array} An array of polyfills to include in the bundle, listed as
+ *   resolved module paths.
+ */
+const makeGetPolyfills = ({ engine, polyfills }) => {
+  if (polyfills === undefined) {
+    const rnPolyfills = require('@react-native/polyfills')
+    polyfills = rnPolyfills()
+  }
+  const ses =
+    engine === 'hermes' ? require.resolve('ses/hermes') : require.resolve('ses')
+  const repair = require.resolve('./repair.js')
+  const harden = require.resolve('./harden.js')
+  return [ses, repair, ...polyfills, harden]
+}
+
+module.exports = makeGetPolyfills
---
+++ packages/react-native-lockdown/src/repair.js
@@ -0,0 +1,9 @@
+repairIntrinsics({
+  errorTaming: 'unsafe',
+  consoleTaming: 'unsafe',
+  errorTrapping: 'none',
+  unhandledRejectionTrapping: 'none',
+  overrideTaming: 'severe',
+  stackFiltering: 'verbose',
+  evalTaming: 'unsafe-eval',
+})
