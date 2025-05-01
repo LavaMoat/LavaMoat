a5f849122d0fa6f578c18e3888b6427227605fd1
Christopher Hiller <boneskull@boneskull.com>
2025-05-01 13:51:21 -0700

    chore(aa): cleanup symlinks
    This was found when running CI locally (self-hosted). A existing symlink created outside of the self-hosted container causes the test to fail. This makes tests using `createProject4Symlink` cleanup after themselves _and_ ensures a clean symlink upon each execution.
    
    We _also_ need comprehensive "clean" scripts, but that's beyond the scope of this change. 😄

--- packages/aa/test/utils.js
+++ packages/aa/test/utils.js
@@ -1,6 +1,11 @@
+// @ts-check
+
 const path = require('node:path')
-const { symlink } = require('node:fs/promises')
-const { existsSync } = require('node:fs')
+const { symlink, unlink } = require('node:fs/promises')
+
+/**
+ * @import {PathLike} from 'node:fs'
+ */
 
 /**
  * Normalizes paths in canonical name map entries
@@ -12,17 +17,33 @@ exports.normalizePaths = (arr) => {
   return arr.map(([dirpath, name]) => [path.normalize(dirpath), name])
 }
 
-exports.osIndependentSymlink = async (target, src) => {
-  if (!existsSync(src)) {
-    await symlink(
-      target,
-      src,
-      'junction' // Ignored on POSIX, Windows only, same as pkg managers use
-    )
+/**
+ * @param {PathLike} target
+ * @param {PathLike} path
+ * @returns {Promise<void>}
+ */
+exports.osIndependentSymlink = async (target, path) => {
+  try {
+    await unlink(path)
+  } catch (err) {
+    // Ignore error if the symlink does not exist
+    if (/** @type {NodeJS.ErrnoException} */ (err).code !== 'ENOENT') {
+      throw err
+    }
   }
+  await symlink(
+    target,
+    path,
+    'junction' // Ignored on POSIX, Windows only, same as pkg managers use
+  )
 }
+
+/**
+ * @returns {Promise<string>} Path to symlink
+ */
 exports.createProject4Symlink = async () => {
   const src = path.join(__dirname, 'projects', '4', 'node_modules', 'aaa')
   const target = path.join(__dirname, 'projects', '4', 'packages', 'aaa')
   await exports.osIndependentSymlink(target, src)
+  return src
 }
