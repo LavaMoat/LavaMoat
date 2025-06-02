6f9cfa11e1a04a3d3238d24cbabc4879d823a371
naugtur <naugtur@gmail.com>
2025-06-02 14:55:54 +0200

    refactor(webpack): untangle generator wrapper while preserving the time constraints

---
+++ packages/webpack/src/buildtime/types.d.ts
@@ -0,0 +1,28 @@
+import type { LavaMoatPolicy } from 'lavamoat-core'
+import type { LockdownOptions } from 'ses'
+
+export interface ScuttlerObjectConfig {
+  enabled?: boolean
+  exceptions?: string[]
+  scuttlerName?: string
+}
+
+export type ScuttlerConfig = ScuttlerObjectConfig | boolean | undefined
+
+export interface LavaMoatPluginOptions {
+  generatePolicy?: boolean
+  rootDir?: string
+  policyLocation: string
+  emitPolicySnapshot?: boolean
+  readableResourceIds?: boolean
+  HtmlWebpackPluginInterop?: boolean
+  inlineLockdown?: RegExp
+  diagnosticsVerbosity?: number
+  lockdown: LockdownOptions
+  policy?: LavaMoatPolicy
+  runChecks?: boolean
+  isBuiltin: (specifier: string) => boolean
+  scuttleGlobalThis?: ScuttlerConfig
+  debugRuntime?: boolean
+  unlockedChunksUnsafe?: RegExp
+}
