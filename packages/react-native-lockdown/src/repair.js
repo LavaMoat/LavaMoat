201d880f6357110057920f45953b8a412092e931
LeoTM <1881059+leotm@users.noreply.github.com>
2025-06-04 14:13:31 +0100

    docs(react-native-lockdown): fix docstring

--- packages/react-native-lockdown/src/repair.js
+++ packages/react-native-lockdown/src/repair.js
@@ -33,10 +33,7 @@ repairIntrinsics({
   evalTaming: 'unsafe-eval',
 })
 
-/**
- * Hermes specific repairs or shims that need to exist before everything is
- * frozen
- */
+// Hermes specific repairs or shims that need to exist before everything is frozen belong here:
 
 // Hermes built-in implementation of Promise is using this global field internally to hold on to a noop function :|
 Object.assign(Promise, Object.fromEntries(dumbPolyfillExposedInternals))
