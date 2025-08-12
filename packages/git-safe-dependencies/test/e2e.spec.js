0205379ab52a4e1458df5c9b660a9ceba9f8c660
Christopher Hiller <boneskull@boneskull.com>
2025-08-12 05:58:15 -0700

    chore(git-safe-dependencies): fix snapshots by normalizing output (#1759)
    Fixes #1757
    
    This adds further normalization of CLI output to avoid test failures when the actions referenced in the E2E test fixtures are updated (in real life).
    
    - E2E tests do not depend on `process.cwd()` at all; it was previously assumed to be the `git-safe-dependencies` workspace dir, but that is not something we can necessarily expect. This dir is now computed from `__dirname`.
    - Adds some types

--- packages/git-safe-dependencies/test/e2e.spec.js
+++ packages/git-safe-dependencies/test/e2e.spec.js
@@ -1,36 +1,75 @@
-const test = require('ava')
-const { exec } = require('node:child_process')
+/**
+ * @import {TestFn, ExecutionContext} from 'ava'
+ */
+
+const test = /** @type {TestFn} */ (/** @type {unknown} */ (require('ava')))
+const { execFile } = require('node:child_process')
 const path = require('node:path')
 const { promisify } = require('node:util')
 
-const execAsync = promisify(exec)
+const execAsync = promisify(execFile)
 
+/**
+ * Cleans up a string to be used in snapshot test by normalizing certain values.
+ *
+ * @param {string} str The input string to clean up.
+ * @returns {string} The cleaned-up string.
+ */
+const cleanupSnapshotString = (str) =>
+  str
+    // strip cwd
+    .replace(CWD, '/LavaMoat/packages/git-safe-dependencies')
+    // normalize SHA to a fixed value
+    .replace(
+      /(?<=use:\s)([a-z_-]+\/[a-z_-]+)@(?:[0-9a-f]{40})/,
+      '$1@a110ca7ab1edeadbeefcafeaffablebabblebabe'
+    )
+    // normalize problem IDs to a fixed value
+    .replace(
+      /(?<=problem\sid:\s)([a-z_-]+\/[a-z_-]+):(?:[0-9a-f]{8})/,
+      '$1:a110ca7a'
+    )
+
+/**
+ * `git-safe-dependencies` workspace dir
+ */
+const CWD = path.join(__dirname, '..')
+
+/**
+ * `git-safe-dependencies/src` directory
+ */
+const SRC_DIR = path.join(CWD, 'src')
+
+/**
+ * Runs a CLI test with the provided arguments and snapshots the output.
+ *
+ * @param {ExecutionContext} t The test context
+ * @param {Object} options Options
+ * @param {Object} options.args Arguments to pass to the CLI to be passed as
+ *   options
+ * @param {string} [options.cli] The CLI file to run relative to the , defaults
+ *   to 'cli.js'. Relative to
+ * @returns {Promise<void>} A promise that resolves when the test is complete.
+ */
 const runCliTest = test.macro(async (t, { args, cli = 'cli.js' } = {}) => {
   const resolvedArgs = Object.entries(args).map(
     ([key, value]) =>
-      `--${key}=${path.relative(process.cwd(), path.resolve(__dirname, 'fixtures', value))}`
+      `--${key}=${path.relative(process.cwd(), path.join(__dirname, 'fixtures', value))}`
   )
 
-  const bin = path.relative(
-    process.cwd(),
-    path.resolve(__dirname, '..', 'src', cli)
-  )
+  const bin = path.relative(CWD, path.join(SRC_DIR, cli))
+
   const command = `node ${bin} ${resolvedArgs.join(' ')}`
-  const {
-    stdout,
-    stderr,
-    code = 0,
-  } = await execAsync(command, {
-    shell: true,
-  }).catch((error) => error)
+  t.log(`Running command: ${command}`)
+  const { stdout, stderr, ...rest } = await execAsync(process.execPath, [
+    bin,
+    ...resolvedArgs,
+  ]).catch((error) => error)
   t.snapshot({
     command,
-    stdout,
-    stderr: stderr.replace(
-      process.cwd(),
-      '/LavaMoat/packages/git-safe-dependencies'
-    ),
-    code,
+    stdout: cleanupSnapshotString(stdout),
+    stderr: cleanupSnapshotString(stderr),
+    code: 'code' in rest ? rest.code : 0,
   })
 })
 
