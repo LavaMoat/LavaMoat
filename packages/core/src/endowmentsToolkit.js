81fc5acdc4ff5dc76f9e3a6402c70e75be3f6d94
naugtur <naugtur@gmail.com>
2025-08-25 18:46:43 +0200

    feat(core): support FireFox contentscript shenanigans with cross-realm window prototypes

--- packages/core/src/endowmentsToolkit.js
+++ packages/core/src/endowmentsToolkit.js
@@ -531,14 +531,13 @@ function endowmentsToolkit({
     const globalProtoChain = getPrototypeChain(globalRef)
     // the index for the common prototypal ancestor, Object.prototype
     // this should always be the last index, but we check just in case
-    const commonPrototypeIndex = globalProtoChain.findIndex(
+    let commonPrototypeIndex = globalProtoChain.findIndex(
       (globalProtoChainEntry) => globalProtoChainEntry === Object.prototype
     )
     if (commonPrototypeIndex === -1) {
-      // TODO: fix this error message
-      throw new Error(
-        'Lavamoat - unable to find common prototype between Compartment and globalRef'
-      )
+      // "Why would a global have no reference in prototype chain that matches Object.prototype?" you might ask.
+      // It's when the global is partially from a different Realm. And that's possible in extension contentscript in FireFox
+      commonPrototypeIndex = globalProtoChain.length
     }
     // we will copy endowments from all entries in the prototype chain, excluding Object.prototype
     const endowmentSources = globalProtoChain.slice(0, commonPrototypeIndex)
