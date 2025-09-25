9c83030d97371478bf59b10d636e63168bd8473d
Zbyszek Tenerowicz <naugtur@gmail.com>
2025-09-25 14:11:18 +0200

    feat(webpack): document and officially release scuttling in webpack plugin (#1829)

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -6,5 +6,6 @@ export * from './index'
 export type * from './moduleRecord'
 export type * from './options'
 export type * from './parseForPolicy'
+export type { LavaMoatScuttleOpts } from './scuttle.d.ts'
 export { E as EndowmentsToolkitFactory }
 
