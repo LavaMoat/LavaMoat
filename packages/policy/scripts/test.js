f518735aa78498b7cc4a020a790f72aa6ff21356
Christopher Hiller <boneskull@boneskull.com>
2025-11-17 16:10:15 -0800

    feat(policy): create @lavamoat/policy
    This creates `@lavamoat/policy`, a package which centralizes the definition of a LavaMoat policy, provides validation, and contains a JSON schema.

---
+++ packages/policy/scripts/test.js
@@ -0,0 +1,17 @@
+import { spawn } from 'node:child_process'
+import path from 'node:path'
+
+if (
+  process.versions.node.startsWith('20.') ||
+  process.versions.node.startsWith('18.')
+) {
+  console.error('Skipping tests for unsupported Node.js v18.x-v20.x')
+} else {
+  spawn('npm run test:run', {
+    stdio: 'inherit',
+    shell: true,
+    cwd: path.join(import.meta.dirname, '..'),
+  }).once('exit', (code) => {
+    process.exitCode = code ?? 0
+  })
+}
