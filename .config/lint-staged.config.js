34a0a55b5549446b0b3d63e4152c2119a625d1d3
Christopher Hiller <boneskull@boneskull.com>
2026-03-12 15:29:36 -0700

    chore: fix lint-staged config to avoid unprettifiable files
    The previous globs were a heroic effort, but ultimately flawed.  This ensures we do not prettify _anything_ in a `node_modules` dir, which includes test fixtures.

--- .config/lint-staged.config.js
+++ .config/lint-staged.config.js
@@ -1,10 +1,31 @@
 // @ts-check
 
-/** @type {import('lint-staged').Config} */
+/** @param {string} filename */
+const isFormattable = (filename) => !filename.includes('/node_modules/')
+
+/** @type {import('lint-staged').Configuration} */
 module.exports = {
-  '!(**/fixture/**/node_modules/**/)*.js': ['eslint --fix', 'prettier --write'],
-  '!(**/fixture/**/node_modules/**/)*.(ts|md|ya?ml|json)': ['prettier --write'],
-  '!((!(**/fixture/**/node_modules/**/)|(package-lock|policy)*)).json': [
-    'prettier --write',
-  ],
+  '*.js': (files) => {
+    const formattableFiles = files.filter(isFormattable)
+    return formattableFiles.length
+      ? [
+          `eslint --fix ${formattableFiles.join(' ')}`,
+          `prettier --write ${formattableFiles.join(' ')}`,
+        ]
+      : []
+  },
+  '*.(ts|md|ya?ml)': (files) => {
+    const formattableFiles = files.filter(isFormattable)
+    return formattableFiles.length
+      ? [`prettier --write ${formattableFiles.join(' ')}`]
+      : []
+  },
+  '*.json': (files) => {
+    const formattableFiles = files.filter(
+      (p) => isFormattable(p) && !/\/(?:package-lock|policy)[^/]*\.json$/.test(p)
+    )
+    return formattableFiles.length
+      ? [`prettier --write ${formattableFiles.join(' ')}`]
+      : []
+  },
 }
