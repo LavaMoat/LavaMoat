551acb6674572ab9db5d4ce95fed9e76da7d12f1
Christopher Hiller <boneskull@boneskull.com>
2026-05-25 17:18:44 -0700

    feat(vog)!: expose terminal-link as terminalLink
    BREAKING CHANGES: Subpath exports now have `.js` extensions.
    
    - Export `createConsola` as `createLogger` from `log.js`
    - Add `util.js` subpath export.
    - Expose `terminalLink` from `util.js`.

--- packages/vog/src/util.ts
+++ packages/vog/src/util.ts
@@ -9,7 +9,12 @@
  * - Text alignment
  * - Tree drawing
  *
+ * Also included:
+ *
+ * - `terminal-link` for creating links in the terminal
+ *
  * @packageDocumentation
+ * @see {@link https://www.npmjs.com/package/terminal-link}
  */
-
 export * from 'consola/utils'
+export { default as terminalLink } from 'terminal-link'
