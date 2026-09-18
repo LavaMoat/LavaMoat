96d233038c38c129ac7c188de606b90473f92b99
Christopher Hiller <boneskull@boneskull.com>
2026-09-18 13:27:37 -0700

    feat(vog)!: wrap cli-spinner
    BREAKING CHANGE: `@lavamoat/vog` no longer exports a `spinner` namespace and instead exports a `Spinner` class. This new `Spinner` class wraps the original class from `cli-spinner` with new functionality.
    
    - `Spinner` constructor accepts a `chars` option which allows direct access to the underlying characters the spinner uses. This allows consumers to style the spinner characters with ANSI.
    - `Spinner` constructor accepts a `delay` option which calls the `setSpinnerDelay` method so it need not be called after instantiation.

--- packages/vog/src/spinner.ts
+++ packages/vog/src/spinner.ts
@@ -1,7 +1,46 @@
 /**
- * Re-exports {@link https://www.npmjs.com/package/ora ora} as `spinner`.
+ * Wraps {@link https://www.npmjs.com/package/cli-spinner cli-spinner}; exports
+ * {@link Spinner}.
  *
  * @packageDocumentation
  */
 
-export * as spinner from 'cli-spinner'
+import * as spinner from 'cli-spinner'
+
+export interface SpinnerOptions extends spinner.Options {
+  /**
+   * The characters to use for the spinner.
+   */
+  chars?: string[]
+
+  /**
+   * The delay between frames in milliseconds.
+   *
+   * @default 60
+   */
+  delay?: number
+}
+
+/**
+ * Wrapper for {@link https://www.npmjs.com/package/cli-spinner cli-spinner}'s
+ * {@link spinner.Spinner Spinner} class which allows granular control over
+ * spinner characters (enabling ANSI formatting).
+ */
+export class Spinner extends spinner.Spinner {
+  chars!: string[]
+
+  constructor(options: spinner.Options & { chars?: string[]; delay?: number }) {
+    super(options)
+    if (options.chars?.length) {
+      this.setChars(options.chars)
+    }
+    if (options.delay !== undefined) {
+      this.setSpinnerDelay(options.delay)
+    }
+  }
+
+  setChars(chars: string[]): this {
+    this.chars = chars
+    return this
+  }
+}
